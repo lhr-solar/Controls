c00b1b6aae14f5a11cbb8c9246fb537655570478
JimothyGreene <rishiponnekanti@gmail.com>
2020-06-08 15:28:02 -0500

    Creates GUI with buttons for each of the eight useful buttons on the steering wheel. Each button press writes the correct value to the Switches.csv file to reflect the button state

--- BSP/Simulator/Hardware/simulate.py
+++ BSP/Simulator/Hardware/simulate.py
@@ -4,23 +4,42 @@ import tkinter as tk
 import Switches
 
 
-def create_buttons():
-    left_turn = tk.Button(master=window, text="LT", command=lambda:Switches.toggle("LT"))
-    left_turn.grid(row=0, column=0, sticky='nsew')
-    right_turn = tk.Button(master=window, text="RT", command=lambda:Switches.toggle("RT"))
-    right_turn.grid(row=0, column=1, sticky='nsew')
-    headlight = tk.Button(master=window, text="HDLT", command=lambda:Switches.toggle("HDLT"))
-    headlight.grid(row=0, column=2, sticky='nsew')
-    fwd_rev = tk.Button(master=window, text="FWD/REV", command=lambda:Switches.toggle("FWD/REV"))
-    fwd_rev.grid(row=1, column=0, sticky='nsew')
-    hazard = tk.Button(master=window, text="HZD", command=lambda:Switches.toggle("HZD"))
-    hazard.grid(row=1, column=1, sticky='nsew')
-    crs_set = tk.Button(master=window, text="CRS_SET", command=lambda:Switches.toggle("CRS_SET"))
-    crs_set.grid(row=1, column=2, sticky='nsew')
-    crs_en = tk.Button(master=window, text="CRS_EN", command=lambda:Switches.toggle("CRS_EN"))
-    crs_en.grid(row=2, column=0, sticky='nsew')
-    regen = tk.Button(master=window, text="REGEN", command=lambda:Switches.toggle("REGEN"))
-    regen.grid(row=2, column=1, sticky='nsew')
+def update_buttons():
+    """Periodically update the display state of buttons"""
+    switches = Switches.read()
+    if switches & 1:
+        left_turn.config(relief=tk.SUNKEN)
+    else:
+        left_turn.config(relief=tk.RAISED)
+    if switches & 1<<1:
+        right_turn.config(relief=tk.SUNKEN)
+    else:
+        right_turn.config(relief=tk.RAISED)
+    if switches & 1<<2:
+        headlight.config(relief=tk.SUNKEN)
+    else:
+        headlight.config(relief=tk.RAISED)
+    if switches & 1<<3:
+        fwd_rev.config(relief=tk.SUNKEN)
+    else:
+        fwd_rev.config(relief=tk.RAISED)
+    if switches & 1<<4:
+        hazard.config(relief=tk.SUNKEN)
+    else:
+        hazard.config(relief=tk.RAISED)
+    if switches & 1<<5:
+        crs_set.config(relief=tk.SUNKEN)
+    else:
+        crs_set.config(relief=tk.RAISED)
+    if switches & 1<<6:
+        crs_en.config(relief=tk.SUNKEN)
+    else:
+        crs_en.config(relief=tk.RAISED)
+    if switches & 1<<7:
+        regen.config(relief=tk.SUNKEN)
+    else:
+        regen.config(relief=tk.RAISED)
+    window.after(1, update_buttons)
 
 
 # Sets up the display environment variable
@@ -29,9 +48,38 @@ if os.environ.get('DISPLAY','') == '':
 
 # Sets up window
 window = tk.Tk()
-window.rowconfigure([0, 1, 2], minsize=50, weight=1)
-window.columnconfigure([0, 1, 2], minsize=100, weight=1)
+window.rowconfigure([0], minsize=200, weight=1)
+window.columnconfigure([0, 1], minsize=200, weight=1)
 
-create_buttons()
+# Sets up frames
+button_frame = tk.Frame(master=window)
+button_frame.rowconfigure([0, 1, 2], minsize=50, weight=1)
+button_frame.columnconfigure([0, 1, 2], minsize=100, weight=1)
+button_frame.grid(row=0, column=0, sticky='nsew')
 
+display_frame = tk.Frame(master=window)
+display_frame.rowconfigure([0, 1, 2], minsize=50, weight=1)
+display_frame.columnconfigure([0, 1, 2], minsize=100, weight=1)
+display_frame.grid(row=0, column=1, sticky='nsew')
+
+# Creates all buttons
+left_turn = tk.Button(master=button_frame, text="LT", command=lambda:Switches.toggle("LT"))
+left_turn.grid(row=0, column=0, sticky='nsew')
+right_turn = tk.Button(master=button_frame, text="RT", command=lambda:Switches.toggle("RT"))
+right_turn.grid(row=0, column=1, sticky='nsew')
+headlight = tk.Button(master=button_frame, text="HDLT", command=lambda:Switches.toggle("HDLT"))
+headlight.grid(row=0, column=2, sticky='nsew')
+fwd_rev = tk.Button(master=button_frame, text="FWD/REV", command=lambda:Switches.toggle("FWD/REV"))
+fwd_rev.grid(row=1, column=0, sticky='nsew')
+hazard = tk.Button(master=button_frame, text="HZD", command=lambda:Switches.toggle("HZD"))
+hazard.grid(row=1, column=1, sticky='nsew')
+crs_set = tk.Button(master=button_frame, text="CRS_SET", command=lambda:Switches.toggle("CRS_SET"))
+crs_set.grid(row=1, column=2, sticky='nsew')
+crs_en = tk.Button(master=button_frame, text="CRS_EN", command=lambda:Switches.toggle("CRS_EN"))
+crs_en.grid(row=2, column=0, sticky='nsew')
+regen = tk.Button(master=button_frame, text="REGEN", command=lambda:Switches.toggle("REGEN"))
+regen.grid(row=2, column=1, sticky='nsew')
+
+# Sets up periodic updates
+window.after(1, update_buttons)
 window.mainloop()
