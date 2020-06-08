cd9f43a3766203930a81bcb0828c21a56ea699b4
JimothyGreene <rishiponnekanti@gmail.com>
2020-06-08 14:38:45 -0500

    Creates GUI using tkinter in place of old ASCII UI. Much better and has actual button presses

--- BSP/Simulator/Hardware/simulate.py
+++ BSP/Simulator/Hardware/simulate.py
@@ -1,129 +1,37 @@
-import curses
-import threading
-import getch
 import os
+import tkinter as tk
 
 import Switches
 
-# Globals
-stdscr = None
-lights = {"BPS_RDY":0, "ARY_RDY":0, "BPS:FLT":0, "CTRL_FLT":0, "LT":0, "RT":0, "MTR":0, "HDLT":0}
 
-def display():
-    """
-    Displays a simulated version of the steering wheel
-    and accelerator and brake pedals
-    """
-    global stdscr
-    ### Steering Wheel ###
-    # A
-    stdscr.addstr(2, 10, "------------")
-    stdscr.addstr(3, 10, "| << || >> |")
-    stdscr.addstr(4, 10, "------------")
-    # B
-    stdscr.addstr(11, 35, "| FWD/REV |")
-    stdscr.addstr(12, 35, "-----------")
-    # C
-    stdscr.addstr(7, 51, " CRS EN  |")
-    stdscr.addstr(8, 51, "----------")
-    # D
-    stdscr.addstr(5, 51, " CRS SET |")
-    stdscr.addstr(6, 51, "----------")
-    # E
-    stdscr.addstr(3, 62, "| REGEN |")
-    stdscr.addstr(4, 62, "---------")
-    # F
-    stdscr.addstr(0, 0, "---------")
-    stdscr.addstr(1, 0, "| RADIO |")
-    stdscr.addstr(2, 0, "---------")
-    # G
-    stdscr.addstr(5, 10, "| BPS ARY  |")
-    stdscr.addstr(6, 10, "| BPS CTRL |")
-    stdscr.addstr(7, 10, "| LT  RT   |")
-    stdscr.addstr(8, 10, "| MTR HDLT |")
-    stdscr.addstr(9, 10, "------------")
-    # H
-    stdscr.addstr(2, 51, "----------")
-    stdscr.addstr(3, 51, "   HZD   |")
-    stdscr.addstr(4, 51, "----------")
-    # I
-    stdscr.addstr(0, 62, "--------")
-    stdscr.addstr(1, 62, "| HORN |")
-    stdscr.addstr(2, 62, "--------")
-    # J
-    stdscr.addstr(11, 25, "| HDLT |")
-    stdscr.addstr(12, 25, "--------")
-    # Center
-    stdscr.addstr(2, 22, "-----------------------------")
-    stdscr.addstr(3, 50, "|")
-    stdscr.addstr(4, 50, "|")
-    stdscr.addstr(5, 50, "|")
-    stdscr.addstr(6, 50, "|")
-    stdscr.addstr(7, 50, "|")
-    stdscr.addstr(8, 22, "-----------------------------")
-    stdscr.addstr(9, 22, "          Rear View         |")
-    stdscr.addstr(10, 22, "----------------------------")
-
-    ### Pedals ###
-    stdscr.refresh()
+def create_buttons():
+    left_turn = tk.Button(master=window, text="LT", command=lambda:Switches.toggle("LT"))
+    left_turn.grid(row=0, column=0, sticky='nsew')
+    right_turn = tk.Button(master=window, text="RT", command=lambda:Switches.toggle("RT"))
+    right_turn.grid(row=0, column=1, sticky='nsew')
+    headlight = tk.Button(master=window, text="HDLT", command=lambda:Switches.toggle("HDLT"))
+    headlight.grid(row=0, column=2, sticky='nsew')
+    fwd_rev = tk.Button(master=window, text="FWD/REV", command=lambda:Switches.toggle("FWD/REV"))
+    fwd_rev.grid(row=1, column=0, sticky='nsew')
+    hazard = tk.Button(master=window, text="HZD", command=lambda:Switches.toggle("HZD"))
+    hazard.grid(row=1, column=1, sticky='nsew')
+    crs_set = tk.Button(master=window, text="CRS_SET", command=lambda:Switches.toggle("CRS_SET"))
+    crs_set.grid(row=1, column=2, sticky='nsew')
+    crs_en = tk.Button(master=window, text="CRS_EN", command=lambda:Switches.toggle("CRS_EN"))
+    crs_en.grid(row=2, column=0, sticky='nsew')
+    regen = tk.Button(master=window, text="REGEN", command=lambda:Switches.toggle("REGEN"))
+    regen.grid(row=2, column=1, sticky='nsew')
 
 
-def generate():
-    """
-    Generates all hardware simulated data
-    """
-    pass
+# Sets up the display environment variable
+if os.environ.get('DISPLAY','') == '':
+    os.environ.__setitem__('DISPLAY', ':0')
 
+# Sets up window
+window = tk.Tk()
+window.rowconfigure([0, 1, 2], minsize=50, weight=1)
+window.columnconfigure([0, 1, 2], minsize=100, weight=1)
 
-def screen_init():
-    global stdscr
-    stdscr = curses.initscr()
-    curses.start_color()
-    curses.noecho()
-    curses.cbreak()
+create_buttons()
 
-
-def screen_end():
-    curses.echo()
-    curses.nocbreak()
-    curses.endwin()
-    stdscr = None
-
-
-def user_input():
-    """
-    Gets user input to alter the state of the display
-    """
-    while True:
-        ch = getch.getch().lower()
-        if ch == 'q':
-            screen_end()
-            os._exit(0)
-        elif ch == 'r':
-            lights["RT"] = 1
-
-
-def engine():
-    """
-    Main engine for the simulator
-    """
-    screen_init()
-    while True:
-        try:
-            display()
-        except KeyboardInterrupt:
-            screen_end()
-            os._exit(1)
-        except Exception as e:
-            screen_end()
-            print(e)
-
-
-if __name__ == "__main__":
-    engine = threading.Thread(target=engine, daemon=True)
-    user_input = threading.Thread(target=user_input, daemon=True)
-    engine.start()
-    user_input.start()
-    screen_end()
-    while True:
-        pass
+window.mainloop()
