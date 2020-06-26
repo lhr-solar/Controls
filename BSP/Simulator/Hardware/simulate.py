c5b392a582ac6ea53534ba08dbb71f35370a479b
Rishi Ponnekanti <51857775+JimothyGreene@users.noreply.github.com>
2020-06-26 14:30:53 -0500

    Simulator Switches (#21)
    * Creates toggle function to allow the simulator engine to toggle a specific switch
    
    * Creates read function to allow the simulation engine to read the current switch states
    
    * Creates toggle function to allow the simulator engine to toggle a specific switch
    
    * Creates BSP_Switches outline
    
    * Creates test for reading switch states
    
    * Cleans up update buttons
    
    * Adds locks on the file opening
    
    * Adds documentation
    
    * Adds iginition switches and cleans up simulator code
    
    * Separates file names into the bsp.h file

--- BSP/Simulator/Hardware/simulate.py
+++ BSP/Simulator/Hardware/simulate.py
@@ -1,5 +1,6 @@
 import os
 import tkinter as tk
+from functools import partial
 
 import Switches
 
@@ -7,38 +8,11 @@ import Switches
 def update_buttons():
     """Periodically update the display state of buttons"""
     switches = Switches.read()
-    if switches & 1:
-        left_turn.config(relief=tk.SUNKEN)
-    else:
-        left_turn.config(relief=tk.RAISED)
-    if switches & 1<<1:
-        right_turn.config(relief=tk.SUNKEN)
-    else:
-        right_turn.config(relief=tk.RAISED)
-    if switches & 1<<2:
-        headlight.config(relief=tk.SUNKEN)
-    else:
-        headlight.config(relief=tk.RAISED)
-    if switches & 1<<3:
-        fwd_rev.config(relief=tk.SUNKEN)
-    else:
-        fwd_rev.config(relief=tk.RAISED)
-    if switches & 1<<4:
-        hazard.config(relief=tk.SUNKEN)
-    else:
-        hazard.config(relief=tk.RAISED)
-    if switches & 1<<5:
-        crs_set.config(relief=tk.SUNKEN)
-    else:
-        crs_set.config(relief=tk.RAISED)
-    if switches & 1<<6:
-        crs_en.config(relief=tk.SUNKEN)
-    else:
-        crs_en.config(relief=tk.RAISED)
-    if switches & 1<<7:
-        regen.config(relief=tk.SUNKEN)
-    else:
-        regen.config(relief=tk.RAISED)
+    for i, button in enumerate(buttons):
+        if switches & 1<<i:
+            button.config(relief=tk.SUNKEN)
+        else:
+            button.config(relief=tk.RAISED)
     window.after(1, update_buttons)
 
 
@@ -53,8 +27,10 @@ window.columnconfigure([0, 1], minsize=200, weight=1)
 
 # Sets up frames
 button_frame = tk.Frame(master=window)
-button_frame.rowconfigure([0, 1, 2], minsize=50, weight=1)
-button_frame.columnconfigure([0, 1, 2], minsize=100, weight=1)
+button_frame_rows = [0, 1, 2, 3]
+button_frame_columns = [0, 1, 2]
+button_frame.rowconfigure(button_frame_rows, minsize=50, weight=1)
+button_frame.columnconfigure(button_frame_columns, minsize=100, weight=1)
 button_frame.grid(row=0, column=0, sticky='nsew')
 
 display_frame = tk.Frame(master=window)
@@ -63,22 +39,11 @@ display_frame.columnconfigure([0, 1, 2], minsize=100, weight=1)
 display_frame.grid(row=0, column=1, sticky='nsew')
 
 # Creates all buttons
-left_turn = tk.Button(master=button_frame, text="LT", command=lambda:Switches.toggle("LT"))
-left_turn.grid(row=0, column=0, sticky='nsew')
-right_turn = tk.Button(master=button_frame, text="RT", command=lambda:Switches.toggle("RT"))
-right_turn.grid(row=0, column=1, sticky='nsew')
-headlight = tk.Button(master=button_frame, text="HDLT", command=lambda:Switches.toggle("HDLT"))
-headlight.grid(row=0, column=2, sticky='nsew')
-fwd_rev = tk.Button(master=button_frame, text="FWD/REV", command=lambda:Switches.toggle("FWD/REV"))
-fwd_rev.grid(row=1, column=0, sticky='nsew')
-hazard = tk.Button(master=button_frame, text="HZD", command=lambda:Switches.toggle("HZD"))
-hazard.grid(row=1, column=1, sticky='nsew')
-crs_set = tk.Button(master=button_frame, text="CRS_SET", command=lambda:Switches.toggle("CRS_SET"))
-crs_set.grid(row=1, column=2, sticky='nsew')
-crs_en = tk.Button(master=button_frame, text="CRS_EN", command=lambda:Switches.toggle("CRS_EN"))
-crs_en.grid(row=2, column=0, sticky='nsew')
-regen = tk.Button(master=button_frame, text="REGEN", command=lambda:Switches.toggle("REGEN"))
-regen.grid(row=2, column=1, sticky='nsew')
+buttons = []
+for i, switch in enumerate(Switches.get_switches()):
+    button = tk.Button(master=button_frame, text=switch, command=partial(Switches.toggle, switch))
+    button.grid(row=i//max(button_frame_rows), column=i%len(button_frame_columns), sticky='nsew')
+    buttons.append(button)
 
 # Sets up periodic updates
 window.after(1, update_buttons)
---
+++ Config/Inc/config.h
@@ -0,0 +1,11 @@
+/**
+ * Configuration file to hold
+ * shared constants, enumerations, structs
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+typedef enum {OFF = 0, ON} state_t;
+
+#endif
