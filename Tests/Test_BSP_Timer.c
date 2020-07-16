df905a8d365e09ab1b9763bac0b1eb34bc51093f
rafidh611 <55178680+rafidh611@users.noreply.github.com>
2020-07-16 11:20:56 -0500

    Create Test_BSP_Timer.c

---
+++ Tests/Test_BSP_Timer.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include "BSP_Timer.h"
+
+//temporary test function
+void functionOne()
+{
+  std::cout<<"First Function worked";
+
+}
+
+void functionTwo()
+{
+  std::cout<<"Second Function worked";
+}
+
+
+int main()
+{
+    void (*functOnepoiner)= &functionOne;
+    void (*functTwopoiner)= &functionTwo;
+    int count=0;
+
+
+    BSP_Timer_Init(1000,functOnepoiner,functTwopoiner);
+
+    while(1){
+      count++;
+      BSP_Update_Time(count);
+    }
+    
+}
