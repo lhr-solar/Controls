66e2fe5f62ffb7cb0ee1d4ed82e0cb73aafe79a4
rafidh611 <55178680+rafidh611@users.noreply.github.com>
2020-07-16 11:17:59 -0500

    Update BSP_Timer.h

--- BSP/Inc/BSP_Timer.h
+++ BSP/Inc/BSP_Timer.h
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int (*function1);    //function pointers
-int (*function2);  
+void (*function1);    //function pointers
+void (*function2);  
 void BSP_Update_Time(int row);
-void BSP_Timer_Init(int reload, int *func1, int *func2);
+void BSP_Timer_Init(int reload, void *func1, void *func2);
