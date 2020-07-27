3b8b382a7dcc3a8a779a62bba552034a9a7acda7
rafidh611 <55178680+rafidh611@users.noreply.github.com>
2020-07-27 12:26:24 -0500

    Update BSP_Timer.h
    Updated the header file to update Coding Format

--- BSP/Inc/BSP_Timer.h
+++ BSP/Inc/BSP_Timer.h
@@ -1,8 +1,22 @@
-#include<stdio.h>
+/**
+ * Header file for the library to interact
+ * with the timers
+ */
+#include <stdio.h>
 
-#include<stdlib.h>
+#include <stdlib.h>
 
-
+/** 
+ * @brief   Updates the time by reading the CSV file and calling relevant functions at the right time 
+ * @param   none
+ * @return  none
+ */  
 void BSP_Timer_Update();
+
+/** 
+ * @brief   Intializes the Timer
+ * @param   2 reload values for two timers, Two function pointers 
+ * @return  none
+ */  
 void BSP_Timer_Init(int Timer1Reload,int Timer2Reload, void *func1, void *func2);
 
