e9ba5bd86c3e0b154da10b2fff4ed07b3fbcff44
rafidh611 <55178680+rafidh611@users.noreply.github.com>
2020-07-28 11:29:41 -0500

    Update BSP_Timer.c
    Made modifications to both of the functions after PR review

--- BSP/Simulator/Src/BSP_Timer.c
+++ BSP/Simulator/Src/BSP_Timer.c
@@ -3,73 +3,59 @@
 static void (*Timer1Handler)(int);
 static void (*Timer2Handler)(int);
 //had to pass some sort of parameter to the pointer or it doesn't work
+
 /** 
-
- * @brief   Updates the time by reading the CSV file and calling relevant functions at the right time 
-
- * @param   none
-
- * @return  none
-
- */  
-
+* @brief   Updates the time by reading the CSV file and calling relevant functions at the right time 
+* @param   none
+* @return  none
+*/  
 void BSP_Timer_Update()
 {
-   FILE *file = fopen("Timer.csv", "r");
-    int arr[4];
-
+    FILE *file = fopen("Timer.csv", "r");
+    int fno = fileno(file);
+    flock(fno, LOCK_EX);
+    int timeCurrent[2];
     int current;
-
     int reload;
-
     int i=0;
-
     int index=0;
-
-
+    
     while(i<2)
     {
         fscanf(file,"%d , %d",&current,&reload);
-        arr[index]=current;
-        arr[index+1]=reload;
-        index=+2;
+        timeCurrent[index]=current;
+        index++;
         i++;
         
     }
-     
+    flock(fno, LOCK_UN);
     fclose(file);
  
 
-     if(arr[0]==0)
+     if(timeCurrent[0]==0)
       {
           Timer1Handler(0);
       }
-      if(arr[2]==0)
+      if(timeCurrent[1]==0)
       {
           Timer2Handler(0); 
       }
-    
+  }
 
-    
-
-    
-
-}
 /** 
-
- * @brief   Intializes the Timer
-
- * @param   2 reload values for two timers, Two function pointers 
-
- * @return  none
-
+* @brief   Intializes the Timer
+* @param   First reload value for Timer one
+* @param   Second reload value for Timer two
+* @param   First function pointer
+* @param   Second function pointer
+* @return  none
  */  
 
 void BSP_Timer_Init(int Timer1Reload,int Timer2Reload,void *func1,void *func2)
 {
   FILE *TimerData = fopen("Timer.csv","w");
-  fprintf(TimerData,"%d , %d",Timer1Reload,Timer1Reload);
-  fprintf(TimerData,"\n%d , %d",Timer2Reload,Timer2Reload);
+  fprintf(TimerData,"%d,%d",Timer1Reload,Timer1Reload);
+  fprintf(TimerData,"\n%d,%d",Timer2Reload,Timer2Reload);
   fclose(TimerData);
   Timer1Handler = func1;
   Timer2Handler = func2;
