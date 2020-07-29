99ffbdc7fde635c6a617ed584258e1be39bdf10d
Sugam Arora <sugamarora@DESKTOP-EAPDOTJ.localdomain>
2020-07-29 15:28:42 -0500

    Fixed documentation style and variable names in Contactor.py. Removed redundant code from Contactor.py. Created Label frame for contactors on GUI and made a separate function to update contactors status.

--- BSP/Simulator/Hardware/Contactor.py
+++ BSP/Simulator/Hardware/Contactor.py
@@ -5,23 +5,23 @@ import fcntl
 
 file = "BSP/Simulator/Hardware/Data/Contactors.csv"
 
-#Reads file and returns 1 or 0, 1 is on, 0 is off
-def Read_Contactor():
+def read():
+    """Reads contactor file and returns 1 or 0, 1 is on, 0 is off
+    Returns:
+        list: [motor_state, array_state]
+    """
     returnNothing = ["", ""]
-    if os.path.isfile(file) == True:    #only execute the following code if the .csv file has been created
-        os.makedirs(os.path.dirname(file), exist_ok=True)
-        with open(file, 'r') as csvfile:
-            fcntl.flock(csvfile.fileno(), fcntl.LOCK_EX)    # Lock file before reading
-            try:    #the following code is to prevent simulator from crashing if the csv is blank
-                csvreader = csv.reader(csvfile)
-                Contactor_State = next(csvreader)
-            except StopIteration as stopIt:
-                Contactor_State = returnNothing
-            fcntl.flock(csvfile.fileno(), fcntl.LOCK_UN)    # Unlock file after reading
-            return Contactor_State
-    else:
-        os.makedirs(os.path.dirname(file), exist_ok=True)
-        with open(file, 'w') as csvfile:
-            fcntl.flock(csvfile.fileno(), fcntl.LOCK_EX)    
-            fcntl.flock(csvfile.fileno(), fcntl.LOCK_UN)   
-        return returnNothing #csv file has just been made, there is no data to read
+    os.makedirs(os.path.dirname(file), exist_ok=True)
+    if not os.path.exists(file):
+        open(file, 'w')
+
+    with open(file, 'r') as csvfile:
+        fcntl.flock(csvfile.fileno(), fcntl.LOCK_EX)    # Lock file before reading
+        try:    #the following code is to prevent simulator from crashing if the csv is blank
+            csvreader = csv.reader(csvfile)
+            Contactor_State = next(csvreader)
+        except StopIteration as stopIt:
+            Contactor_State = returnNothing
+        fcntl.flock(csvfile.fileno(), fcntl.LOCK_UN)    # Unlock file after reading
+        return Contactor_State 
+        
--- BSP/Simulator/Hardware/simulate.py
+++ BSP/Simulator/Hardware/simulate.py
@@ -9,16 +9,20 @@ import Contactor
 def update_buttons():
     """Periodically update the display state of buttons"""
     switches = Switches.read()
-    contactorState = Contactor.Read_Contactor()
     for i, button in enumerate(buttons):
         if switches & 1<<i:
             button.config(relief=tk.SUNKEN)
         else:
             button.config(relief=tk.RAISED)
-    contactor_status = tk.Label(master=window, text="Motor Contactor:" + contactorState[0]+"\nArray Contactor:" + contactorState[1], font=("Helvetica", 8))
-    contactor_status.place(x=325, y= 10)
     window.after(10, update_buttons)
 
+def update_contactors():
+    """Periodically update the display state of the Motor and Array Contactors"""
+    global motor_status, array_status
+    contactors_status = Contactor.read()
+    motor_status.set(f"Motor Contactor: {contactors_status[0]}")
+    array_status.set(f"Array Contactor: {contactors_status[1]}")
+    window.after(30, update_contactors)
 
 # Sets up the display environment variable
 if os.environ.get('DISPLAY','') == '':
@@ -42,6 +46,13 @@ display_frame.rowconfigure([0, 1, 2], minsize=50, weight=1)
 display_frame.columnconfigure([0, 1, 2], minsize=100, weight=1)
 display_frame.grid(row=0, column=1, sticky='nsew')
 
+contactor_frame = tk.LabelFrame(master=window, text="Contactors")
+contactor_frame_rows = [0,1]
+contactor_frame_columns = [0]
+contactor_frame.rowconfigure(contactor_frame_rows, minsize=50, weight = 1)
+contactor_frame.columnconfigure(contactor_frame_columns, minsize=50, weight=1)
+contactor_frame.grid(row=1, column=0, sticky='nsew')
+
 # Creates all buttons
 buttons = []
 for i, switch in enumerate(Switches.get_switches()):
@@ -49,11 +60,15 @@ for i, switch in enumerate(Switches.get_switches()):
     button.grid(row=i//max(button_frame_rows), column=i%len(button_frame_columns), sticky='nsew')
     buttons.append(button)
 
-#Creates text that displays Contactor states
-contactor_status = tk.Label(master=window, text="Motor Contactor: \nArray Contactor:", font=("Helvetica", 8))
-contactor_status.place(x=325, y= 10)
-
+### Contactors ###
+motor_status = tk.StringVar(value= 'Motor Contactor: ')
+motor_ = tk.Label(master=contactor_frame, textvariable=motor_status)
+motor_.grid(row = 0, column = 0, sticky='nsew')
+array_status = tk.StringVar(value= 'Array Contactor: ')
+array_txt = tk.Label(master=contactor_frame, textvariable=array_status)
+array_txt.grid(row=1, column=0, sticky='nsew')
 
 # Sets up periodic updates
 window.after(10, update_buttons)
+window.after(30, update_contactors)
 window.mainloop()
