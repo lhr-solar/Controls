4542158e24753016354a02ba02ebbc535252b2e1
rafidh611 <55178680+rafidh611@users.noreply.github.com>
2020-08-23 10:03:19 -0500

    Update CAN.c

--- Drivers/Src/CAN.c
+++ Drivers/Src/CAN.c
@@ -85,3 +85,31 @@ static void floatToBytes(float val, uint8_t* bytes_array, uint8_t bytes) {
 
 	return(bytes_array);
 }
+
+/**
+ * @brief   Checks if the CAN ID matches with expected ID and then copies message to given buffer array
+ * @param   CAN line bus
+ * @param   CAN message ID
+ * @param   pointer to buffer array to store message
+ * @return  0 if ID matches and 1 if it doesn't
+ */
+
+int CANbus_Read(CAN_t bus, uint32_t Expected_ID, uint8_t* buffer)
+{
+    uint32_t ID;
+    uint8_t data[];
+    uint8_t count = BSP_CAN_READ(bus,*ID,data);
+    if(Expected_ID == id)
+    {
+        for(int i=0;i<count;i++)
+        {
+            buffer[i]=data[i];
+        }
+	return 0;
+    }
+    else
+    {
+      return 1;
+    }
+    
+}
