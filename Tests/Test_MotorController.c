66e9a09038a9a36d675a35f7d7f8df0196376cb2
Sugam Arora <sugamarora@DESKTOP-EAPDOTJ.localdomain>
2020-08-25 11:47:14 -0500

    Removed expectedID items from test file

--- Tests/Test_MotorController.c
+++ Tests/Test_MotorController.c
@@ -10,12 +10,11 @@ int main(){
     MotorController_Drive(input1,input2);
 
     CANbuff *tester;
-    uint32_t expectedID = 0x243;
     uint32_t id = 0x243;
     uint8_t data[8] = {0x00, 0x55, 0x55, 0x22,     0x11, 0x11, 0x11, 0x11};
     BSP_CAN_Write(CAN_2, id, data, 2);
-    bool check = MotorController_Read(tester, expectedID);
-    printf("The expected ID is :%x \n\rThe actual ID on the bus was: %x\n\rDid they match? : %d\n\r%x, %x", expectedID, tester->id, check,tester->firstNum, tester->secondNum); 
+    bool check = MotorController_Read(tester);
+    printf("The ID on the bus was: %x\n\rMessage: %x, %x",tester->id, tester->firstNum, tester->secondNum); 
     exit(0);
     
 }
