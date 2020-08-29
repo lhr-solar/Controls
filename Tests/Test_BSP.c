7d672446307aa1b92e7d1c086286eda5bb67e756
Erick Cortez <erick-cortez98@hotmail.com>
2020-08-29 15:25:28 -0500

    deleted individual selection of test in TEST_BSP.c

--- Tests/Test_BSP.c
+++ Tests/Test_BSP.c
@@ -13,31 +13,19 @@
 #include "BSP_ADC.h"
 #include <bsp.h>
 
-int selectionMessage(){
-    int selection = -1;
-    printf("-----------------------------------------------------\n");
-    printf("---------------------BSP TESTS-----------------------\n");
-    printf("Test ADC - 0\n");
-    printf("Test CAN - 1\n");
-    printf("Test Contactors - 2\n");
-    printf("Test Switches - 3\n");
-    printf("Test Timer - 4\n");
-    printf("Test UART - 5\n");
-    printf("-----------------------------------------------------\n");
-    printf("Provide number of test\n");
-    scanf("%d", &selection);
-
-    return selection;
-}
 
 int main() {
     while(1) {
-        int selection = selectionMessage();
         
-        while(selection < 0 || selection > 5){
-            printf("\nERROR: NUMBER OF TEST PROVIDED IS NOT BETWEEN 0 AND 5\n");
-            selection = selectionMessage();
-        }
-        break;
+        printf("----------------------------------------------------\n");
+        printf("--------------------ADC Test------------------------\n");
+        //Init the BSP with the A
+        BSP_ADC_Init(ADC_0);
+        BSP_ADC_Init(ADC_1);
+
+        BSP_CAN_Init(CAN_1);
+        BSP_CAN_Init(CAN_2);
+        printf("Accelerator: %5.1d\tBrake: %5.1d\r", BSP_ADC_Get_Millivoltage(ADC_0), BSP_ADC_Get_Millivoltage(ADC_1));
+
     }
 }
