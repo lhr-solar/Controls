ed23eb2c42d704320874063dda02867c5a794429
Cam0Cow <roie.roiegal@gmail.com>
2020-08-29 16:04:31 -0500

    Created cursor manipulation helper function

--- Tests/Test_BSP.c
+++ Tests/Test_BSP.c
@@ -13,6 +13,14 @@
 #include "BSP_ADC.h"
 #include <bsp.h>
 
+/**
+ * Moves the cursor n lines
+ */
+static void moveCursorUp(int n) {
+    if (n < 1) n = 1;
+    if (n > 99) n = 99;
+    printf("%c[%dA", 0x1B, n);
+}
 
 int main() {
     while(1) {
@@ -25,7 +33,7 @@ int main() {
 
         BSP_CAN_Init(CAN_1);
         BSP_CAN_Init(CAN_2);
-        printf("Accelerator: %5.1d\tBrake: %5.1d\r", BSP_ADC_Get_Millivoltage(ADC_0), BSP_ADC_Get_Millivoltage(ADC_1));
+        printf("Accelerator: %5.1d\tBrake: %5.1d\n", BSP_ADC_Get_Millivoltage(ADC_0), BSP_ADC_Get_Millivoltage(ADC_1));
 
     }
 }
