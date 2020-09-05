1ee4be557951afcbb386658851edac97ca30a1e9
Sidharth Babu <sidharth.n.babu@utexas.edu>
2020-09-05 17:39:41 -0500

    removed extraneous comments

--- BSP/Inc/BSP_Lights.h
+++ BSP/Inc/BSP_Lights.h
@@ -1,12 +1,12 @@
 #ifndef __BSP_Lights_H
-#define __BSP_ADC_H
+#define __BSP_Lights_H
 
-#include "bsp.h"
+#include <bsp.h>
 #include "common.h"
 #include "config.h"
 
 typedef enum {M_CNCTR=0,RSVD_LED,CTRL_FAULT,RIGHT_BLINK,HeadlightPWR,A_CNCTR,BPS_FAULT,LEFT_BLINK,BPS_PWR,BrakeLight} LIGHT_t;
-//external: turn signals (left,right); brake lights; internal: 
+
 void BSP_Lights_Init();
 state_t BSP_Lights_Read(LIGHT_t LightChannel);
 
