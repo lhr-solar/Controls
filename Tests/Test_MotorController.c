aea5f5f95ad0509d8dbd038aa126f80e43a489bb
Sugam Arora <sugamarora@DESKTOP-EAPDOTJ.localdomain>
2020-09-05 11:56:16 -0500

    Changed Test file by initializing CANbuff data type and then passing reference to that object for MotorController_Read().

--- Tests/Test_MotorController.c
+++ Tests/Test_MotorController.c
@@ -8,13 +8,13 @@ int main(){
     uint32_t input1 = 0x5053;
     uint32_t input2 = 0x4056;
     MotorController_Drive(input1,input2);
-
-    CANbuff *tester;
+    
+    CANbuff tester = {0, 0, 0};
     uint32_t id = 0x243;
     uint8_t data[8] = {0x00, 0x55, 0x55, 0x22,     0x11, 0x11, 0x11, 0x11};
     BSP_CAN_Write(CAN_2, id, data, 8);
-    bool check = MotorController_Read(tester);
-    printf("The ID on the bus was: %x\n\rMessage: %x, %x\n\rSuccess: %d",tester->id, tester->firstNum, tester->secondNum, check); 
+    bool check = MotorController_Read(&tester);
+    printf("The ID on the bus was: %x\n\rMessage: %x, %x\n\rSuccess: %d",tester.id, tester.firstNum, tester.secondNum, check); 
     exit(0);
     
 }
