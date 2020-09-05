5d798c9dfa36c7323fdeff58d1a830b3aa4d2792
Erick Isaac Cortez Valdez <erick-cortez98@hotmail.com>
2020-09-05 15:34:02 -0500

    Displayboard (#36)
    * Add an (untested) compatible driver for communicating with the Gecko.
    
    Problem
    =======
    We need to be able to communicate with the Gecko board, which we are
    using to display information.
    
    Solution
    ========
    This adds a driver which uses USART2 to communicate with the Gecko
    board, using the same protocol as what should currently be running
    on that board.
    
    Notes
    =====
    This is untested as of the time of this commit. Please make sure to
    test this code before choosing to rely on it.
    
    Future Steps
    ============
    Also, I personally do not like this protocol, so if this needs to be
    extended in the future to accomodate other status information beyond
    speed, cruise control state, regen state, and CAN error indicators,
    then I would suggest changing from a string-based communication to a
    bitstream encoding the struct that stores all that information. This
    would require changing the code on the Gecko as well.
    
    * Added UART enum to account for two UART devices
    
    * Made UART_Read more flexible and updated UART tests
    
    * Modified UART write function to account for more than one UART devices
    
    * Modified Write function of UART
    
    * fixed small double declaration of same variable bugs in write function of UART file
    
    * Fixed UART tests
    
    * Fixed test
    
    * Fixed UART data not being written to csv correctly
    
    * minor change
    
    * Modified documentation of BSP_UART file due to changes in member functions and deleted file BSP_UART2 because it was integrated into BSP_UART
    
    * Removed extraneous imports and defintions, basic display test
    
    * Wrote test for display
    
    * Added the abilty to send UART messages from the GUI
    
    * Removed extraneous arguments, updated test
    
    Co-authored-by: Chase Block <chase@chaseblock.com>
    Co-authored-by: Cam0Cow <roie.roiegal@gmail.com>

---
+++ BSP/Simulator/Hardware/UART.py
@@ -0,0 +1,41 @@
+import csv
+import os
+import fcntl
+
+# path to the file
+file = "BSP/Simulator/Hardware/Data/UART.csv"
+
+
+def write(msg):
+    """ Writes the message to UART_2
+    Doesn't return anything
+    """
+
+    # Creates file if it doesn't exist
+    os.makedirs(os.path.dirname(file), exist_ok=True)
+    if not os.path.exists(file):
+        open(file, 'w')
+        file.close()
+    
+    lines = []
+    # Grab the current UART data
+    with open(file, "r") as csv:
+        fcntl.flock(csv.fileno(), fcntl.LOCK_EX)
+        lines = csv.readlines()
+
+    # If the file hasn't been initialzed yet, set the two entries to empty
+    length = len(lines)
+    if length < 2:
+        for i in range(length,2):
+            lines.append('\n')
+    
+    # Write back the UART data, modifying the specified one
+    with open(file, "w") as csv:
+        for (i, line) in enumerate(lines):
+            if i == 1:
+                csv.write(msg)
+                csv.write('\n')
+            else:
+                csv.write(line)
+
+        fcntl.flock(csv.fileno(), fcntl.LOCK_UN)
