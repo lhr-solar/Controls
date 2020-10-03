ae9a011d913880c8ccfd3cea1aa660ece2c5750d
Sidharth Babu <sidharth.n.babu@utexas.edu>
2020-10-03 15:55:15 -0500

    Added MotorControllerTest

--- Tests/Test_IntegratedDriverTest.c
+++ Tests/Test_IntegratedDriverTest.c
@@ -14,17 +14,25 @@
 int main() {
     //Init Pedals module
     Pedals_Init();
+
     //init MotorController 
     MotorController_Init();
+    CANbuff MotorControllerTest={0,0,0};
+
     //init Display
     Display_Init();
+
     //init Canbus
     CANbus_Init();
 
+    // MotorController Test
+    MotorController_Drive(10,10);
+    bool ReadCheck = MotorController_Read(&MotorControllerTest); 
+    printf("Message Read from Motor Controller Bus?: %d", MotorControllerTest);
+    printf("ID of CAN Msg: %x\n\rMessage: %x, %x",MotorControllerTest.id,MotorControllerTest.firstNum,MotorControllerTest.secondNum);
 
     while(1) {
         //Pedals test
         printf("Accelerator: %5.1d%%\tBrake: %5.1d%%\r",Pedals_Read(ACCELERATOR),Pedals_Read(BRAKE));
-        //
     }
 }
