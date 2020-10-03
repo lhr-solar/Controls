15495911b8a82ebcb00d82a2d8a82ddf4400fd15
Cam0Cow <roie.roiegal@gmail.com>
2020-10-03 14:48:53 -0500

    Display driver updated to new format

--- Drivers/Src/Display.c
+++ Drivers/Src/Display.c
@@ -1,7 +1,7 @@
 #include "bsp.h"
 #include "Display.h"
 
-#define GECKO_DATA_LENGTH 25
+#define GECKO_DATA_LENGTH 5
 
 /*
  * Initialize the Wonder Gecko communications.
@@ -12,24 +12,28 @@ void Display_Init() {
 
 /*
  * Updates the Gecko display with the data provided.
+ * @param status the new display data
+ * @return void
  */
 void Display_SetData(display_data_t *status) {
-    // The Gecko is expecting the code in string format
-    // TODO: this should be adjusted to something better,
-    //       since snprintf and sscanf add latencies on both MCUs
+
+    // The update gecko will expect five bytes: a float a some flags
+    // The float will be sent in little endian format, and the flags
+    // will be encoded in the order they're declared in the struct,
+    // starting at bit 3 and going til bit 0 of the fifth byte
     char msg[GECKO_DATA_LENGTH];
-    int rc = snprintf(msg, sizeof(msg), "%f,%d,%d,%d,%d\n",
-                      status->speed,
-                      status->cruiseEnabled,
-                      status->cruiseSet,
-                      status->regenEnabled,
-                      status->canError);
+    int flags = (status->cruiseEnabled << 3) |
+        (status->cruiseSet << 2) |
+        (status->regenEnabled << 1) |
+        status->canError;
 
-    // Make sure the string was written correctly
     #ifdef DEBUG
-    assert(rc > 0 && rc < GECKO_DATA_LENGTH);
+    assert(sizeof(float) == 4); // Just a sanity check
     #endif
+    memcpy(msg, &(status->speed), sizeof(float));
+    msg[GECKO_DATA_LENGTH-1] = flags;
 
-    // Send the string of info to the Gecko controller
-    BSP_UART_Write(UART_2, msg, rc);
+
+    // Send the bit vector of info to the Gecko controller
+    BSP_UART_Write(UART_2, msg, GECKO_DATA_LENGTH);
 }
