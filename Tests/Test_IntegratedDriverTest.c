2ae531b8baa3465b2201185df4bd6e4b7eb27149
Sidharth Babu <sidharth.n.babu@utexas.edu>
2020-10-03 13:11:37 -0500

    Initialized Drivers

--- Tests/Test_IntegratedDriverTest.c
+++ Tests/Test_IntegratedDriverTest.c
@@ -12,4 +12,19 @@
 
 
 int main() {
+    //Init Pedals module
+    Pedals_Init();
+    //init MotorController 
+    MotorController_Init();
+    //init Display
+    Display_Init();
+    //init Canbus
+    CANbus_Init();
+
+
+    while(1) {
+        //Pedals test
+        printf("Accelerator: %5.1d%%\tBrake: %5.1d%%\r",Pedals_Read(ACCELERATOR),Pedals_Read(BRAKE));
+        //
+    }
 }
