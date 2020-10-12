97d224ac7e86bfc54b45c5f0f7d42f031b7708f2
AleNeff <alejandro.neff@utexas.edu>
2020-10-11 20:41:13 -0500

    MotorController testing added (i think)

--- Tests/Test_AleFullTest.c
+++ Tests/Test_AleFullTest.c
@@ -2,6 +2,7 @@
 #include "config.h"
 #include "Pedals.h"
 #include "Display.h"
+#include "MotorController.h"
 #include <bsp.h>
 
 int main() {
@@ -20,7 +21,7 @@ int main() {
         // Pedal Test
         acceleratorPercentage = Pedals_Read(ACCELERATOR);
         brakePercentage = Pedals_Read(BRAKE);
-        printf("Accelerator: %5.1d%%,  Brake: %5.1d%%,", 
+        printf("Accelerator: %5.1d%%,  Brake: %5.1d%%\n", 
             acceleratorPercentage,brakePercentage);
         // Display Test
         // Speed
@@ -38,13 +39,24 @@ int main() {
         cruiseEnabled = BSP_Switches_Read(6);
         cruiseSet = BSP_Switches_Read(5);
         regenEnabled = BSP_Switches_Read(7);
-        printf("  CruiseEn: %d,  CruiseSet: %d,  regenEnabled: %d\r", cruiseEnabled, cruiseSet, regenEnabled);
+        printf("CruiseEn: %d,  CruiseSet: %d,  regenEnabled: %d\n", cruiseEnabled, cruiseSet, regenEnabled);
 
         Display_SetData(&packet);
 
         // Motor Controller
+        uint32_t input1 = 0x5053;
+        uint32_t input2 = 0x4056;
+        MotorController_Drive(input1,input2);
         
+        CANbuff tester = {0, 0, 0};
+        uint32_t id = 0x243;
+        uint8_t data[8] = {0x00, 0x55, 0x55, 0x22,     0x11, 0x11, 0x11, 0x11};
+        BSP_CAN_Write(CAN_2, id, data, 8);
+        bool check = MotorController_Read(&tester);
+        printf("BusID: %x\tMsg: %x, %x\tSuccess: %d\n",tester.id, tester.firstNum, tester.secondNum, check); 
 
+
+        system("clear");
 
     }
 }
