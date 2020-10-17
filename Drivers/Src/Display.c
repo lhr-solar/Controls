d977f1ea6686862b2a2f39eb8bbf33cd23e96ab7
Cam0Cow <roie.roiegal@gmail.com>
2020-10-17 11:58:29 -0500

    C code now uses new display format

--- Drivers/Src/Display.c
+++ Drivers/Src/Display.c
@@ -1,7 +1,20 @@
 #include "bsp.h"
 #include "Display.h"
 
-#define GECKO_DATA_LENGTH 5
+#define GECKO_DATA_LENGTH 5*2
+
+/*
+ * Encodes a byte into two hex digits and places them at buf
+ * @param byte the given data byte
+ * @param buf the output buffer
+ * @return void
+ */
+static void encodeByte(uint8_t byte, uint8_t *buf) {
+    uint8_t hi = (byte >> 4) & 0x0F;
+    uint8_t lo = byte & 0x0F;
+    buf[0] = hi < 10 ? (hi + '0') : (hi - 10 + 'A');
+    buf[1] = lo < 10 ? (lo + '0') : (lo - 10 + 'A');
+}
 
 /*
  * Initialize the Wonder Gecko communications.
@@ -17,7 +30,8 @@ void Display_Init() {
  */
 void Display_SetData(display_data_t *status) {
 
-    // The update gecko will expect five bytes: a float a some flags
+    // The update gecko will expect ten bytes: a float a some flags
+    // Each byte will be encoded as two hex digits
     // The float will be sent in little endian format, and the flags
     // will be encoded in the order they're declared in the struct,
     // starting at bit 3 and going til bit 0 of the fifth byte
@@ -30,8 +44,15 @@ void Display_SetData(display_data_t *status) {
     #ifdef DEBUG
     assert(sizeof(float) == 4); // Just a sanity check
     #endif
-    memcpy(msg, &(status->speed), sizeof(float));
-    msg[GECKO_DATA_LENGTH-1] = flags;
+
+    static union fi {float f; int i} data; // So we can interpret the float as a bitvector
+    data.f = status->speed;
+    for (int j=0; j<4; j++) {
+        encodeByte(data.i & 0xFF, msg+j*2);
+        data.i >>= 8; // shift the data down by a byte
+    }
+    
+    encodeByte((uint8_t) flags, msg+4*2);
 
 
     // Send the bit vector of info to the Gecko controller
