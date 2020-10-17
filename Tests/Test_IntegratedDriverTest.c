bf3c15318f62e55a4b58521df63371ec1b8a8607
Sidharth Babu <sidharth.n.babu@utexas.edu>
2020-10-17 15:15:26 -0500

    Made test loop infinitely; Changed test for motor disable to check canRead properly

--- Tests/Test_IntegratedDriverTest.c
+++ Tests/Test_IntegratedDriverTest.c
@@ -7,19 +7,32 @@
 #include "CANbus.h"
 
 int main() {
-
     //CAN Test
     CANbus_Init();
     uint32_t ids[10] = {0x242, 0x243, 0x244, 0x245, 0x246, 0x247, 0x24B, 0x24E, 0x580, 0x10A};
     uint8_t buffer[8];
     CANData_t data;
-    data.d = 0x87654321;
+    data.w = 0x87654321;
     CANPayload_t payload;
     payload.data = data;
     payload.bytes = 4;
+
     for(int i=0; i<sizeof(ids)/sizeof(ids[0]); i++){
         CANbus_Send(ids[i], payload);
-        printf("Sent ID: 0x%x - Success(1)/Failure(0): %d\n", ids[i], CANbus_Read(buffer));
+        ErrorStatus read = CANbus_Read(buffer);
+        printf("\nSent ID: 0x%x - Motor Disable Detected?: %d\n", ids[i], CANbus_Read(buffer));
+        //printf("Payload match send?(Success: 1, Fail: 0): %d", buffer[i]==payload.data.w);
+        if(read==SUCCESS){
+
+            printf("Payload/Buffer Match?: \n\r");
+            printf("BufferValues: \n");
+            for(int count=0;count<4;count++){
+                printf("%x",buffer[count]);
+            };
+            printf("\n");
+            printf("PayloadVal:\n%x\n",payload.data.w);
+        }
+
     }
     
     // Display init
@@ -33,24 +46,18 @@ int main() {
     //Init Pedals module
     Pedals_Init();
 
-
-    //display
-    
-    int count = 0;
-    while(count<10) {
-        //pedals
-        printf("Accelerator: %d\n\rBrake: %d\n\r",Pedals_Read(ACCELERATOR),Pedals_Read(BRAKE));
+    printf("\n\nAccelerator,    Brake,    MotorControllerBusRead?,    MCB msg ID,    MCB msg\n\n");
+    while (1){
         MotorController_Drive((rand()/500),(rand()/500));
-        bool ReadCheck = MotorController_Read(&MotorControllerTest); 
-        printf("Message Read from Motor Controller Bus?: %d\n\r", ReadCheck);
-        printf("ID of CAN Msg: %x\n\rMessage: %x, %x\n\r",MotorControllerTest.id,MotorControllerTest.firstNum,MotorControllerTest.secondNum);
+        bool check=MotorController_Read(&MotorControllerTest);
+
+
+        printf("%d\t%d\t%d\t%x\t%x,%x\r",Pedals_Read(ACCELERATOR),Pedals_Read(BRAKE),check,MotorControllerTest.id,MotorControllerTest.firstNum,MotorControllerTest.secondNum);
         packet.speed = (rand() % 500) / 10.0;
         packet.cruiseEnabled = rand() % 2;
         packet.cruiseSet = rand() % 2;
         packet.regenEnabled = rand() % 2;
         packet.canError = rand() % 10;
         Display_SetData(&packet);
-        count++;
     }
-    exit(0);
 }
