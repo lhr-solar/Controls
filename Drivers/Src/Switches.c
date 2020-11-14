e5ce69c4d6beb6d4294ed12f4d1730dcdf720a11
Sidharth Babu <sidharth.n.babu@utexas.edu>
2020-11-14 15:42:07 -0600

    Created Initialization function for switch driver

---
+++ Drivers/Src/Switches.c
@@ -0,0 +1,21 @@
+#include "Switches.h"
+
+void Switches_Init(void){
+    BSP_SPI_Init();
+    //Data Structure of SPI module is Opcode+RW,Register Address, then Data as 3 element byte array
+    //Read: 1, Write: 0
+    //1)Read IODIRA: Address x00
+    uint8_t initData[3]={0x41,0x00,0x00};
+    BSP_SPI_Write(initData,3);
+    uint8_t initReadData;
+    BSP_SPI_Read(initReadData,1);
+    // OR IODIRA with 0b01111111
+    initData[2] = initReadData|0x7F;
+    //Write result of OR to IODIRA
+    BSP_SPI_Write(initData,3);
+};
+State Switches_Read(switches_t sw){
+
+};
+
+//
