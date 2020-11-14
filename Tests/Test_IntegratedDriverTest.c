242117b690dbcc64a5546b044c7ef18cb61acc28
Sidharth Babu <sidharth.n.babu@utexas.edu>
2020-11-14 11:34:39 -0600

    Added Display test print out

--- Tests/Test_IntegratedDriverTest.c
+++ Tests/Test_IntegratedDriverTest.c
@@ -8,6 +8,7 @@
 
 int main() {
     //CAN Test
+    printf("Testing CAN module:");
     CANbus_Init();
     uint32_t ids[10] = {0x242, 0x243, 0x244, 0x245, 0x246, 0x247, 0x24B, 0x24E, 0x580, 0x10A};
     uint8_t buffer[8];
@@ -21,7 +22,6 @@ int main() {
         CANbus_Send(ids[i], payload);
         ErrorStatus read = CANbus_Read(buffer);
         printf("\nSent ID: 0x%x - Motor Disable Detected?: %d\n", ids[i], CANbus_Read(buffer));
-        //printf("Payload match send?(Success: 1, Fail: 0): %d", buffer[i]==payload.data.w);
         if(read==SUCCESS){
 
             printf("Payload/Buffer Match?: \n\r");
@@ -49,12 +49,13 @@ int main() {
     while (1){
         MotorController_Drive((rand()/500),(rand()/500));
         bool check=MotorController_Read(&MotorControllerTest);
-        printf("Accelerator: %d\t Brake:%d\t MotorControllerBusRead:%d\t MCB msg ID:%x\t MCBmsg: %x,%x\r",Pedals_Read(ACCELERATOR),Pedals_Read(BRAKE),check,MotorControllerTest.id,MotorControllerTest.firstNum,MotorControllerTest.secondNum);
+        printf("Accelerator: %d, Brake:%d, MotorControllerBusRead:%d, MCB msg ID:%x, MCBmsg: %x,%x\r",Pedals_Read(ACCELERATOR),Pedals_Read(BRAKE),check,MotorControllerTest.id,MotorControllerTest.firstNum,MotorControllerTest.secondNum);
         packet.speed = (rand() % 500) / 10.0;
         packet.cruiseEnabled = rand() % 2;
         packet.cruiseSet = rand() % 2;
         packet.regenEnabled = rand() % 2;
         packet.canError = rand() % 10;
         Display_SetData(&packet);
+        printf("||Display Test: Speed=%f, cruiseEnabled=%d, cruiseSet=%d, regenEnabled=%d, canError=%d,|| ",packet.speed,packet.cruiseEnabled,packet.regenEnabled,packet.canError);
     }
 }
