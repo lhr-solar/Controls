f5df49ccf0930639aa58c882289fd6b08def23bb
Rishi Ponnekanti <51857775+JimothyGreene@users.noreply.github.com>
2020-11-28 10:08:24 -0600

    Adds try/catch blocks in simulator to handle exceptions gracefully (#90)

--- BSP/Simulator/Hardware/Display.py
+++ BSP/Simulator/Hardware/Display.py
@@ -10,11 +10,11 @@ file = "BSP/Simulator/Hardware/Data/UART.csv"
 
 # Data
 data = {
-    "speed" : 0,
-    "cruise_control_en" : 0,
-    "cruise_control_set" : 0,
-    "regen_en" : 0,
-    "CAN_err" : 0
+    "speed": 0,
+    "cruise_control_en": 0,
+    "cruise_control_set": 0,
+    "regen_en": 0,
+    "CAN_err": 0,
 }
 
 
@@ -32,15 +32,15 @@ def read():
 
     Returns:
         dict: dictionary with filled in values
-    """    
+    """
     global data
     # Creates file if it doesn't exist
     os.makedirs(os.path.dirname(file), exist_ok=True)
     if not os.path.exists(file):
-        with open(file, 'w'):
+        with open(file, "w"):
             pass
     uart = list()
-    with open(file, 'r') as csvfile:
+    with open(file, "r") as csvfile:
         fcntl.flock(csvfile.fileno(), fcntl.LOCK_EX)
         csvreader = csv.reader(csvfile)
         for row in csvreader:
@@ -50,14 +50,24 @@ def read():
         uart = uart[0][0]
         speed_str = uart[0:8]
 
-        speed_arr = [int(uart[0:2], 16), int(uart[2:4], 16), int(uart[4:6], 16), int(uart[6:8], 16)]
-        (speed,) = struct.unpack("<f", bytearray(speed_arr)) # Convert our byte string to a float
-        flags_str = uart[8:10]
-        flags = int(flags_str, 16)
-        
-        data["speed"] = round(speed, 2) # Otherwise we get too many decimal places
-        data["cruise_control_en"] = (flags >> 3) & 1
-        data["cruise_control_set"] = (flags >> 2) & 1
-        data["regen_en"] = (flags >> 1) & 1
-        data["CAN_err"] = (flags) & 1
+        try:
+            speed_arr = [
+                int(uart[0:2], 16),
+                int(uart[2:4], 16),
+                int(uart[4:6], 16),
+                int(uart[6:8], 16),
+            ]
+            (speed,) = struct.unpack(
+                "<f", bytearray(speed_arr)
+            )  # Convert our byte string to a float
+            flags_str = uart[8:10]
+            flags = int(flags_str, 16)
+
+            data["speed"] = round(speed, 2)  # Otherwise we get too many decimal places
+            data["cruise_control_en"] = (flags >> 3) & 1
+            data["cruise_control_set"] = (flags >> 2) & 1
+            data["regen_en"] = (flags >> 1) & 1
+            data["CAN_err"] = (flags) & 1
+        except Exception:
+            pass
     return data
--- BSP/Simulator/Hardware/MotorController.py
+++ BSP/Simulator/Hardware/MotorController.py
@@ -11,26 +11,28 @@ file = "BSP/Simulator/Hardware/Data/CAN.csv"
 
 
 def sendTouC():
-    """Simulates motors sending CAN messages back to the uC 
-        
-        Returns:
-        CAN ID of the last message sent
+    """Simulates motors sending CAN messages back to the uC
+
+    Returns:
+    CAN ID of the last message sent
     """
 
     currentID = CarState.motor_info[CarState.infoIdx]
 
     if currentID == CarState.VELOCITY_ID:
         tx_message = int(CarState.CURRENT_VELOCITY)
-        tx_message = (tx_message << 32) + int((60 * CarState.CURRENT_VELOCITY)/(math.pi * 2 * CarState.radius))
-    
+        tx_message = (tx_message << 32) + int(
+            (60 * CarState.CURRENT_VELOCITY) / (math.pi * 2 * CarState.radius)
+        )
+
     elif currentID == CarState.MOTOR_BUS_ID:
         tx_message = int(CarState.ABSOLUTE_CURRENT)
         tx_message = (tx_message << 32) + int(CarState.BUS_VOLTAGE)
-    
+
     elif currentID == CarState.MOTOR_PHASE_CURRENT_ID:
         tx_message = int(CarState.PHASE_C_CUR)
         tx_message = (tx_message << 32) + int(CarState.PHASE_B_CUR)
-    
+
     elif currentID == CarState.MOTOR_VOLTAGE_VECTOR_ID:
         tx_message = int(CarState.V_REAL)
         tx_message = (tx_message << 32) + int(CarState.V_IMAGINE)
@@ -47,33 +49,19 @@ def sendTouC():
         tx_message = int(CarState.PHASEC_TEMP)
         tx_message = (tx_message << 32) + int(CarState.INTERNAL_TEMP)
 
-    
     write(currentID, tx_message)
-    CarState.infoIdx = (CarState.infoIdx + 1) %  7   #increment index
-    return currentID
+    CarState.infoIdx = (CarState.infoIdx + 1) % 7  # increment index
+    CarState.last_message = [hex(currentID), hex(tx_message)]
+    return CarState.last_message
 
 
 def read():
-    """Reads CAN2 bus
+    """Reads last message from CAN2 bus
 
     Returns:
-        list: [ID, Message, Length]
+        list: [ID, Message]
     """
-    # Creates file if it doesn't exist
-    os.makedirs(os.path.dirname(file), exist_ok=True)
-    if not os.path.exists(file):
-        with open(file, 'w'):
-            pass
-    message = []
-    with open(file, 'r') as csvfile:
-        fcntl.flock(csvfile.fileno(), fcntl.LOCK_EX)
-        csvreader = csv.reader(csvfile)
-        for row in csvreader:
-            message.append(row)
-        fcntl.flock(csvfile.fileno(), fcntl.LOCK_UN)
-    message = [0, 0] if message == [] else message[1]
-    canId = sendTouC()
-    return message
+    return CarState.last_message
 
 
 def write(id_, message):
@@ -82,27 +70,14 @@ def write(id_, message):
     Args:
         id_ (int): ID of message to be sent
         message (int): contents of message to be sent
-    """    
+    """
     CAN1 = CAN.read()
-    with open(file, 'w') as csvfile:
+    with open(file, "w") as csvfile:
         fcntl.flock(csvfile.fileno(), fcntl.LOCK_EX)
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(CAN1)
-        csvwriter.writerow([hex(id_), hex(message), len(hex(message)[2:])//2])
+        csvwriter.writerow([hex(id_), hex(message), len(hex(message)[2:]) // 2])
         fcntl.flock(csvfile.fileno(), fcntl.LOCK_UN)
-
-
-def confirm_power():
-    try:
-        id_, message = read()
-        id_ = int(id_, 16)
-        message = int(message, 16)
-        if id_ == CarState.MOTOR_POWER_ID:
-            # Read the message and separate
-            desired_current = (message >> 32) & 0xFFFFFFFF
-            CarState.MAX_CURRENT = CarState.ABSOLUTE_CURRENT * (desired_current/100.0)
-    except ValueError:
-        pass
 
 
 def confirm_drive():
@@ -114,7 +89,7 @@ def confirm_drive():
     Returns:
         tuple: desired and current velocities to display
     """
-    try:      
+    try:
         id_, message, _ = read()
         id_ = int(id_, 16)
         message = int(message, 16)
@@ -122,20 +97,25 @@ def confirm_drive():
             # Read the message and separate
             desired_current = (message >> 32) & 0xFFFFFFFF
             desired_velocity = message & 0xFFFFFFFF
-            toggle_torque(desired_velocity)     #enable torque control mode if desired_velocity is an extremely large number
-            #update max available current value
+            toggle_torque(
+                desired_velocity
+            )  # enable torque control mode if desired_velocity is an extremely large number
+            # update max available current value
             if CarState.mode != 1:
-                CarState.CURRENT_SETPOINT = CarState.MAX_CURRENT * (desired_current/100.0)
+                CarState.CURRENT_SETPOINT = CarState.MAX_CURRENT * (
+                    desired_current / 100.0
+                )
             update_velocity(desired_velocity)
             # Write the current velocity value
-            tx_message = int(CarState.CURRENT_VELOCITY) << 32 + int(CarState.CURRENT_VELOCITY)
+            tx_message = int(CarState.CURRENT_VELOCITY) << 32 + int(
+                CarState.CURRENT_VELOCITY
+            )
             write(CarState.VELOCITY_ID, tx_message)
             return desired_velocity, CarState.CURRENT_VELOCITY
         else:
             return CarState.CURRENT_VELOCITY, CarState.CURRENT_VELOCITY
     except ValueError:
         return CarState.CURRENT_VELOCITY, CarState.CURRENT_VELOCITY
-
 
 
 def toggle_torque(velocity):
@@ -145,12 +125,13 @@ def toggle_torque(velocity):
         CarState.mode = 0
 
 
-
 def torque_control(pedalPercent):
-    #following code will only execute if motor is in torque control mode
+    # following code will only execute if motor is in torque control mode
     if CarState.mode == 1:
-        CarState.CURRENT_SETPOINT = pedalPercent * CarState.MAX_CURRENT    #param will be a value from 0.0 to 1.0
-        CarState.velocity_increase = CarState.CURRENT_SETPOINT     #update rate
+        CarState.CURRENT_SETPOINT = (
+            pedalPercent * CarState.MAX_CURRENT
+        )  # param will be a value from 0.0 to 1.0
+        CarState.velocity_increase = CarState.CURRENT_SETPOINT  # update rate
 
 
 def update_velocity(v):
@@ -180,10 +161,21 @@ class CarState:
     MOTOR_BACKEMF_ID = 0x247
     MOTOR_TEMP_ID = 0x24B
 
-    motor_info = [MOTOR_BUS_ID, VELOCITY_ID, MOTOR_PHASE_CURRENT_ID, MOTOR_VOLTAGE_VECTOR_ID, MOTOR_CURRENT_VECTOR_ID, MOTOR_BACKEMF_ID, MOTOR_TEMP_ID]
+    motor_info = [
+        MOTOR_BUS_ID,
+        VELOCITY_ID,
+        MOTOR_PHASE_CURRENT_ID,
+        MOTOR_VOLTAGE_VECTOR_ID,
+        MOTOR_CURRENT_VECTOR_ID,
+        MOTOR_BACKEMF_ID,
+        MOTOR_TEMP_ID,
+    ]
+
+    # Last message
+    last_message = None
 
     # Index
-    infoIdx = 0 
+    infoIdx = 0
 
     radius = 21.67  # in inches
 
@@ -193,34 +185,35 @@ class CarState:
     BUS_VOLTAGE = 5
 
     # Phase Currents
-    PHASE_C_CUR = 1 
+    PHASE_C_CUR = 1
     PHASE_B_CUR = 1
 
     # Voltage Vectors
     V_REAL = 0
     V_IMAGINE = 0
 
-    #Current Vectors
+    # Current Vectors
     I_REAL = 0
     I_IMAGINE = 0
 
-    #Motor BackEMF components
+    # Motor BackEMF components
     REAL_COMP = 0
     NEUTRAL_MOTOR = 0
 
-    #Temperature
+    # Temperature
     PHASEC_TEMP = 0
     INTERNAL_TEMP = 0
 
-    mode = 0    # 0 = Velocity control mode, 1 = Torque control mode
+    mode = 0  # 0 = Velocity control mode, 1 = Torque control mode
 
-    velocity_increase = 0.5 #how much CURRENT_VELOCITY is increased by in update_velocity()
+    velocity_increase = (
+        0.5  # how much CURRENT_VELOCITY is increased by in update_velocity()
+    )
 
-    CURRENT_SETPOINT = 0.0 #set by user via MOTOR_DRIVE commands
+    CURRENT_SETPOINT = 0.0  # set by user via MOTOR_DRIVE commands
 
-    MAX_CURRENT = 1.0 #max available current, this is a percent of the Absolute bus current
+    MAX_CURRENT = (
+        1.0  # max available current, this is a percent of the Absolute bus current
+    )
 
-    ABSOLUTE_CURRENT = 5.0  #physical limitation
-    
-
-
+    ABSOLUTE_CURRENT = 5.0  # physical limitation
