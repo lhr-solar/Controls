236c2f5cb7e4a3ba0525584818869e0784adaaa0
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2020-11-28 10:37:38 -0600

    Updates BSP GPIO to use ports instead of pins. Briefly tested and seems to work propelry

--- BSP/Inc/BSP_GPIO.h
+++ BSP/Inc/BSP_GPIO.h
@@ -1,3 +1,10 @@
+/* Copyright (c) 2020 UT Longhorn Racing Solar */
+
+/**
+ * Header file for the library to interact
+ * with the GPIO ports
+ */
+
 #ifndef __BSP_GPIO_H
 #define __BSP_GPIO_H
 
@@ -5,35 +12,28 @@
 #include "config.h"
 #include <bsp.h>
 
-typedef enum {PA0 = 0, PA2, PA3, PB0, PB1, PB2, PB4, PB5, PB6, PB7, PB8, PB14, PB15} Pin; 
-
-#define GPIOSIZE 13
+typedef enum {PORTA = 0, PORTB, NUM_PORTS} port_t; 
 
 /**
- * @brief   Confirms that the CSV file
- *          has been created and throws
- *          an error if not
- * @param   None
+ * @brief   Initializes a GPIO port
+ * @param   port to initialize
  * @return  None
  */ 
-void BSP_GPIO_Init(void);
+void BSP_GPIO_Init(port_t port);
 
 /**
- * @brief   
- * 
- * @param   
- * 
- * @return  
+ * @brief   Reads value of the specified port
+ * @param   port to read
+ * @return  data of the port
  */ 
-int BSP_GPIO_Read(Pin pn);
+uint16_t BSP_GPIO_Read(port_t port);
 
 /**
- * @brief   
- * 
- * @param   
- * 
- * @return  
+ * @brief   Writes data to a specified port 
+ * @param   port to write to
+ * @param   data to write 
+ * @return  None
  */ 
-int BSP_GPIO_Write(Pin pn, int data);
+void BSP_GPIO_Write(port_t port, uint16_t data);
 
 #endif
--- BSP/Simulator/Src/BSP_GPIO.c
+++ BSP/Simulator/Src/BSP_GPIO.c
@@ -1,3 +1,9 @@
+/* Copyright (c) 2020 UT Longhorn Racing Solar */
+
+/**
+ * Source file to mimic GPIO port communication
+ */ 
+
 #include "BSP_GPIO.h"
 
 #define FILE_NAME DATA_PATH(GPIO_CSV)
@@ -6,10 +12,10 @@
  * @brief   Confirms that the CSV file
  *          has been created and throws
  *          an error if not
- * @param   None
+ * @param   port to initialize
  * @return  None
  */ 
-void BSP_GPIO_Init(void) {
+void BSP_GPIO_Init(port_t port) {
     if (access(FILE_NAME, F_OK) != 0) {
         // File doesn't exist if true
         perror(GPIO_CSV);
@@ -17,42 +23,47 @@ void BSP_GPIO_Init(void) {
     }
 }
 
-int BSP_GPIO_Read(Pin pn){
+/**
+ * @brief   Reads value of the line of
+ *          the CSV file
+ * @param   port to read
+ * @return  data of the port
+ */ 
+uint16_t BSP_GPIO_Read(port_t port) {
     FILE* fp = fopen(FILE_NAME, "r");
-    if(!fp){
-        perror("GPIO file was not found\n");
-        exit(EXIT_FAILURE);
+    if (!fp) {
+        printf("GPIO not available\n\r");
+        return 0;
     }
     
     // Lock file
     int fno = fileno(fp);
     flock(fno, LOCK_EX);
 
-    int csvContents[GPIOSIZE];
+    uint16_t csvContents[NUM_PORTS] = {0};
 
-    for(int i = 0; i < GPIOSIZE; i++){
-        int tok;
-        fscanf(fp, "%d", &tok);
-        if(tok){
-            csvContents[i] = tok;
-        } else {
-            csvContents[i] = -1;
-        }
+    int tok;
+    for (int i = 0; i < NUM_PORTS && fscanf(fp, "%d", &tok); i++) {
+        csvContents[i] = tok;
     }
 
-    // for(int i = 0; i < GPIOSIZE; i++){
-    //     printf("%d\n", csvContents[i]);
-    // }
-
+    // Unlock file
+    flock(fno, LOCK_UN);
     fclose(fp);
 
-    return csvContents[pn];
+    return csvContents[port];
 }
 
-int BSP_GPIO_Write(Pin pn, int data){
+/**
+ * @brief   Writes data to a line in CSV fle
+ * @param   port to write to
+ * @param   data to write 
+ * @return  None
+ */ 
+void BSP_GPIO_Write(port_t port, uint16_t data) {
     FILE* fp = fopen(FILE_NAME, "r");
-    if(!fp){
-        perror("GPIO file was not found\n");
+    if (!fp) {
+        perror("GPIO not available\n\r");
         exit(EXIT_FAILURE);
     }
     
@@ -60,9 +71,9 @@ int BSP_GPIO_Write(Pin pn, int data){
     int fno = fileno(fp);
     flock(fno, LOCK_EX);
 
-    int csvContents[GPIOSIZE];
+    int csvContents[NUM_PORTS];
 
-    for(int i = 0; i < GPIOSIZE; i++){
+    for(int i = 0; i < NUM_PORTS; i++){
         int tok;
         fscanf(fp, "%d", &tok);
         if(tok){
@@ -72,20 +83,15 @@ int BSP_GPIO_Write(Pin pn, int data){
         }
     }
 
-    csvContents[pn] = data;
+    csvContents[port] = data;
 
     freopen(FILE_NAME, "w", fp);
 
-    for(int i = 0; i < GPIOSIZE; i++){
+    for(int i = 0; i < NUM_PORTS; i++){
         fprintf(fp, "%d\n", csvContents[i]);
     }
 
-    // for(int i = 0; i < GPIOSIZE; i++){
-    //     printf("%d\n", csvContents[i]);
-    // }
-
+    // Unlock file
+    flock(fno, LOCK_UN);
     fclose(fp);
-
-    return 1;
-
 }
