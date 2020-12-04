5ad6fda03018d3e00c1f4fbca5e133ce6194cf14
Sidharth Babu <sidharth.n.babu@utexas.edu>
2020-12-03 23:36:51 -0600

    added doc comments

--- Drivers/Src/Switches.c
+++ Drivers/Src/Switches.c
@@ -2,6 +2,13 @@
 
 //Data Structure of SPI module is Opcode+RW,Register Address, then Data as 3 element byte array
 //Readwrite bit = Read: 1, Write: 0
+
+/**
+ * @brief   Initializes all switches
+ *          from the steering wheel
+ * @param   None
+ * @return  None
+ */ 
 void Switches_Init(void){
     BSP_SPI_Init();
     //1)Read IODIRA: Address x00
@@ -16,6 +23,12 @@ void Switches_Init(void){
     BSP_SPI_Write(initData,3);
 };
 
+/**
+ * @brief   Writes to SPI with Write Bit set (no change allowed) to query GPIOA Register (x09), 
+ * reads it back using SPI, isolates for input switch using AND mask and returns state 
+ * @param   sw
+ * @return  ON/OFF State
+ */ 
 State Switches_Read(switches_t sw){
     uint8_t query[2]={0x41,0x09}; //query GPIOA
     BSP_SPI_Write(query,2);
