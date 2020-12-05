29e7e43863ea0d2606276f5bb1b897bf49ccc7b8
Sidharth Babu <sidharth.n.babu@utexas.edu>
2020-12-04 23:46:21 -0600

    simplified test to figure out SPI bug

--- Tests/Test_Switches.c
+++ Tests/Test_Switches.c
@@ -7,16 +7,16 @@ int main() {
     printf("CRUZ_st\tCRUZ_EN\tHZD_SQ\tFR_SW\tHeadlight_SW\tLEFT_SW\tRIGHT_SW\t\n");
     
     while(1){
-        printf("%d", Switches_Read(LEFT_SW));
-        //printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\r",
-            //Switches_Read(CRUZ_st),
-            // Switches_Read(CRUZ_EN),
-            // Switches_Read(HZD_SQ),
-            // Switches_Read(FR_SW),
-        //     Switches_Read(Headlight_SW),
-        //     Switches_Read(LEFT_SW),
-        //     Switches_Read(RIGHT_SW));
-         //printf("inside loop\r");
+        //printf("%d\r", Switches_Read(LEFT_SW));
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\r",
+            Switches_Read(CRUZ_st),
+            Switches_Read(CRUZ_EN),
+            Switches_Read(HZD_SQ),
+            Switches_Read(FR_SW),
+            Switches_Read(Headlight_SW),
+            Switches_Read(LEFT_SW),
+            Switches_Read(RIGHT_SW));
+         printf("inside loop\r");
     }
     printf("\n");
 }
