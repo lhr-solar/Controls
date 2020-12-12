07e660513cf13c34b5b9cf7ec567fc5df72c65ee
Cam0Cow <roie.roiegal@gmail.com>
2020-12-12 11:16:43 -0600

    Simulator UART can now handle non-printable characters

--- BSP/Simulator/Src/BSP_UART.c
+++ BSP/Simulator/Src/BSP_UART.c
@@ -13,6 +13,41 @@
 #define TX_SIZE 128
 
 /**
+ * Encode the string so it's always printable
+ * @param in input string
+ * @param out output string
+ * @return void
+ * Note that strlen(output) == 2 * strlen(input), so ensure correct sizing
+ */
+static void convertTo(const char *in, char *out) {
+    int len = strlen(in);
+    for (int i=0; i<len; i++) {
+        int hi = in[i] >> 4;
+        int lo = in[i] & 0xf;
+        out[i*2  ] = hi > 9 ? hi + 87 : hi + 48;
+        out[i*2+1] = lo > 9 ? lo + 87 : lo + 48;
+    }
+    out[len*2] = '\0';
+}
+
+/**
+ * Recover an encoded string
+ * @param in the encoded string
+ * @param out where to write the normal string
+ * Note that strlen(input) == 2 * strlen(output)
+ */
+static void convertFrom(const char *in, char *out) {
+    int len = strlen(in) / 2;
+    for (int i=0; i<len; i++) {
+        int hi = in[i*2  ] > 96 ? in[i*2  ] - 87 : in[i*2  ] - 48;
+        int lo = in[i*2+1] > 96 ? in[i*2+1] - 87 : in[i*2+1] - 48; 
+
+        out[i] = (hi << 4) | lo;
+    }
+    out[len] = '\0';
+}
+
+/**
  * @brief   Confirms that the CSV file
  *          has been created and throws
  *          an error if not
@@ -48,11 +83,11 @@ uint32_t BSP_UART_Read(UART_t uart, char* str) {
     flock(fno, LOCK_EX);
 
     // Get raw CSV strings
-    char csv[NUM_UART][RX_SIZE];
+    char csv[NUM_UART][RX_SIZE*2];
     for (int i=0; fgets(csv[i], RX_SIZE, fp); i++);
 
     // Put string into return buffer
-    strcpy(str, csv[uart]);
+    convertFrom(csv[uart], str);
 
     // Unlock file
     flock(fno, LOCK_UN);
@@ -83,12 +118,15 @@ uint32_t BSP_UART_Write(UART_t uart, char* str, uint32_t len) {
     flock(fno, LOCK_EX);
 
     // Copying current contents
-    char currentUART[NUM_UART][TX_SIZE];
-    char csv[TX_SIZE];
+    char currentUART[NUM_UART][TX_SIZE*2];
+    char csv[TX_SIZE*2];
 
     for(uint8_t i = 0; fgets(csv, TX_SIZE, fp); i++){
         strcpy(currentUART[i], csv);
     }
+
+    // Put the converted string in variable for later
+    convertTo(str, csv);
 
     // Close file
     flock(fno, LOCK_UN);
@@ -104,7 +142,7 @@ uint32_t BSP_UART_Write(UART_t uart, char* str, uint32_t len) {
     // Write to the file
     for(uint8_t i = 0; i < NUM_UART; i++){
         if(uart == i){
-            fprintf(fp, "%s\n", str);
+            fprintf(fp, "%s\n", csv); // write the converted string
         }else{
             fprintf(fp, "%s", currentUART[i]);
         }
