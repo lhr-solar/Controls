37b347d29cc98c5fbbe948bc4b67f73ce730d678
Sidharth Babu <sidharth.n.babu@utexas.edu>
2020-12-19 16:48:57 -0600

    Fixed GPIOA taking more bits than allowed; added GPIOB handling for Reverse Switch; Added GPIO Port handling for ignition switches; Fixed button not toggling error

--- BSP/Simulator/Hardware/Switches.py
+++ BSP/Simulator/Hardware/Switches.py
@@ -10,7 +10,7 @@ from ShiftRegister import reg
 file = "BSP/Simulator/Hardware/Data/GPIO.csv"
 
 # Names of switches
-switches = ["CRUZ_ST", "CRUZ_EN", "HZD_SQ", "FR_SW", "Headlight_SW", "LEFT_SW", "RIGHT_SW", "REGEN", "IGN_1", "IGN_2"]
+switches = ["CRUZ_ST", "CRUZ_EN", "HZD_SQ", "FR_SW", "Headlight_SW", "LEFT_SW", "RIGHT_SW", "REGEN", "IGN_1", "IGN_2","REV_SW"]
 
 
 def get_switches():
@@ -19,14 +19,10 @@ def get_switches():
 
 def toggle(switch, gui):
     """Toggles a specified switch state
-
     Args:
         switch (string): name of the switch to toggle
         gui (tk.Button list): Button object list
     """
-
-    #needs to be updated to use ShiftRegister register_write() function (bank=1 but if this throws error, check SPI_init)
-
     states = read()
     for i, sw in enumerate(switches):
         if sw == switch:
@@ -38,23 +34,32 @@ def toggle(switch, gui):
             if sw == "IGN_2" and states & 1<<i:
                 states |= 1<<(i-1)
                 gui[i-1].config(relief=tk.SUNKEN)
-    #check the length of the states variable; if its 9 bits, then ign1 is on, if its 10, ign2 is on, and if its less than 10 neither is on
+    #check the length of the states variable; if its 9 bits, then ign1 is on, if its 10, ign2 is on, if its 11 Reverse is on and if its less than 10 none are on
     if len(bin(states)[2:]) > 8:
-        #print("inside if statement")
-        #print("states" + str(states))
+        revState = (states & 1<<10)>>10
         ignStates = states & 3<<8
-        
-        # Deleting ignition switches from state 
-        #states = states & 0b0011111111
-
-        #print("ignStates"  + str(ignStates))
         writeGPIO(ignStates)
+        reg.REGISTER_DATA["GPIOB"] = reg.REGISTER_DATA["GPIOB"]|revState<<7
+        reg.REGISTER_DATA["GPIOA"]=states & 0x0FF
     else: 
         writeGPIO(0)
-    # set both ignitions to 0
+        reg.REGISTER_DATA["GPIOB"] = reg.REGISTER_DATA["GPIOB"] & 0x7F
+        reg.REGISTER_DATA["GPIOA"]=states
+   
     
-    #set Register Data
-    reg.REGISTER_DATA["GPIOA"]=states
+def read():
+    """Reads switch states
+
+    Returns:
+        int: bit string of switch states
+    """
+    # should be updated to also pull GPIO for Ignition
+    gpioStates = readGPIO()
+    states = reg.REGISTER_DATA["GPIOA"]
+    revState = (reg.REGISTER_DATA["GPIOB"] & 0x80)>>7
+    states = states|revState<<10
+    states = states|gpioStates<<8
+    return states
 
 def writeGPIO(ignStates):
     # Creates file if it doesn't exist
@@ -64,7 +69,7 @@ def writeGPIO(ignStates):
             pass
     
     lines = []
-    # Grab the current CAN data
+    # Grab the current GPIO data
     with open(file, "r") as csv:
         fcntl.flock(csv.fileno(), fcntl.LOCK_EX)
         lines = csv.readlines()
@@ -75,9 +80,7 @@ def writeGPIO(ignStates):
         for i in range(length,2):
             lines.append('\n')
     
-    # Write back the CAN data, modifying the specified one
-    #CANtext = "%s,%s,%d" % (id, msg, round((len(msg)-2)/2.0+0.1))
-    #CANtext = (id + ', ' + msg ', ' + (str)(len(msg)))
+
     with open(file, "w") as csv:
         for (i, line) in enumerate(lines):
             if i == 0:
@@ -88,13 +91,15 @@ def writeGPIO(ignStates):
 
         fcntl.flock(csv.fileno(), fcntl.LOCK_UN)
 
-
-def read():
-    """Reads switch states
-
-    Returns:
-        int: bit string of switch states
-    """
-    # should be updated to use ShiftRegister.py register_read function where Bank is set to 1 (if this throws an error check the SPI_init)
-    states = reg.REGISTER_DATA["GPIOA"]
-    return states
+def readGPIO():
+    os.makedirs(os.path.dirname(file), exist_ok=True)
+    if not os.path.exists(file):
+        with open(file, 'w'):
+            pass
+    
+    lines = []
+    # Grab the current GPIO data
+    with open(file, "r") as csv:
+        fcntl.flock(csv.fileno(), fcntl.LOCK_EX)
+        lines = csv.readlines()
+    return int(lines[0])
