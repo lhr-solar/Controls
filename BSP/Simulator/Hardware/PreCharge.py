efd66e8d47aae4b448e6871c680529e8dcbe1250
Cam0Cow <roie.roiegal@gmail.com>
2020-12-19 14:09:44 -0600

    GPIO read from correct line
    PreCharge.py now reads the precharge status from the correct line.
    Deleted the write function in PreCharge.py as it is not needed.

--- BSP/Simulator/Hardware/PreCharge.py
+++ BSP/Simulator/Hardware/PreCharge.py
@@ -5,26 +5,6 @@ import csv
 import fcntl
  
 file = "BSP/Simulator/Hardware/Data/GPIO.csv";
-
-def write(data, port=2):
-    os.makedirs(os.path.dirname(file), exist_ok = True);
-    if not os.path.exists(file):
-        with open(file, 'w'):
-            pass
-
-    with open(file, 'r') as csvfile:
-        fcntl.flock(csvfile.fileno(), fcntl.LOCK_EX);
-        lines = csvfile.readlines()
-    
-    try:
-        lines[port-1] = str(data) + '\n'
-        
-    except IndexError:
-        pass
-
-    with open(file, "w") as csvfile:
-        csvfile.writelines(lines)
-        fcntl.flock(csv.fileno(), fcntl.LOCK_UN)
     
 def read():
     os.makedirs(os.path.dirname(file), exist_ok = True);
@@ -37,11 +17,13 @@ def read():
         fcntl.flock(csvfile.fileno(), fcntl.LOCK_EX);
         try:
             csvreader = csv.reader(csvfile);
+            next(csvreader) # skip first line
+            next(csvreader) # skip second line
             charges = next(csvreader);
         except StopIteration:
             charges = [0];
         fcntl.flock(csvfile.fileno(), fcntl.LOCK_UN);
         states = [0, 0]
         states[0] = (int(charges[0]) % 2)
-        states[1] = (int(charges[0])>>1 % 2)
+        states[1] = ((int(charges[0])>>3) % 2)
         return states
