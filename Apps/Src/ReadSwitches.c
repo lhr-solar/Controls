11e33bbc2bd1d61c1b2f8542afc430594274eb57
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-01-30 10:32:19 -0600

    Added Initial skeleton to post to semaphores

--- Apps/Src/ReadSwitches.c
+++ Apps/Src/ReadSwitches.c
@@ -1,4 +1,5 @@
-#include "Tasks.h"
+//#include "Tasks.h"
+#include <os.h>
 
 // Needs to Read the Switches states (Driver level switches code) and trigger appropriate RTOS events
 // Needs to Signal:
@@ -17,12 +18,29 @@
 */
 void Task_ReadSwitches (void* p_arg) {
     Switches_Init();
+    OS_ERR err;
     while(1){
        if (Switches_Read(IGN_2)) {
            //Should post to both ActivateMotor and ActivateArray
+           OSSemSet(
+              (OS_SEM) &ActivateMotor_Sem4,
+               (OS_SEM_CTR) 1,
+               (OS_ERR*)&err
+           );
+           OSSemSet(
+               (OS_SEM) &ActivateArray_Sem4,
+               (OS_SEM_CTR) 1,
+               (OS_ERR*)&err
+           );
        } else if (Switches_Read(IGN_1)) {
            //Should post to only ActivateArray and not ActivateMotor
+           OSSemSet(
+               (OS_SEM) &ActivateArray_Sem4,
+               (OS_SEM_CTR) 1,
+               (OS_ERR*)&err
+           );
        }
        //Check other switches
+        
     }
 }
