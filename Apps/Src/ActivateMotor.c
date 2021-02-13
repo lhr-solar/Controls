472e7b3961c3688dc80d5da381846ad222bae1dc
Cam0Cow <roie.roiegal@gmail.com>
2021-02-13 15:16:45 -0600

    Updated motor connection task

--- Apps/Src/ActivateMotor.c
+++ Apps/Src/ActivateMotor.c
@@ -6,10 +6,6 @@ void Task_MotorConnection(void *p_arg) {
     (void) p_arg;
 
     OS_ERR err;
-    CPU_TS  ts;
-
-    OSSemPend(&ActivateMotor_Sem4, 0, OS_OPT_PEND_BLOCKING, &ts, &err);
-    // TODO: check for errors
 
     Precharge_Write(MOTOR_PRECHARGE, ON); // Activate the motor recharge
     Contactors_Init(MOTOR);
@@ -19,8 +15,80 @@ void Task_MotorConnection(void *p_arg) {
 
     Contactors_Set(MOTOR, ON); // Actually activate motor contactors
 
-    OSSemPost(&SendTritium_Sem4, OS_OPT_POST_1, &err);
-    // TODO: check for errors
+    // Now we need to spawn the four CAN related tasks
 
-    OSTaskDel(NULL, &err);
+    // Spawn SendCarCAN
+    OSTaskCreate(
+        (OS_TCB*)&SendCarCAN_TCB,
+        (CPU_CHAR*)"SendCarCAN",
+        (OS_TASK_PTR)Task_SendCarCAN,
+        (void*)NULL,
+        (OS_PRIO)TASK_SEND_CAR_CAN_PRIO,
+        (CPU_STK*)SendCarCAN_Stk,
+        (CPU_STK_SIZE)WATERMARK_STACK_LIMIT,
+        (CPU_STK_SIZE)TASK_SEND_CAR_CAN_STACK_SIZE,
+        (OS_MSG_QTY)NULL,
+        (OS_TICK)NULL,
+        (void*)NULL,
+        (OS_OPT)(OS_OPT_TASK_STK_CLR),
+        (OS_ERR*)&err
+    );
+    // TODO: check for task creation error
+
+    // Spawn ReadCarCAN
+    OSTaskCreate(
+        (OS_TCB*)&ReadCarCAN_TCB,
+        (CPU_CHAR*)"ReadCarCAN",
+        (OS_TASK_PTR)Task_ReadCarCAN,
+        (void*)NULL,
+        (OS_PRIO)TASK_READ_CAR_CAN_PRIO,
+        (CPU_STK*)ReadCarCAN_Stk,
+        (CPU_STK_SIZE)WATERMARK_STACK_LIMIT,
+        (CPU_STK_SIZE)TASK_READ_CAR_CAN_STACK_SIZE,
+        (OS_MSG_QTY)NULL,
+        (OS_TICK)NULL,
+        (void*)NULL,
+        (OS_OPT)(OS_OPT_TASK_STK_CLR),
+        (OS_ERR*)&err
+    );
+    // TODO: check for task creation error
+
+    // Spawn ReadTritium
+    OSTaskCreate(
+        (OS_TCB*)&ReadTritium_TCB,
+        (CPU_CHAR*)"ReadTritium",
+        (OS_TASK_PTR)Task_ReadTritium,
+        (void*)NULL,
+        (OS_PRIO)TASK_READ_TRITIUM_PRIO,
+        (CPU_STK*)ReadTritium_Stk,
+        (CPU_STK_SIZE)WATERMARK_STACK_LIMIT,
+        (CPU_STK_SIZE)TASK_READ_TRITIUM_STACK_SIZE,
+        (OS_MSG_QTY)NULL,
+        (OS_TICK)NULL,
+        (void*)NULL,
+        (OS_OPT)(OS_OPT_TASK_STK_CLR),
+        (OS_ERR*)&err
+    );
+
+    // TODO: check for task creation error
+
+    // Spawn SendTritium
+    OSTaskCreate(
+        (OS_TCB*)&SendTritium_TCB,
+        (CPU_CHAR*)"SendTritium",
+        (OS_TASK_PTR)Task_SendTritium,
+        (void*)NULL,
+        (OS_PRIO)TASK_SEND_TRITIUM_PRIO,
+        (CPU_STK*)SendTritium_Stk,
+        (CPU_STK_SIZE)WATERMARK_STACK_LIMIT,
+        (CPU_STK_SIZE)TASK_SEND_TRITIUM_STACK_SIZE,
+        (OS_MSG_QTY)NULL,
+        (OS_TICK)NULL,
+        (void*)NULL,
+        (OS_OPT)(OS_OPT_TASK_STK_CLR),
+        (OS_ERR*)&err
+    );
+    // TODO: check for task creation error
+
+    OSTaskDel(NULL, &err); // we're done 
 }
