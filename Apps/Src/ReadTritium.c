b8ba7989af962dea5086dc6719b00491b16b0909
Cam0Cow <roie.roiegal@gmail.com>
2021-03-13 14:02:55 -0600

    Rehauled code to conform to new task architecture

--- Apps/Src/ReadTritium.c
+++ Apps/Src/ReadTritium.c
@@ -2,37 +2,24 @@
 
 #include "ReadTritium.h"
 
-void Task_ReadTritium(void* p_arg){
+void Task_ReadTritium(void* p_arg) {
 
 	OS_ERR err;
-	CPU_TS ts;
-
-	OSSEMPend(&ReadTritium_Sem4, 0, OS_OPT_PEND_BLOCKING, &ts, &err);
 
 	CANbus_Init();
 
-	uint8_t *message;
-	ErrorStatus status = CANbus_Read(message);
+	while (1) {
 
-	if(status){
+		uint8_t *message;
+		ErrorStatus status = CANbus_Read(message);
 
-		err = OSQPost(CANbus_MsgQ, (void *)message);
-		
-		// Not sure how we want to handle each error
-		switch(err){
-			case OS_Q_FULL:
-				break;
-			case OS_ERR_EVENT_TYPE:
-				break;
-			case OS_ERR_PEVENT_NULL:
-				break;
-			case OS_ERR_POST_NULL_PTR:
-				break;
-			default:
-				break;
+		if(status == SUCCESS) {
 
+			OSQPost(&CANBus_MsgQ, (void *)message, strlen((char *) message), OS_OPT_POST_FIFO, &err);
+			
+			if (err != OS_ERR_NONE) {
+				// TODO: error handling
+			}
 		}
-
 	}
-	// Not sure what to do if message returns null
 }
