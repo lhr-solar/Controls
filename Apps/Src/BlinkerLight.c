5818482d851266ff972a21e8e34c8a517c06780e
Cam0Cow <roie.roiegal@gmail.com>
2021-03-13 15:18:35 -0600

    blinkerlights

---
+++ Apps/Src/BlinkerLight.c
@@ -0,0 +1,40 @@
+/* Copyright (c) 2020 UT Longhorn Racing Solar */
+
+#include "BlinkerLight.h"
+
+/**
+* @brief   Toggle proper indicator lights
+* @param   None
+* @return  None
+*/ 
+void Task_BlinkLight(void *p_arg){
+    OS_ERR err;
+    CPU_TS ts;
+
+    while(OSSemPend(&BlinkLight_Sem4, 0, OS_OPT_PEND_BLOCKING, &ts, &err)){
+        if(switches.LEFT_SW && switches.RIGHT_SW){
+            //Both lights blink at the same rate
+            Lights_Set(LEFT_BLINK, !Lights_Read(LEFT_BLINK));
+            Lights_Set(RIGHT_BLINK, !Lights_Read(LEFT_BLINK));
+
+        }else if(switches.LEFT_SW){
+            // Only the left light blinks
+            Lights_Set(LEFT_BLINK, !Lights_Read(LEFT_BLINK));
+            Lights_Set(RIGHT_BLINK, 0);
+
+        }else if(switches.RIGHT_SW){
+            // Only the right light blinks
+            Lights_Set(RIGHT_BLINK, !Lights_Read(RIGHT_BLINK));
+            Lights_Set(LEFT_BLINK, 0);
+        }
+
+        // Wait 0.64 seconds
+        OSTimeDlyHMSM (0, 0, 0, 640, OS_OPT_TIME_HMSM_STRICT, &err);
+    }
+
+    // Turn both lights off
+    Lights_Set(LEFT_BLINK, 0); 
+    Lights_Set(RIGHT_BLINK, 0); 
+
+}
+
