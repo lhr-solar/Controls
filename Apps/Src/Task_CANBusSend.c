cf0ecc65844c34171445334d1f891bf035a06a92
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-03-27 14:15:07 -0500

    Created CanBusSend task

---
+++ Apps/Src/Task_CANBusSend.c
@@ -0,0 +1,27 @@
+#include "os.h"
+#include "Tasks.h"
+#include "bsp.h"
+#include "CANbus.h"
+
+void Task_SendCarCAN(void *p_arg){
+    OS_ERR err;
+    OS_MSG_SIZE msgSize;
+    CPU_TS timestamp;
+    CANMSG_t *msg;
+    CANbus_Init();
+
+    while (1){
+        (msg) = (CANMSG_t*)OSQPend(
+            &CANBus_MsgQ,
+            0,
+            OS_OPT_PEND_BLOCKING,
+            msgSize,
+            &timestamp,
+            &err
+        );
+        if (err == OS_ERR_NONE){ //if a message is recieved
+            CANbus_BlockSend(msg->id,msg->payload); //send message
+        }
+    }
+    
+}
