9511b723e03d4c5546d8b1040e442dbd33aa1f3d
Cam0Cow <roie.roiegal@gmail.com>
2021-04-17 16:30:10 -0500

    Initial files

---
+++ Apps/Inc/UpdateLights.h
@@ -0,0 +1,10 @@
+/* Copyright (c) 2020 UT Longhorn Racing Solar */
+
+#ifndef __UPDATE_LIGHTS_H
+#define __UPDATE_LIGHTS_H
+
+#include "os.h"
+#include "common.h"
+#include "Tasks.h"
+
+#endif
---
+++ Apps/Src/UpdateLights.c
@@ -0,0 +1,17 @@
+/* Copyright (c) 2020 UT Longhorn Racing Solar */
+
+#include "UpdateLights.h"
+
+void Task_UpdateLights(void *p_arg) {
+    car_state_t *car = (car_state_t *) p_arg;
+
+    // Brake lights
+
+    // hazard lights
+
+    // headlights
+
+    // turn signals
+
+    // faults
+}
