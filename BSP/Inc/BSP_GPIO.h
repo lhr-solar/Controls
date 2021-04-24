472bb621f1634c32c721d3b07d9a6b258ce182d9
erickcortez98 <erick-cortez98@hotmail.com>
2021-04-24 14:44:16 -0500

    incorporated three functions from branch gpio-thoughts

--- BSP/Inc/BSP_GPIO.h
+++ BSP/Inc/BSP_GPIO.h
@@ -36,4 +36,29 @@ uint16_t BSP_GPIO_Read(port_t port);
  */ 
 void BSP_GPIO_Write(port_t port, uint16_t data);
 
+/**
+ * @brief   Reads data to a specified pin
+ * @param   port The port to read from
+ * @param   pin The pin to read from 
+ * @return  State of the pin
+ */ 
+uint8_t BSP_GPIO_Read_Pin(port_t port, uint8_t pin);
+
+/**
+ * @brief   Writes data to a specified pin
+ * @param   port The port to write to
+ * @param   pin The pin to write to 
+ * @param   enable output of pin
+ * @return  None
+ */ 
+void BSP_GPIO_Write_Pin(port_t port, uint8_t pin, uint8_t enable);
+
+/**
+ * @brief   Returns state of output pin
+ * @param   port The port to get state from
+ * @param   pin The pin to get state from
+ * @return  1 if pin is high, 0 if low
+ */ 
+uint8_t BSP_GPIO_Get_State(port_t port, uint8_t pin);
+
 #endif
--- BSP/STM32F413/Src/BSP_GPIO.c
+++ BSP/STM32F413/Src/BSP_GPIO.c
@@ -104,7 +104,7 @@ uint16_t BSP_GPIO_Read(port_t port){
 	GPIO_TypeDef *gpio_port = GPIO_GetPort(port);
 	
 	GPIO_Wait(gpio_port);
-	return gpio_port->IDR & 0xFFFF;
+	return GPIO_ReadInputData(gpio_port);
 }
 
 /**
@@ -117,5 +117,50 @@ void BSP_GPIO_Write(port_t port, uint16_t data){
 	GPIO_TypeDef *gpio_port = GPIO_GetPort(port);
 	
 	GPIO_Wait(gpio_port);
-	gpio_port->ODR = data & 0xFFFF;
+	GPIO_Write(gpio_port, data);
 }
+
+/**
+ * @brief   Reads data to a specified pin
+ * @param   port The port to read from
+ * @param   pin The pin to read from 
+ * @return  State of the pin
+ */ 
+uint8_t BSP_GPIO_Read_Pin(port_t port, uint8_t pin){
+	GPIO_TypeDef *gpio_port = GPIO_GetPort(port);
+	
+	GPIO_Wait(gpio_port);
+	GPIO_ReadInputDataBit(gpio_port, pin);
+}
+
+/**
+ * @brief   Writes data to a specified pin
+ * @param   port The port to write to
+ * @param   pin The pin to write to 
+ * @param   enable output of pin
+ * @return  None
+ */ 
+void BSP_GPIO_Write_Pin(port_t port, uint8_t pin, uint8_t enable){
+	GPIO_TypeDef *gpio_port = GPIO_GetPort(port);
+	
+	GPIO_Wait(gpio_port);
+	GPIO_WriteBit(gpio_port, pin, enable);
+}
+
+/**
+ * @brief   Returns state of output pin
+ * @param   port The port to get state from
+ * @param   pin The pin to get state from
+ * @return  1 if pin is high, 0 if low
+ */ 
+uint8_t BSP_GPIO_Get_State(port_t port, uint8_t pin){
+	GPIO_TypeDef *gpio_port = GPIO_GetPort(port);
+
+	GPIO_Wait(gpio_port);
+	GPIO_ReadOutputDataBit(gpio_port, pin);	
+}
+
+
+
+
+
