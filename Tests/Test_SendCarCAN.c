5f6b4476cd8bdf1efd087360704d8f50940e3c09
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-05-22 10:38:57 -0500

    Created Full SendCarCAN test

--- Tests/Test_SendCarCAN.c
+++ Tests/Test_SendCarCAN.c
@@ -2,14 +2,56 @@
 #include "CANbus.h"
 
 void Task1(void *p_arg){
+    CPU_Init();
+    OS_CPU_SysTickInit();
     OS_ERR err;
     CPU_TS ts;
-    OSQCreate(); //initializes CAN Send queue
-    OSQPost();//populate CAN SEND Queue with test msgs
-    //spawn CAN send task
-    //Use CANRead to read back the messages to see if they match properly
+    OSQCreate(&CANBus_MsgQ, "CANBus Q", 16, &err); //initializes CAN Send queue
+    
+    //spawn can send task
+    OSTaskCreate(
+        (OS_TCB*)&SendCarCAN_TCB,
+        (CPU_CHAR*)"SendCarCan",
+        (OS_TASK_PTR)Task_SendCarCAN,
+        (void*)NULL,
+        (OS_PRIO)3,
+        (CPU_STK*)SendCarCAN_Stk,
+        (CPU_STK_SIZE)128/10,
+        (CPU_STK_SIZE)128,
+        (OS_MSG_QTY)NULL,
+        (OS_TICK)NULL,
+        (void*)NULL,
+        (OS_OPT)(OS_OPT_TASK_STK_CLR),
+        (OS_ERR*)&err
+    );
+
+    uint8_t buffer[8];
+    buffer[0]=99; //this should change after read
+    CANMSG_t msg;
+    CANPayload_t payload;
+    CANData_t dat;
+    dat.b = 1;
+    payload.bytes = 1;
+    payload.data = dat;
+    payload.idx = 0;
+    msg.id = CAR_STATE; //ON-OFF 
+    msg.payload = payload;
+    while(1){
+        //post a message to the queue
+        OSQPost(
+            &CANBus_MsgQ,
+            &msg,
+            sizeof(msg),
+            OS_OPT_POST_FIFO,
+            &err
+        );
+        //Test the reading functionality
+        CANbus_Read(&buffer[0],CAN_NON_BLOCKING);
+        printf("Read Result: %i\n",buffer[0]);
+        msg.payload.data.b ^= 0x01; //toggle bit 0  to swap data
+    }
 }
-void main(void){ //startup OS stuff, spawn test task
+int main(void){ //startup OS stuff, spawn test task
     static OS_TCB Task1_TCB;
     static CPU_STK Task1_Stk[128];
     OS_ERR err;
@@ -17,7 +59,7 @@ void main(void){ //startup OS stuff, spawn test task
     if(err != OS_ERR_NONE){
         printf("OS error code %d\n",err);
     }
-    INIT_TASK(Task1,3,NULL,err);
+    INIT_TASK(Task1,4,NULL,err);
      if (err != OS_ERR_NONE) {
         printf("Task1 error code %d\n", err);
     }
@@ -25,4 +67,5 @@ void main(void){ //startup OS stuff, spawn test task
     if (err != OS_ERR_NONE) {
         printf("OS error code %d\n", err);
     }
+    return 0;
 }
