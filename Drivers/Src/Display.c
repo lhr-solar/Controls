9e242b017b804eb4c9899ccbf411732ce66953aa
Cam0Cow <roie.roiegal@gmail.com>
2021-10-30 12:35:04 -0500

    No longer constructing instruction string in memory

--- Drivers/Src/Display.c
+++ Drivers/Src/Display.c
@@ -3,10 +3,14 @@
 #include <math.h>
 #include <string.h>
 
-#define NEXTION_INSTRUCTION_SUCCESSFUL 0xffffff01
+#define NEXTION_INSTRUCTION_SUCCESSFUL 0x01ffffff
 
-static inline NextionIsFailure(uint32_t val) {
-    return ((val & 0xFFFFFF00) == 0xFFFFFF00 && (val & 0xFF) != 1);
+static const char *SEPERATOR = ".";
+static const char *ASSIGNMENT = "=";
+static const char *TERMINATOR = "\xff\xff\xff";
+
+static inline IsNextionFailure(uint32_t val) {
+    return ((val & ~0x00FFFFFF) != (1 << 24));
 }
 
 
@@ -45,24 +49,22 @@ static char *CommandStrings[] = {
 
 /**
  * Convert signed 32-bit integer to string
- * Returns the length of the string
+ * Returns pointer to the string
  */
 static int *to_charp(int32_t num, char *dest) {
-    static char buf[12];
     bool neg = false;
     if (num < 0) {
         neg = true;
         num = -num;
     }
     int index = 12;
-    buf[--index] = '\0';
+    dest[--index] = '\0';
     do {
-        buf[--index] = (num % 10) + '0';
+        dest[--index] = (num % 10) + '0';
         num /= 10;
     } while (num > 9);
-    if (num != 0) buf[--index] = num + '0';
-    if (neg) buf[--index] = '-';
-    strcpy(dest, buf+index);
+    if (num != 0) dest[--index] = num + '0';
+    if (neg) dest[--index] = '-';
     return 12 - index; // return string length
 }
 
@@ -70,12 +72,25 @@ static int *to_charp(int32_t num, char *dest) {
  * Sets an object's attribute to a value
  */
 static ErrorStatus updateValue(enum CommandString_t obj_index, enum CommandString_t attr_index, int32_t val) {
-    char buf[256];
+    char buf[12]; // To store converted int
+    char *number = to_charp(val, buf); // Convert integer to string
     char *obj = CommandStrings[obj_index];
     char *attr = CommandStrings[attr_index];
     int len1 = strlen(obj);
     int len2 = strlen(attr);
 
+    // If not modifying a global, send obj
+    if (len1 != 0) {
+        BSP_UART_Write(UART_2, obj, len1);
+        BSP_UART_Write(UART_2, SEPERATOR, 1);
+    }
+
+    BSP_UART_Write(UART_2, attr, len2); // Send the attribute
+    BSP_UART_Write(UART_2, ASSIGNMENT, 1);
+    BSP_UART_Write(UART_2, number, strlen(number)); // Send the value
+    BSP_UART_Write(UART_2, TERMINATOR, strlen(TERMINATOR));
+
+/*
     // construct the string "obj.attr=val"
     strcpy(buf, obj);
     buf[len1] = '.';
@@ -87,14 +102,15 @@ static ErrorStatus updateValue(enum CommandString_t obj_index, enum CommandStrin
     buf[len1+len2+len3+3] = 0xFF;
     buf[len1+len2+len3+4] = '\0';
 
-    printf("Command is \"%s\"\n", (len1 == 0) ? buf+1 : buf);
+    //printf("Command is \"%s\"\n", (len1 == 0) ? buf+1 : buf);
 
     // Get rid of extraneous period in global attribute
     BSP_UART_Write(UART_2, (len1 == 0) ? buf+1 : buf, strlen(buf));
+    */
 
     BSP_UART_Read(UART_2, buf);
     int ret = *((uint32_t *) buf);
-    return (ret == NEXTION_INSTRUCTION_SUCCESSFUL) ? SUCCESS : ERROR;
+    return (IsNextionFailure(ret)) ? ERROR : SUCCESS;
 }
 
 /*
