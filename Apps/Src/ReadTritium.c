efc3626c878ddecb2fbdf8217373dc11852d3a3a
Roie Gal <roie.roiegal@gmail.com>
2021-11-06 12:13:06 -0500

    Modified tritium tasks to work with new MotorConnection infrastracture (#149)

--- Apps/Src/ReadTritium.c
+++ Apps/Src/ReadTritium.c
@@ -5,12 +5,21 @@
 void Task_ReadTritium(void* p_arg) {
 
 	OS_ERR err;
+	CPU_TS ts;
 	
 	car_state_t *car_state = (car_state_t *) p_arg;
 
 	MotorController_Init();
 
 	while (1) {
+		OSTaskSemPend(0, OS_OPT_PEND_NON_BLOCKING, &ts, &err);
+        if (err == OS_ERR_NONE) {
+            // A signal was received, so the task should wait until signaled again
+            OSTaskSemPend(0, OS_OPT_PEND_BLOCKING, &ts, &err);
+            // TODO: error handling
+        } else if (err != OS_ERR_PEND_WOULD_BLOCK) {
+            // TODO: error handling
+        }
 
 		CANbuff buf;
 		ErrorStatus status = MotorController_Read(&buf);
