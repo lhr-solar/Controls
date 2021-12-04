337024a7b74309522ef87e349cf529c143173ca1
5p1cyP3p <nathaniel.delgado@utexas.edu>
2021-12-04 11:51:29 -0600

    Added Gear Reduction to config.h file

--- Config/Inc/config.h
+++ Config/Inc/config.h
@@ -12,6 +12,9 @@ typedef enum {OFF = 0, ON} State;
 
 // Wheel diameter in meters
 #define WHEEL_DIAMETER 0.4f
+// Any sort of gear reduction from the motor to the wheels
+// Defined as motor velocity / wheel velocity
+#define GEAR_REDUCTION 1.0f
 
 #define PRECHARGE_ARRAY_DELAY 5
 
