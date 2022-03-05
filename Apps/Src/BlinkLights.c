3d6f78ea1ae47056dbd55ab0a253ece4eb2423d1
IshDeshpa <ishdeshpa@gmail.com>
2022-03-05 14:14:34 -0800

    created blinklights task

--- Apps/Src/BlinkLights.c
+++ Apps/Src/BlinkLights.c
@@ -6,7 +6,20 @@
 **/
 
 void Task_BlinkLight(void* p_arg){
+    OS_ERR err;
     while(1){
+        Lights_MultiToggle();
         
+        //lock thread to run at 90ish times per minute if blinkLights needed.
+        OSTimeDlyHMSM(
+            0,
+            0,
+            0,
+            650, //650 milliseconds corresponds to 92ish times per minute 
+            OS_OPT_TIME_HMSM_STRICT,
+            &err
+        );
+        assertOSError(OS_BLINK_LIGHTS_LOC,err);
     }
+    
 }
