3d09018e07a0eb13d3a27fa731df0fd128bcc24b
Nathaniel Delgado <89701060+NathanielDelgado@users.noreply.github.com>
2022-03-05 16:15:16 -0600

    Switches Driver Fixes (#183)
    * Fixed the SPI register addresses for bank=0
    
    * Fixes to Switches Init
    
    * Updated the switches ordering and updated switches Read
    
    * Commented out some things to make sure it compiled. Added the test files from RTOS_SwitchRead
    
    * initial threadsafing of switch driver
    
    * Added mutex initialization
    
    * Fixed locking mismatch in init, added OSError assertions (0 was given as placeholder for location for now)
    
    * removed mutexes on read, fixed RxBuf, fixed BSP_GPIO_Write_Bit
    
    * fixed BSP_GPIO_Read to pass RxBuf by reference
    
    * made BSP_GPIO_Write_Pin better
    
    * Changed gpio functions to bps layer. Changed bsp function definitions to include mask. Created read and update function and replace struct with bitmap. Removes struct from carstate.h file
    
    * Fixed warnings
    
    * Added Tianda's changes
    
    * Added mutex protection for CS pins
    
    * Removed redundant mutex pend/posts
    
    * Added sids suggestions
    
    * Update Drivers/Src/Switches.c
    
    Co-authored-by: Roie Gal <roie.roiegal@gmail.com>
    
    Co-authored-by: Sidharth N. Babu <sidharth.n.babu@utexas.edu>
    Co-authored-by: Cam0Cow <roie.roiegal@gmail.com>
    Co-authored-by: IshDeshpa <ishdeshpa@gmail.com>
    Co-authored-by: Manthan Upadhyaya <manthanand@gmail.com>
    Co-authored-by: manthanand <55002301+manthanand@users.noreply.github.com>

---
+++ Tests/Test_MiniboardSwitches_NoRTOS.c
@@ -0,0 +1,28 @@
+#include "common.h"
+#include "config.h"
+#include <unistd.h>
+//#include "BSP_PLL.h"
+#include "Switches.h"
+#include "BSP_UART.h"
+
+// Test file that sends out the status of FOR_SW to UART_2 without RTOS
+
+
+// static void delay_u(uint16_t micro)
+// {
+//   uint32_t delay = BSP_PLL_GetSystemClock() / 1000000;
+// 	for(uint32_t i = 0; i < micro; i++)
+// 	{
+// 		for(uint32_t j = 0; j < delay; j++);
+// 	}
+// }
+
+int main(){
+    BSP_UART_Init(UART_2);
+    Switches_Init();
+    switches_t sw = FOR_SW;
+    while(1){
+        printf("FOWARD Switch Status: %d\n", Switches_Read(sw));
+        //delay_u(250);
+    }
+}
