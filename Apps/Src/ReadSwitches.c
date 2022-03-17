ecf8a245a6907c4b29fc94a26c9f4a1113001710
Tianda Huang <huangtianda@gmail.com>
2022-03-17 17:53:29 -0500

    Changes requested by @babusid
    Also some "helpful" comments

--- Apps/Src/ReadSwitches.c
+++ Apps/Src/ReadSwitches.c
@@ -4,26 +4,25 @@
 #include "ReadSwitches.h"
 
 // Macros
-#define OS_DELAY_MS(ms) \
-    OSTimeDlyHMSM(0, 0, 0, (ms), OS_OPT_TIME_HMSM_NON_STRICT, &err)
-#define MAX_SWITCH_LAG          100  // delay (ms) for checking switches while precharging
-#define PRECHARGE_DELAY_CYCLES  (PRECHARGE_ARRAY_DELAY*1000 / MAX_SWITCH_LAG)
+#define MAX_SWITCH_LAG          100  // period (ms) for checking switches while precharging
+#define PRECHARGE_DELAY_CYCLES  ((PRECHARGE_ARRAY_DELAY*1000) / MAX_SWITCH_LAG)
 
 // Globals
-switch_states_t CurrSwitches;
-car_state_t *CarStates;
 OS_ERR err;
 
-// helper functions for reading/updating switches
+// Helper functions for reading/updating switches
+
 static uint8_t UpdateSwitches();
 static void UpdateLights();
 
-// state functions
+// State functions
+
 static void ElectronicsOn();
 static void ArrayPrecharge();
 static void ArrayOn();
 static void MotorOn();
 
+// States
 typedef enum {
     UNUSED_SWITCH_NUM = 0,
     ELEC_ON = 0,
@@ -39,7 +38,7 @@ typedef struct {
      *      1: delay for array precharge -> array on
      *      3: motor on
      */
-    uint8_t next_state[4];
+    ignition_state_num_t next_state[4];
 
     void (* statefunction)();   // function executed at state
     uint32_t delay_ms;          // ms delay after executing function, should be >= 1
@@ -53,12 +52,10 @@ fsm_state_t IgnitionFSM[] = {
 };
 
 /**
- * @brief Reads and processes switch states,
+ * @brief Updates switch states and controls blinker/headlights accordingly;
  *        Also handles precharge/contactor logic with the ignition switch
  */
 void Task_ReadSwitches(void* p_arg) {
-
-    CarStates = (car_state_t *)p_arg;
     
     fsm_state_t *curr_fsm_state = &(IgnitionFSM[0]);
 
@@ -73,40 +70,60 @@ void Task_ReadSwitches(void* p_arg) {
     // Main loop
     while (1) {
         curr_fsm_state->statefunction();
-        OS_DELAY_MS(curr_fsm_state->delay_ms);
         curr_fsm_state = &(IgnitionFSM[curr_fsm_state->next_state[UpdateSwitches()]]);  // set next state
+        OSTimeDlyHMSM(0, 0, 0, curr_fsm_state->delay_ms, OS_OPT_TIME_HMSM_NON_STRICT, &err);
     }
 }
 
-
+/**
+ * @brief State where only electronics are on; contactors are all off
+ */
 static void ElectronicsOn() {
-    Contactors_Set(ARRAY_PRECHARGE, OFF);
     Contactors_Set(ARRAY_CONTACTOR, OFF);
+    Contactors_Set(ARRAY_PRECHARGE, OFF);
     Contactors_Set(MOTOR_CONTACTOR, OFF);
     return;
 }
 
+/**
+ * @brief State for precharging the array before turning on. 
+ *        Array will precharge for a time defined in "config.h"
+ * @note  During the precharge delay, switches will be continually updated 
+ *        with a period defined by MAX_SWITCH_LAG
+ */
 static void ArrayPrecharge() {
     Contactors_Set(ARRAY_PRECHARGE, ON);
 
     uint16_t i = 0;
     while ((i < PRECHARGE_DELAY_CYCLES) && (UpdateSwitches() != 0)) {
-        OS_DELAY_MS(MAX_SWITCH_LAG);
+        OSTimeDlyHMSM(0, 0, 0, MAX_SWITCH_LAG, OS_OPT_TIME_HMSM_NON_STRICT, &err);
         i++;
     }
 }
 
+/**
+ * @brief Array On state. Motor contactor should be off here.
+ */
 static void ArrayOn() {
     Contactors_Set(MOTOR_CONTACTOR, OFF);
-    Contactors_Set(ARRAY_PRECHARGE, OFF);
     Contactors_Set(ARRAY_CONTACTOR, ON);
+    Contactors_Set(ARRAY_PRECHARGE, OFF);
 }
 
+/**
+ * @brief Motor On state. All non-prechage contactors - 
+ *        (Array contactor, Motor contactor) are on.
+ */
 static void MotorOn() {
     Contactors_Set(MOTOR_CONTACTOR, ON);
 }
 
-
+/**
+ * @brief Helper function for updating switches and lights. Updated switch 
+ *        values are stored locally in the switches driver.
+ * 
+ * @return uint8_t 2-bit ignition switch value
+ */
 static uint8_t UpdateSwitches() {
     Switches_UpdateStates();
 
@@ -115,6 +132,9 @@ static uint8_t UpdateSwitches() {
     return (Switches_Read(IGN_2) << 1) + Switches_Read(IGN_1);
 }
 
+/**
+ * @brief Helper function for updating lights with switch values.
+ */
 static void UpdateLights() {
     Lights_Set(Headlight_ON, Switches_Read(HEADLIGHT_SW));
     
