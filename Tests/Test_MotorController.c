6cb9fc52b5450c06fd91f0d59dd363a2b2439304
5p1cyP3p <nathaniel.delgado@utexas.edu>
2022-03-26 22:12:24 -0500

    Changed CAN3 to Normal mode and switched the veloicty and current order in the can message

--- Tests/Test_MotorController.c
+++ Tests/Test_MotorController.c
@@ -35,7 +35,7 @@ void Task1(void* arg){
     while(1){
         unatttainable_Velocity = 2500.0f;
 
-        desiredCurrent = 0.5f; //spin up to .5 amps worth
+        desiredCurrent = 5.0f; //spin up to .5 amps worth
         currentSetPoint = desiredCurrent/BUSCURRENT;
         for (int i=0; i<30; i++) { // Run for ~6 seconds
             MotorController_Drive(unatttainable_Velocity,currentSetPoint);
