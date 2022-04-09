ce3535d1cffb294f81bde0fc988abf861e0461e3
Cam0Cow <roie.roiegal@gmail.com>
2022-04-09 11:19:46 -0500

    Added calibration parameters to pedals driver

--- Drivers/Src/Pedals.c
+++ Drivers/Src/Pedals.c
@@ -6,6 +6,19 @@
 
 #include "Pedals.h"
 
+// Constants used to tune the pedals
+// Indexed using pedal_t
+// Refine in testing
+static const int16_t LowerBound[NUMBER_OF_PEDALS] = {
+    1000, // Accelerator lower bound
+    2000, // Brake lower bound
+};
+
+static const int16_t UpperBound[NUMBER_OF_PEDALS] = {
+    ADC_RANGE_MILLIVOLTS, // Accelerator upper bound
+    ADC_RANGE_MILLIVOLTS, // Brake upper bound
+};
+
 /**
  * @brief   Initializes the brake and accelerator by using the 
  *          BSP_ADC_Init function with parameters ACCELERATOR
@@ -20,14 +33,22 @@ void Pedals_Init(){
 /**
  * @brief   Fetches the millivoltage value of the potentiomenter as provided 
  *          by the ADC channel of the requested pedal (Accelerator or Brake),
- *          converts it to a percentage of the total distance pressed and 
- *          returns it
+ *          converts it to a percentage of the total distance pressed using 
+ *          data from calibration testing, and returns it
  * @param   pedal_t, ACCELERATOR or BRAKE as defined in enum
  * @return  distance the pedal has been pressed in percentage
  */
 int8_t Pedals_Read(pedal_t pedal){
+    if (pedal >= NUMBER_OF_PEDALS) return 0;
     int16_t millivoltsPedal = BSP_ADC_Get_Millivoltage(pedal);
-    int8_t percentage = millivoltsPedal * 100 / ADC_RANGE_MILLIVOLTS;
+
+    int8_t percentage = 0;
+    
+    if (millivoltsPedal >= LowerBound[pedal]) {
+        percentage = (millivoltsPedal - LowerBound[pedal]) * 100 /
+            (UpperBound[pedal] - 1000);
+    }
+
     return percentage;
 }
 
