3340167a3501c508b69a89d50278eecf30575524
manthanand <55002301+manthanand@users.noreply.github.com>
2022-04-16 15:30:25 -0500

    Minions Driver (#200)
    * Made lights and switches into one minions file with shared mutex
    
    * Made lights and switches into one minions file with shared mutex
    
    * Fixed compiler issues
    
    * Create ChipSelect and ChipDeselect helper functions
    
    * Change enum spacing
    
    Co-authored-by: Sidharth N. Babu <sidharth.n.babu@utexas.edu>
    
    * implemented requested changes by sidharth
    
    * Update documentation for Minions Initialization
    
    Co-authored-by: Sidharth N. Babu <sidharth.n.babu@utexas.edu>
    
    * Update documentation for read switches
    
    Co-authored-by: Sidharth N. Babu <sidharth.n.babu@utexas.edu>
    
    * renamed and initialized switchStatesBitmap
    
    * removed lights_multiset and lights_multitoggle from minions driver
    
    * added lights_toggle_bitmap_read back to header file
    
    * light toggle bitmap now only contains bits for left and right blink, as those are the only lights that need toggle functionality. updated functions accordingly
    
    * added semicolon; fixed build errors
    
    * ensured that hazard switch on GPIOB is not being set to an output at any point
    
    * corrected switches_init
    
    * Created new test files for miniboard testing (switches and lights), changed shift logic in the assignment of the switch bitmap and changed SPI3->SPI1
    
    * Applied negative logic fixes for right and left blink lights
    
    * Apply suggestions from code review
    
    * Changed FWD_SW to FOR_SW
    
    * Fixed conditional statements for Switch Read
    
    * Fix the negative logic of the left and right blink lights
    
    Co-authored-by: Cam0Cow <roie.roiegal@gmail.com>
    Co-authored-by: Ishan Deshpande <ishdeshpa@gmail.com>
    Co-authored-by: Sidharth N. Babu <sidharth.n.babu@utexas.edu>
    Co-authored-by: 5p1cyP3p <nathaniel.delgado@utexas.edu>

--- Apps/Src/Tasks.c
+++ Apps/Src/Tasks.c
@@ -48,6 +48,7 @@ OS_Q CANBus_MsgQ;
 /**
  * Semaphores
  */
+OS_SEM FaultState_Sem4;
 OS_SEM VelocityChange_Sem4;
 OS_SEM DisplayChange_Sem4;
 OS_SEM LightsChange_Sem4;
--- Drivers/Inc/Minions.h
+++ Drivers/Inc/Minions.h
@@ -1,19 +1,34 @@
 /* Copyright (c) 2020 UT Longhorn Racing Solar */
 
-#ifndef __Lights_H
-#define __Lights_H
-
-#include "BSP_GPIO.h"
+#ifndef MINIONS_H
+#define MINIONS_H
 #include "BSP_SPI.h"
+#include "BSP_GPIO.h"
 #include "common.h"
 #include "config.h"
 #include "GPIOExpander.h"
+#include "Tasks.h"
 
+#define SPI_CS          GPIO_Pin_4
 #define LIGHTS_PORT     PORTC
 #define HEADLIGHT_PIN   6
 #define BRAKELIGHT_PIN  7
 #define RIGHT_BLINK_PIN 8
 #define LEFT_BLINK_PIN  9
+
+typedef enum {
+     CRUZ_ST=0, 
+     CRUZ_EN, 
+     REV_SW,
+     FOR_SW,
+     HEADLIGHT_SW,
+     LEFT_SW,
+     RIGHT_SW,
+     REGEN_SW,
+     HZD_SW,
+     IGN_1,
+     IGN_2
+} switches_t;
 
 // Ordered by pin of minion board
 //TODO: Double check that this ordering remains valid
@@ -29,10 +44,39 @@ typedef enum {
 } light_t;
 
 /**
-* @brief   Initialize Lights Module
-* @return  void
-*/ 
-void Lights_Init(void);
+ * Switch States
+ * 
+ * Stores the current state of each of
+ * the switches that control this system
+ */
+typedef struct {
+    State LT;
+    State RT;
+    State FOR;
+    State REV;
+    State CRS_EN;
+    State CRS_SET;
+    State REGEN;
+    State HZD;
+    State HDLT;
+    State IGN_1;
+    State IGN_2;
+} switch_states_t;
+
+/**
+ * @brief   Initializes all Lights and Switches
+ * @param   None
+ * @return  None
+ */ 
+void Minions_Init(void);
+
+/**
+ * @brief   Reads the current state of 
+ *          the specified switch
+ * @param   sw the switch to read
+ * @return  State of the switch (ON/OFF)
+ */ 
+State Switches_Read(switches_t sw);
 
 /**
 * @brief   Read the state of the lights
@@ -40,6 +84,12 @@ void Lights_Init(void);
 * @return  returns state enum which indicates ON/OFF
 */ 
 State Lights_Read(light_t light);
+
+/**
+ * @brief   Sends SPI messages to read switches values. Also reads from GPIO's for 
+ *          ignition switch values
+ */ 
+void Switches_UpdateStates(void);
 
 /**
  * @brief   Set light to given state
@@ -50,30 +100,16 @@ State Lights_Read(light_t light);
 void Lights_Set(light_t light, State state);
 
 /**
- * @brief   Set multiple lights given light bitmap
- * @return  void
- */
-void Lights_MultiSet(uint16_t bitmap);
-
-
-/**
 * @brief   Read the lights bitmap
 * @return  returns uint16_t with lights bitmap
 */ 
-uint16_t Lights_Bitmap_Read(light_t light);
+uint16_t Lights_Bitmap_Read();
 
 /**
  * @brief Toggles a light. Should be used only after Toggle_Enable has been called for this light so that we are accurately tracking the enabled and disabled lights
  * @param light Which light to toggle
 */
 void Lights_Toggle(light_t light);
-
-
-/**
- * @brief Toggles multiple lights according to the toggle bitmap
-*/
-void Lights_MultiToggle(void);
-
 
 /**
 
@@ -82,20 +118,19 @@ void Lights_MultiToggle(void);
  * @param   state State to set toggling
  * @return  void
  */
-void Toggle_Set(light_t light, State state);
+void Lights_Toggle_Set(light_t light, State state);
 
 /**
 * @brief   Read the toggle enable state of a specific light from the toggle bitmap
 * @param   light Which Light to read
 * @return  returns State enum which indicates ON/OFF
 */
-State Toggle_Read(light_t light);
-
+State Lights_Toggle_Read(light_t light);
 
 /**
  * @brief   Read toggle bitmap
- * @return  returns uint16_t bitmap for toggle
+ * @return  returns uint8_t bitmap for toggle (LEFT/RIGHT)
  */
-uint16_t Toggle_Bitmap_Read(void);
+uint8_t Lights_Toggle_Bitmap_Read(void);
 
 #endif
