4ac883e860650b608ca266013de07bb942561ab2
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2022-04-19 22:29:13 -0500

    updated SwitchRead thread Testfile to better gauge its new behavior

--- Tests/Test_ReadSwitches.c
+++ Tests/Test_ReadSwitches.c
@@ -6,36 +6,33 @@
  * 
  * * Test Plan * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *    This test prints out the contactor/precharger states every 100ms.
+ *    It repeatedly attempts to turn the contactors in order to test the
+ *    ReadSwitches thread's ability to enable/disable contactors.
  * 
  * 1. Leave the ignition switch at the "electronics on" position.
  *    Verify that the contactors are correct. The readout should be:
  * 
- *          array precharge | array contactor | motor contactor
- *                OFF       |       OFF       |       OFF
+ *          array precharge | array contactor | motor contactor |  IGN_1  | IGN_2   
+ *                OFF       |       OFF       |       OFF       |   OFF   | OFF
  * 
  * 2. Turn the ignition switch to the "array on" position.
- *    For approximately 2 seconds, the readout should be:
+ *    ReadSwitches will now enable the array precharge and array 
+ *    contactor, and they should come on. 
  * 
- *          array precharge | array contactor | motor contactor
- *                ON!       |       OFF       |       OFF
- * 
- *    Then, the readout will switch to (and remain in):
- * 
- *          array precharge | array contactor | motor contactor
- *                OFF       |       ON!       |       OFF
- * 
- * 3. Leave the ignition switch at the "motor on" position.
+ *          array precharge | array contactor | motor contactor |  IGN_1  | IGN_2
+ *                ON        |       ON        |       OFF       |   ON    | OFF
+ 
+ * 3. Move the ignition switch at the "motor on" position.
  *    The readout should be:
  * 
- *          array precharge | array contactor | motor contactor
- *                OFF       |       ON!       |       ON!
- * 
- * 4. Try the following ignition switch sequences and verify that 
- *    the contactors match the readouts listed:
- *      * electronics -> motor
- *      * motor -> array
- *      * array/motor -> electronics
- *      * electronics -> array -> electronics (switch to electronics immediately)
+ *          array precharge | array contactor | motor contactor |  IGN_1  | IGN_2
+ *                ON        |       ON       |       ON         |   ON    | ON
+ * 4. Try the following sequences:
+ * Electronics -> Array
+ * Electronics -> Motor
+ * Electronics -> Array -> Electronics
+ * Electronics -> Motor -> Electronics
+ * Electronics -> Array -> Motor -> Array -> Motor
  */
 
 #include "os.h"
@@ -54,9 +51,10 @@ int main(void) {
     OS_ERR err;
     OSInit(&err);
 
-    // Initialized with error
+    // FaultState semaphore
+    OSSemCreate(&FaultState_Sem4, "FaultState", 0, &err);
     if (err != OS_ERR_NONE) {
-        printf("OS error code %d\n", err);
+        printf("Task error code %d\n", err);
     }
     assertOSError(OS_NONE_LOC, err);
 
@@ -77,13 +75,6 @@ int main(void) {
     );
 
     // Task not created
-    if (err != OS_ERR_NONE) {
-        printf("Task error code %d\n", err);
-    }
-    assertOSError(OS_NONE_LOC, err);
-
-    // FaultState semaphore
-    OSSemCreate(&FaultState_Sem4, "FaultState", 0, &err);
     if (err != OS_ERR_NONE) {
         printf("Task error code %d\n", err);
     }
@@ -129,17 +120,22 @@ void TestReadSwitches(void *p_arg) {
     // Wait for Task_ReadSwitches to finish initialization (with an extra second to be sure)
     OSTimeDlyHMSM(0, 0, PRECHARGE_MOTOR_DELAY + 1, 0, OS_OPT_TIME_HMSM_NON_STRICT, &err);
 
-    uint8_t counter = 0;
+    printf("array precharge | array contactor | motor contactor | IGN1 | IGN2 \n");
     while (1) {
-        if (counter == 0) {
-            printf("array precharge | array contactor | motor contactor\n");
-        }
-        printf("      %s       |       %s       |       %s      \n",
+        //attempt to turn on all contactors
+        Contactors_Set(ARRAY_PRECHARGE, ON);
+        Contactors_Set(ARRAY_CONTACTOR, ON);
+        Contactors_Set(MOTOR_CONTACTOR, ON);
+
+        printf(
+            "      %s       |       %s       |       %s      |       %s       |       %s       |\r",
             (Contactors_Get(ARRAY_PRECHARGE) == ON) ? "ON!" : "OFF",
             (Contactors_Get(ARRAY_CONTACTOR) == ON) ? "ON!" : "OFF",
-            (Contactors_Get(MOTOR_CONTACTOR) == ON) ? "ON!" : "OFF");
+            (Contactors_Get(MOTOR_CONTACTOR) == ON) ? "ON!" : "OFF",
+            (Switches_Read(IGN_1) == ON) ? "ON!" : "OFF" ,
+            (Switches_Read(IGN_2) == ON) ? "ON!" : "OFF" 
+        );
 
-        counter = (counter + 1) % 50;
         OSTimeDlyHMSM(0, 0, 0, 100, OS_OPT_TIME_HMSM_NON_STRICT, &err);
     }
 }
