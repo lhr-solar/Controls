0f1b3d4389252dad8333ede14559b4a2f6163e37
Cam0Cow <roie.roiegal@gmail.com>
2022-04-23 17:38:56 -0500

    Removed code from ReadCarCAN for now

--- Apps/Src/ReadCarCAN.c
+++ Apps/Src/ReadCarCAN.c
@@ -5,33 +5,5 @@
 #define THRESHOLD 3
 
 void Task_ReadCarCAN(void *p_arg) {
-    car_state_t *car = (car_state_t *) p_arg;
-
-    uint8_t buffer[8]; // buffer for CAN message
-
-    static int faultCounter = 0;
-
-    OS_ERR err;
-
-    while (1) {
-        // Check if BPS sent us a message
-        if (CANbus_Read(buffer) == SUCCESS) {
-            long msg = *((long *)(&buffer[0]));
-            if (msg == 0) {
-                car->IsRegenBrakingAllowed = OFF;
-            } else {
-                car->IsRegenBrakingAllowed = ON;
-            }
-
-            faultCounter = 0;
-        } else {
-            faultCounter++;
-        }
-
-        if (faultCounter >= THRESHOLD) {
-            car->IsRegenBrakingAllowed = OFF;
-        }
-
-        OSTimeDlyHMSM(0, 0, 0, 500, OS_OPT_TIME_HMSM_NON_STRICT, &err);
-    }
+    
 }
