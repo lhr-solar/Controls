05c0ce32086cb7d7c55e792a5d6010d87afd1766
Cam0Cow <roie.roiegal@gmail.com>
2022-10-15 17:29:50 -0500

    Added some tests for repeated blocking and non-blocking CANbus_Send

--- Tests/Test_CANbus.c
+++ Tests/Test_CANbus.c
@@ -7,10 +7,17 @@ static OS_TCB Task1TCB;
 static CPU_STK Task1Stk[128];
 
 void Task1(void *p_arg) {
+    (void) p_arg;
     OS_ERR err;
 
     CPU_Init();
     OS_CPU_SysTickInit(SystemCoreClock / (CPU_INT32U) OSCfg_TickRate_Hz);
+
+    /**
+     * +-------------+
+     * | CANbus_Send |
+     * +-------------+
+    */
 
     CANDATA_t msg;
     msg.ID = MOTOR_DRIVE;
@@ -20,14 +27,45 @@ void Task1(void *p_arg) {
     memcpy(&msg.data[0], &vel, sizeof vel);
     memcpy(&msg.data[4], &current, sizeof current);
 
-    CANbus_Send(msg, CAN_NON_BLOCKING, MOTORCAN);
+    CANbus_Send(msg, CAN_BLOCKING, MOTORCAN);
     OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
 
     msg.ID = ARRAY_CONTACTOR_STATE_CHANGE;
     msg.idx = 0;
     msg.data[0] = 1;
-    CANbus_Send(msg, CAN_NON_BLOCKING, CARCAN);
+    CANbus_Send(msg, CAN_BLOCKING, CARCAN);
+    OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
 
+    // Now test non-blocking CAN send
+    // The latter ones should return an error
+    msg.ID = MOTOR_DRIVE;
+    memcpy(&msg.data[0], &vel, sizeof vel);
+    memcpy(&msg.data[4], &current, sizeof current);
+    volatile ErrorStatus e;
+    e = CANbus_Send(msg, CAN_NON_BLOCKING, MOTORCAN);
+    e = CANbus_Send(msg, CAN_NON_BLOCKING, MOTORCAN);
+    e = CANbus_Send(msg, CAN_NON_BLOCKING, MOTORCAN); // Potentially an error?
+    e = CANbus_Send(msg, CAN_NON_BLOCKING, MOTORCAN); // Almost definitely an error, check in GDB
+    (void) e;
+    
+    OSTimeDlyHMSM(0, 0, 2, 0, OS_OPT_TIME_HMSM_STRICT, &err);
+
+    // Now do the same test, but blocking this time
+    // All of the operations should succeed, so we should see four CAN messages
+    e = CANbus_Send(msg, CAN_BLOCKING, MOTORCAN);
+    e = CANbus_Send(msg, CAN_BLOCKING, MOTORCAN);
+    e = CANbus_Send(msg, CAN_BLOCKING, MOTORCAN);
+    e = CANbus_Send(msg, CAN_BLOCKING, MOTORCAN);
+
+
+    /**
+     * +-------------+
+     * | CANbus_Read |
+     * +-------------+
+     */
+    // Might have to put CAN in loopback mode to test this?
+
+    
 }
 
 
