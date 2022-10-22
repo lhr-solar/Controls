2c283728ee2973cbc36dbca460f5e72f2a9a4b59
NathanielDelgado <nathaniel.delgado@utexas.edu>
2022-10-22 19:47:16 +0000

    Minor changes

--- Apps/Src/SendCarCAN.c
+++ Apps/Src/SendCarCAN.c
@@ -1,16 +1,16 @@
 #include "Tasks.h"
-#include "common.h"
 #include "CANbus.h"
 #include "CAN_Queue.h"
 #include "Pedals.h"
 #include "Minions.h"
 #include "Contactors.h"
+#include "common.h"
 
 // Make sure updated to the DATAMSG and carMSGID are reflected in the CAN Bus IDs excel sheet
 
-#define carMSGID 0x581
+#define CARDATA_ID 0x581
 
-typedef union DATAMSG{
+typedef union CarData{
     struct{
         uint64_t accelPedal : 8;
         uint64_t brakePedal : 8;
@@ -20,40 +20,40 @@ typedef union DATAMSG{
         uint64_t reserved : 16;
     };
     uint64_t val;
-}DATAMSG_t;
+}CarData_t;
 
 void Task_SendCarCAN(void *p_arg){
-    CANMSG_t motorMSG;
-    CANMSG_t carMSG;
-    DATAMSG_t data;
+    CANMSG_t motorMsg;
+    CANMSG_t carMsg;
+    CarData_t data;
 
     while (1) {
         // Send motor msg
-        CAN_Queue_Pend(&motorMSG);
-        CANbus_Send(motorMSG.id, motorMSG.payload, CAN_BLOCKING);
+        CAN_Queue_Pend(&motorMsg);
+        CANbus_Send(motorMsg.id, motorMsg.payload, CAN_BLOCKING);
 
         // Get pedal information
         data.accelPedal = Pedals_Read(ACCELERATOR);
         data.brakePedal = Pedals_Read(BRAKE);
 
         // Get switch information
-        for(int i = 0; i < NUM_SWITCHES; i++){
-            bool switchState = (bool)Switches_Read(i);
-            data.switchBitmap = data.switchBitmap | (switchState << i);
+        for(switches_t swtch = 0; swtch < NUM_SWITCHES; swtch++){
+            bool switchState = Switches_Read(swtch) == ON ? true : false;
+            data.switchBitmap = data.switchBitmap | (switchState << swtch);
         }
 
         // Get light information
         data.lightBitmap = (bool)Lights_Read(BrakeLight);
 
         // Get contactor info
-        for(int i = 0; i < NUM_CONTACTORS; i++){
-            bool switchState = (bool)Contactors_Get(i);
-            data.contactorBitmap = data.contactorBitmap | (switchState << i);
+        for(contactor_t contactor = 0; contactor < NUM_CONTACTORS; contactor++){
+            bool switchState = Contactors_Get(contactor) == ON ? true : false;
+            data.contactorBitmap = data.contactorBitmap | (switchState << contactor);
         }
 
         // Send car msg
-        carMSG.payload.data.d = data.val;
-        CANbus_Send(carMSGID, carMSG.payload, CAN_BLOCKING);
+        carMsg.payload.data.d = data.val;
+        CANbus_Send(CARDATA_ID, carMsg.payload, CAN_BLOCKING);
     }
     
 }
