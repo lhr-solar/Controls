1abaee5558a224a99cd17db997a0820e9523cca3
Cam0Cow <roie.roiegal@gmail.com>
2022-10-22 18:19:54 -0500

    Put CAN1 and CAN3 in loopback mode for testing

--- Tests/Test_CANbus.c
+++ Tests/Test_CANbus.c
@@ -6,6 +6,12 @@
 static OS_TCB Task1TCB;
 static CPU_STK Task1Stk[128];
 
+void createDummyMsg(CANDATA_t *msg, CANId_t id, uint64_t data) {
+    msg->ID = id;
+    msg->idx = 0;
+    memcpy(&msg->data, &data , sizeof data);
+}
+
 void Task1(void *p_arg) {
     (void) p_arg;
     OS_ERR err;
@@ -29,7 +35,7 @@ void Task1(void *p_arg) {
     memcpy(&msg.data[4], &current, sizeof current);
 
     CANbus_Send(msg, CAN_BLOCKING, MOTORCAN);
-    CANbus_Read(&response, CAN_BLOCKING, CARCAN);
+    CANbus_Read(&response, CAN_BLOCKING, MOTORCAN);
     OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
 
     
@@ -37,37 +43,46 @@ void Task1(void *p_arg) {
     msg.idx = 0;
     msg.data[0] = 1;
     CANbus_Send(msg, CAN_BLOCKING, CARCAN);
-    CANbus_Read(&response, CAN_BLOCKING, MOTORCAN);
+    CANbus_Read(&response, CAN_BLOCKING, CARCAN);
     OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
 
     // Now test non-blocking CAN send
     // The latter ones should return an error
-    msg.ID = MOTOR_DRIVE;
-    memcpy(&msg.data[0], &vel, sizeof vel);
-    memcpy(&msg.data[4], &current, sizeof current);
+    CANDATA_t msg1, msg2, msg3, msg4;
+    createDummyMsg(&msg1, MOTOR_DRIVE, 1L);
+    createDummyMsg(&msg2, MOTOR_DRIVE, 2L);
+    createDummyMsg(&msg3, MOTOR_DRIVE, 3L);
+    createDummyMsg(&msg4, MOTOR_DRIVE, 4L);
+
     volatile ErrorStatus e;
-    e = CANbus_Send(msg, CAN_NON_BLOCKING, MOTORCAN);
-    e = CANbus_Send(msg, CAN_NON_BLOCKING, MOTORCAN);
-    e = CANbus_Send(msg, CAN_NON_BLOCKING, MOTORCAN); // Potentially an error?
-    e = CANbus_Send(msg, CAN_NON_BLOCKING, MOTORCAN); // Almost definitely an error, check in GDB
+    e = CANbus_Send(msg1, CAN_NON_BLOCKING, MOTORCAN);
+    e = CANbus_Send(msg2, CAN_NON_BLOCKING, MOTORCAN);
+    e = CANbus_Send(msg3, CAN_NON_BLOCKING, MOTORCAN); // Potentially an error?
+    e = CANbus_Send(msg4, CAN_NON_BLOCKING, MOTORCAN); // Almost definitely an error, check in GDB
+
+    OSTimeDlyHMSM(0, 0, 0, 500, OS_OPT_TIME_HMSM_STRICT, &err);
+
+    CANbus_Read(&msg1, CAN_NON_BLOCKING, MOTORCAN);
+    CANbus_Read(&msg2, CAN_NON_BLOCKING, MOTORCAN);
+    CANbus_Read(&msg3, CAN_NON_BLOCKING, MOTORCAN);
+    CANbus_Read(&msg4, CAN_NON_BLOCKING, MOTORCAN);
     
-    CANbus_Read(&response, CAN_BLOCKING, CARCAN);
-    CANbus_Read(&response, CAN_BLOCKING, CARCAN);
-    CANbus_Read(&response, CAN_BLOCKING, CARCAN);
-    CANbus_Read(&response, CAN_BLOCKING, CARCAN);
     (void) e;
     
     OSTimeDlyHMSM(0, 0, 2, 0, OS_OPT_TIME_HMSM_STRICT, &err);
 
-    /*
+    
     // Now do the same test, but blocking this time
     // All of the operations should succeed, so we should see four CAN messages
-    e = CANbus_Send(msg, CAN_BLOCKING, MOTORCAN);
-    e = CANbus_Send(msg, CAN_BLOCKING, MOTORCAN);
-    e = CANbus_Send(msg, CAN_BLOCKING, MOTORCAN);
-    e = CANbus_Send(msg, CAN_BLOCKING, MOTORCAN);
-    */
+    e = CANbus_Send(msg1, CAN_BLOCKING, MOTORCAN);
+    e = CANbus_Send(msg2, CAN_BLOCKING, MOTORCAN);
+    e = CANbus_Send(msg3, CAN_BLOCKING, MOTORCAN);
+    e = CANbus_Send(msg4, CAN_BLOCKING, MOTORCAN);
     
+    CANbus_Read(&msg1, CAN_BLOCKING, MOTORCAN);
+    CANbus_Read(&msg2, CAN_BLOCKING, MOTORCAN);
+    CANbus_Read(&msg3, CAN_BLOCKING, MOTORCAN);
+    CANbus_Read(&msg4, CAN_BLOCKING, MOTORCAN);
 
 
     /**
