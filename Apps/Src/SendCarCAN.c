925695536ce71d0482938d0bc67604c948abb7c8
NathanielDelgado <nathaniel.delgado@utexas.edu>
2022-11-12 22:18:48 +0000

    Rewrote SendCarCan task with minions changes

--- Apps/Src/SendCarCAN.c
+++ Apps/Src/SendCarCAN.c
@@ -6,7 +6,6 @@
 #include "Contactors.h"
 #include "common.h"
 
-// Make sure updated to the DATAMSG and carMSGID are reflected in the CAN Bus IDs excel sheet
 
 /**
  * @file SendCarCAN.c
@@ -19,16 +18,18 @@
  * 
  */
 
+
+// Make sure updated to the DATAMSG and carMSGID are reflected in the CAN Bus IDs excel sheet
+
 #define CARDATA_ID 0x581
 
 typedef union CarData{
     struct{
         uint64_t accelPedal : 8;
         uint64_t brakePedal : 8;
-        uint64_t switchBitmap : 16;
-        uint64_t lightBitmap : 8;
+        uint64_t minionBitmap : 8;
         uint64_t contactorBitmap : 8;
-        uint64_t reserved : 16;
+        uint64_t reserved : 32;
     };
     uint64_t val;
 }CarData_t;
@@ -45,31 +46,31 @@ void Task_SendCarCAN(void *p_arg){
     CANMSG_t carMsg;
     CarData_t data;
 
+    Minion_Error_t Merr;
+
     while (1) {
         // Send motor msg
         CAN_Queue_Pend(&motorMsg);
         CANbus_Send(motorMsg.id, motorMsg.payload, CAN_BLOCKING);
 
-        //Initialize CarData
+        //Initialize CarData & Minion Err
+        Merr = MINION_ERR_NONE;
         data.val = 0;
 
         // Get pedal information
         data.accelPedal = Pedals_Read(ACCELERATOR);
         data.brakePedal = Pedals_Read(BRAKE);
 
-        // Get switch information
-        for(switches_t swtch = 0; swtch < NUM_SWITCHES; swtch++){
-            bool switchState = Switches_Read(swtch) == ON ? true : false;
-            data.switchBitmap = data.switchBitmap | (switchState << swtch);
+        // Get minion information
+        for(MinionPin_t pin = 0; pin < MINIONPIN_NUM; pin++){
+            bool pinState = Minion_Read_Input(pin, &Merr);
+            data.minionBitmap |= pinState << pin;
         }
-
-        // Get light information
-        data.lightBitmap = (bool)Lights_Read(BrakeLight);
 
         // Get contactor info
         for(contactor_t contactor = 0; contactor < NUM_CONTACTORS; contactor++){
-            bool switchState = Contactors_Get(contactor) == ON ? true : false;
-            data.contactorBitmap = data.contactorBitmap | (switchState << contactor);
+            bool contactorState = Contactors_Get(contactor) == ON ? true : false;
+            data.contactorBitmap |= contactorState << contactor;
         }
 
         // Send car msg
