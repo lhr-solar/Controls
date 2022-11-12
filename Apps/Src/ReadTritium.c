20116485ec116ad06df949f9c0eec0f4fd117743
Gage Miller <gage.miller@utexas.edu>
2022-11-12 22:47:16 +0000

    Implemented error handling

--- Apps/Src/ReadTritium.c
+++ Apps/Src/ReadTritium.c
@@ -5,6 +5,40 @@
 #include "CANbus.h"
 #include <string.h>
 
+//status limit flag masks
+#define MASK_MOTOR_TEMP_LIMIT (1<<6) //check if motor temperature is limiting the motor 6
+
+uint16_t Motor_FaultBitmap = T_NONE;
+
+
+/**
+ * @brief Return the static error field from this layer
+ * 
+ */
+tritium_error_code_t MotorController_getTritiumError(void){
+    //TODO: implement for loop to parse this
+	for(int i = 0; i < NUM_TRITIUM_ERRORS; ++i){
+		if(Motor_FaultBitmap & (1<<i)){
+			return ((tritium_error_code_t)(1<<i));
+		}
+	}
+}
+
+/**
+ * @brief   Assert Error if Tritium sends error. When Fault Bitmap is set,
+ *          and semaphore is posted, Fault state will run.
+ * @param   motor_err Bitmap which has motor error codes
+ */
+static void assertTritiumError(uint16_t motor_err){    
+	OS_ERR err;
+	if(motor_err != T_NONE){
+		FaultBitmap |= FAULT_TRITIUM;
+		OSSemPost(&FaultState_Sem4, OS_OPT_POST_1, &err);
+		assertOSError(0, err);
+	}
+}
+
+
 /* OBJECTIVES:
 Objective 1:
 - Receive motor status message from MotorController (18.4.2)
@@ -26,30 +60,31 @@ Objective 2:
 	- release Storage Array mutex
 */
 
-void Task_ReadTritium(void *p_arg)
-{
-
+void Task_ReadTritium(void *p_arg){
 	OS_ERR err;
+	CANDATA_t dataBuf = {0};
+	
+	while (1){
+		ErrorStatus status = CANbus_Read(&dataBuf, CAN_BLOCKING, MOTORCAN);
 
-	// CANDATA_t dataBuf;
-	while (1)
-	{
-		// ErrorStatus status = CANbus_Read(CANDATA_t* data, CAN_blocking_t blocking, CAN_t MOTORCAN);
+		if (status == SUCCESS){
+			switch(dataBuf.ID){
+				case MOTOR_STATUS:{
+					// motor status error flags is in bytes 4-5
+					uint16_t errorFlags = *((uint16_t*)(&dataBuf.data[4]));
+					Motor_FaultBitmap = errorFlags;
+					assertTritiumError(errorFlags);
+					break;
+				}
+				default:{
+					break; //for cases not handled currently
+				}
 
-/*		CANbuff buf;
-		ErrorStatus status = MotorController_Read(&buf);
+			}
 
-		if (status == SUCCESS)
-		{
-			dataBuf.data[0] = buf.secondNum;
-			dataBuf.data[4] = buf.firstNum;
-			dataBuf.ID = buf.id;
 
-			(void) CAN_Queue_Post(dataBuf);
-
-			// TODO: handle error
 		}
-*/
+
 		OSTimeDlyHMSM(0, 0, 0, 10, OS_OPT_TIME_HMSM_NON_STRICT, &err);
 		assertOSError(OS_READ_TRITIUM_LOC, err);
 	}
