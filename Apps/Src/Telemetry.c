e5809728999dce17dd9b4c3260a0f1ea280eeac1
NathanielDelgado <nathaniel.delgado@utexas.edu>
2022-12-03 17:53:50 +0000

    Created a telemetry task to send car data over can line, added needed componenets to the Tasks files, created a new test for it, modified and simplified the sendcarcan test file

---
+++ Apps/Src/Telemetry.c
@@ -0,0 +1,75 @@
+#include "Tasks.h"
+#include "CANbus.h"
+#include "CAN_Queue.h"
+#include "Pedals.h"
+#include "Minions.h"
+#include "Contactors.h"
+#include "common.h"
+
+
+/**
+ * @file SendCarCAN.c
+ * @brief Implements the SendCarCAN Task
+ * 
+ * Creates a datatype to house the data to be read by telemetry
+ * 
+ * Gathers the information of the pedals, lights, switches, and contactors 
+ * to be read by telemetry
+ * 
+ */
+
+
+// Make sure updated to the CarData_t and carMSGID are reflected in the CAN Bus IDs excel sheet
+
+#define CARDATA_ID 0x581
+
+typedef struct CarData{
+    uint8_t accelPedal;
+    uint8_t brakePedal;
+    uint8_t minionBitmap;
+    uint8_t contactorBitmap;
+    uint32_t reserved;
+}CarData_t;
+
+/**
+ * @brief Sends pedal, switch, light, and contactor information to be read by telemetry
+ * 
+ * @param p_arg 
+ */
+void Task_Telemetry(void *p_arg){
+    CANMSG_t carMsg;
+    CarData_t data;
+    OS_ERR err;
+
+    Minion_Error_t Merr;
+
+    while (1) {
+        // Get pedal information
+        data.accelPedal = Pedals_Read(ACCELERATOR);
+        data.brakePedal = Pedals_Read(BRAKE);
+
+        // Get minion information
+        data.minionBitmap = 0;
+        for(MinionPin_t pin = 0; pin < MINIONPIN_NUM; pin++){
+            bool pinState = Minion_Read_Input(pin, &Merr);
+            data.minionBitmap |= pinState << pin;
+        }
+
+        // Get contactor info
+        data.contactorBitmap = 0;
+        for(contactor_t contactor = 0; contactor < NUM_CONTACTORS; contactor++){
+            bool contactorState = Contactors_Get(contactor) == ON ? true : false;
+            data.contactorBitmap |= contactorState << contactor;
+        }
+
+        // Send car msg
+        carMsg.payload.data.d = *((uint64_t*)&data);
+        CANbus_Send(CARDATA_ID, carMsg.payload, CAN_BLOCKING);
+
+        // Delay of few milliseconds (100)
+        OSTimeDlyHMSM(0, 0, 0, 100, OS_OPT_TIME_HMSM_STRICT, &err);
+        if (err != OS_ERR_NONE){
+            assertOSError(OS_UPDATE_VEL_LOC, err);
+        }
+    }
+}
--- Tests/Test_Telemetry.c
+++ Tests/Test_Telemetry.c
@@ -3,6 +3,9 @@
 #include <bsp.h>
 #include "config.h"
 #include "common.h"
+#include "Pedals.h"
+#include "Minions.h"
+#include "Contactors.h"
 #include "CANbus.h"
 #include "CAN_Queue.h"
 
@@ -16,21 +19,25 @@ void Task1(void *arg)
     CANbus_Init();
     CAN_Queue_Init();
     // BSP_UART_Init(UART_2);
+    Pedals_Init();
+    Minion_Init();
+    Contactors_Init();
 
     OS_CPU_SysTickInit(SystemCoreClock / (CPU_INT32U)OSCfg_TickRate_Hz);
 
+    Minion_Error_t Merr;
     OS_ERR err;
 
-    // Initialize SendCarCAN
+    // Initialize Telemetry
     OSTaskCreate(
-        (OS_TCB*)&SendCarCAN_TCB,
-        (CPU_CHAR*)"SendCarCAN",
-        (OS_TASK_PTR)Task_SendCarCAN,
+        (OS_TCB*)&Task_Telemetry,
+        (CPU_CHAR*)"Telemetry",
+        (OS_TASK_PTR)Task_Telemetry,
         (void*)NULL,
-        (OS_PRIO)TASK_SEND_CAR_CAN_PRIO,
-        (CPU_STK*)SendCarCAN_Stk,
+        (OS_PRIO)TASK_TELEMETRY_PRIO,
+        (CPU_STK*)Telemetry_Stk,
         (CPU_STK_SIZE)WATERMARK_STACK_LIMIT,
-        (CPU_STK_SIZE)TASK_SEND_CAR_CAN_STACK_SIZE,
+        (CPU_STK_SIZE)TASK_TELEMETRY_STACK_SIZE,
         (OS_MSG_QTY)0,
         (OS_TICK)0,
         (void*)NULL,
@@ -38,9 +45,24 @@ void Task1(void *arg)
         (OS_ERR*)&err
     );
     assertOSError(OS_MAIN_LOC, err);
+
+    bool lightState = false;
+    State contactorState = OFF;
     
     while (1){
-        // Use a logic analyzer to see if the CAN message gets sent again
+        // Switches can be modified through hardware
+
+        // Check light
+        lightState = !lightState;
+        Minion_Write_Output(BRAKELIGHT, lightState, &Merr);
+
+        // Check contactors (HAVE NOTHING HOOKED UP TO CONTACTORS)
+        contactorState = contactorState == OFF ? ON : OFF;
+        Contactors_Set(MOTOR_CONTACTOR, contactorState);
+        Contactors_Set(ARRAY_CONTACTOR, contactorState);
+
+        OSTimeDlyHMSM(0, 0, 0, 500, OS_OPT_TIME_HMSM_STRICT, &err);       
+        // Use a logic analyzer to read the CAN line and see if the data shows up correctly    
     }
 }
 
