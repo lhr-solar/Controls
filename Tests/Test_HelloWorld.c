6601150aa5b4667c958081809b9d0ed37de984f3
ishdeshpa <ishdeshpa@gmail.com>
2023-01-15 18:21:41 +0000

    updated startup script, more testing

--- Tests/Test_HelloWorld.c
+++ Tests/Test_HelloWorld.c
@@ -4,6 +4,12 @@
 
 int main(void){
     BSP_UART_Init(UART_2);
-    BSP_UART_Write(UART_2, "Hello World!\r", 14);
+    BSP_GPIO_Init(PORTA, 0, INPUT);
+    
+    while(1){
+        printf("GPIO: %d\n\r", BSP_GPIO_Read(PORTA));
+        for(int i=0; i<5000; i++){}
+    }
+
     return 0;
 }
