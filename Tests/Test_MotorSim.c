66a2dec4db11bd4c609f0897debbc038cc3d6967
Gage Miller <gage.miller@utexas.edu>
2023-01-21 22:23:44 +0000

    skeleton code for motor sim

--- Tests/Test_MotorSim.c
+++ Tests/Test_MotorSim.c
@@ -4,7 +4,7 @@
 
 //global variables
 static int velocity; 
-static CANDATA_t oldCD;
+static CANDATA_t oldCD = {CARDATA, 0, 0};
 //lookup table goes here
 static int ctr = 0;
 static 
@@ -14,10 +14,18 @@ static
 int main(void){
     while(1){
         CANDATA_t newCD;
-        CANbus_Read(&newCD, CAN_NON_BLOCKING, MOTORCAN); //returns data value into newCD
-        
+        ErrorStatus error = CANbus_Read(&newCD, CAN_NON_BLOCKING, MOTORCAN); //returns data value into newCD
+
+        if(error == ERROR){
+            newCD = oldCD;
+        }
 
 
+        ++ctr;
+        if(ctr == 3){ //send message to read canbus every 300 ms
+            ctr = 0;
+            CANbus_Send(newCD, CAN_NON_BLOCKING, MOTORCAN);
+        }
 
     }
 }
