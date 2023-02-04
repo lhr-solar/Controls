524ffd39d5954b9b7b9ce758b30372077d8009e5
NathanielDelgado <nathaniel.delgado@utexas.edu>
2023-02-04 23:23:13 +0000

    Added test file to see if command line works

--- Tests/Test_CommandLine.c
+++ Tests/Test_CommandLine.c
@@ -6,8 +6,21 @@
 #include "Minions.h"
 #include "Pedals.h"
 
+char input[500] = {0};
+
 int main(void){
     BSP_UART_Init(UART_2);
+
+    // while(1){
+    //     printf("prompt: ");
+    //     // for(int i = 0; i < 99999999; i++){}
+    //     if(BSP_UART_Read(UART_2, input)){
+    //         printf("Here is what you typed: %s\n\r", input);
+    //     }
+
+    //     // while (USART_GetFlagStatus(UART_2, USART_FLAG_RXNE) == RESET);
+    //     // printf("%c", USART_ReceiveData(UART_2));
+    // }
     CANbus_Init(CAN_1);
     Contactors_Init();
     Minion_Init();
