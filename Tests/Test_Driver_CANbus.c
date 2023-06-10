fca255f2073b20138987e4e1732b06095f9ef57b
Cam0Cow <roie.roiegal@gmail.com>
2023-06-09 19:31:01 -0500

    Created basic CANbus test w/ whitelist support

--- Tests/Test_Driver_CANbus.c
+++ Tests/Test_Driver_CANbus.c
@@ -1,143 +1,106 @@
-#include "common.h"
-#include "os.h"
+#include "Tasks.h"
 #include "CANbus.h"
-#include <string.h>
+#include "BSP_UART.h"
+#include "CANConfig.h"
 
-static OS_TCB Task1TCB;
-static CPU_STK Task1Stk[128];
+static OS_TCB Task1_TCB;
+static CPU_STK Task1_Stk[128];
 
-void createDummyMsg(CANDATA_t *msg, CANId_t id, uint64_t data) {
-    msg->ID = id;
-    msg->idx = 0;
-    memcpy(&msg->data, &data , sizeof data);
-}
+#define STACK_SIZE 128
+
+#define CARCAN_FILTER_SIZE (sizeof carCANFilterList / sizeof(CANId_t))
+
+static void assert(bool);
+
+/*
+ * NOTE: This test must be run with car CAN in loopback mode
+ * TODO: automate this, either with arguments to BSP or #define
+ */
 
 void Task1(void *p_arg) {
     (void) p_arg;
-    OS_ERR err;
 
     CPU_Init();
     OS_CPU_SysTickInit(SystemCoreClock / (CPU_INT32U) OSCfg_TickRate_Hz);
 
-    /**
-     * +-------------+
-     * | CANbus_Send |
-     * +-------------+
-    */
+    CANbus_Init(CARCAN, carCANFilterList, NUM_CARCAN_FILTERS);
 
-   /*
-    CANDATA_t msg;
-    CANDATA_t response;
-    msg.ID = MOTOR_DRIVE;
-    msg.idx = 0;
-    float vel = 312.0f;
-    float current = 0.8f;
-    memcpy(&msg.data[0], &vel, sizeof vel);
-    memcpy(&msg.data[4], &current, sizeof current);
+    CANDATA_t dataBuf, resultBuf;
+    uint64_t data = 0xdeadbeef12345678;
 
-    CANbus_Send(msg, CAN_BLOCKING, MOTORCAN);
-    CANbus_Read(&response, CAN_BLOCKING, MOTORCAN);
-    OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
+    dataBuf.ID = CHARGE_ENABLE; // First, send a value that we want to be able to receive
+    memcpy(&dataBuf.data, &data, sizeof data);
 
-    
-    msg.ID = ARRAY_CONTACTOR_STATE_CHANGE;
-    msg.idx = 0;
-    msg.data[0] = 1;
-    CANbus_Send(msg, CAN_BLOCKING, CARCAN);
-    CANbus_Read(&response, CAN_BLOCKING, CARCAN);
-    OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
+    assert(CANbus_Send(dataBuf, true, CARCAN) == SUCCESS);
+    assert(CANbus_Read(&resultBuf, true, CARCAN) == SUCCESS);
+    assert(memcmp(&dataBuf, &resultBuf, sizeof dataBuf) == 0); // TODO: make sure that all 8 data bytes are copied
 
-    // Now test non-blocking CAN send
-    // The latter ones should return an error
-    CANDATA_t msg1, msg2, msg3, msg4;
-    createDummyMsg(&msg1, MOTOR_DRIVE, 1L);
-    createDummyMsg(&msg2, MOTOR_DRIVE, 2L);
-    createDummyMsg(&msg3, MOTOR_DRIVE, 3L);
-    createDummyMsg(&msg4, MOTOR_DRIVE, 4L);
+    dataBuf.ID = ODOMETER_AMPHOURS; // Now, send a message that we shouldn't receive
+    assert(CANbus_Send(dataBuf, true, CARCAN) == SUCCESS);
+    assert(CANbus_Read(&resultBuf, false, CARCAN) == ERROR); // check that no can message is read
 
-    volatile ErrorStatus e;
-    e = CANbus_Send(msg1, CAN_NON_BLOCKING, MOTORCAN);
-    e = CANbus_Send(msg2, CAN_NON_BLOCKING, MOTORCAN);
-    e = CANbus_Send(msg3, CAN_NON_BLOCKING, MOTORCAN); // Potentially an error?
-    e = CANbus_Send(msg4, CAN_NON_BLOCKING, MOTORCAN); // Almost definitely an error, check in GDB
-
-    OSTimeDlyHMSM(0, 0, 0, 500, OS_OPT_TIME_HMSM_STRICT, &err);
-
-    CANbus_Read(&msg1, CAN_NON_BLOCKING, MOTORCAN);
-    CANbus_Read(&msg2, CAN_NON_BLOCKING, MOTORCAN);
-    CANbus_Read(&msg3, CAN_NON_BLOCKING, MOTORCAN);
-    CANbus_Read(&msg4, CAN_NON_BLOCKING, MOTORCAN);
-    
-    (void) e;
-    
-    OSTimeDlyHMSM(0, 0, 2, 0, OS_OPT_TIME_HMSM_STRICT, &err);
-
-    
-    // Now do the same test, but blocking this time
-    // All of the operations should succeed, so we should see four CAN messages
-    e = CANbus_Send(msg1, CAN_BLOCKING, MOTORCAN);
-    e = CANbus_Send(msg2, CAN_BLOCKING, MOTORCAN);
-    e = CANbus_Send(msg3, CAN_BLOCKING, MOTORCAN);
-    e = CANbus_Send(msg4, CAN_BLOCKING, MOTORCAN);
-    
-    CANbus_Read(&msg1, CAN_BLOCKING, MOTORCAN);
-    CANbus_Read(&msg2, CAN_BLOCKING, MOTORCAN);
-    CANbus_Read(&msg3, CAN_BLOCKING, MOTORCAN);
-    CANbus_Read(&msg4, CAN_BLOCKING, MOTORCAN);
-
-    */
-
-    CANDATA_t msg;
-    msg.ID = STATE_OF_CHARGE;
-    msg.idx = 0;
-    float a = 420.69f;
-    float b = 13.37f;
-    memcpy(&msg.data[0], &a, sizeof a);
-    memcpy(&msg.data[4], &b, sizeof b);
-    CANbus_Send(msg, CAN_BLOCKING, CARCAN);
-    CANbus_Read(&msg, CAN_BLOCKING, MOTORCAN);
-    OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
-
-
-    /**
-     * +-------------+
-     * | CANbus_Read |
-     * +-------------+
-     */
-    // Might have to put CAN in loopback mode to test this?
-
-    
-
-
-    while (1) {
-        OSTimeDlyHMSM(1, 0, 0, 0, OS_OPT_TIME_HMSM_STRICT, &err);
+    // Now send a bunch of messages that should be ignored, followed by those that shouldn't
+    for (int i=0; i<10; i++) {
+        assert(CANbus_Send(dataBuf, true, CARCAN) == SUCCESS);
     }
+
+    dataBuf.ID = BPS_TRIP;
+    assert(CANbus_Send(dataBuf, true, CARCAN) == SUCCESS);
+    dataBuf.ID = STATE_OF_CHARGE;
+    assert(CANbus_Send(dataBuf, true, CARCAN) == SUCCESS);
+    dataBuf.ID = SUPPLEMENTAL_VOLTAGE;
+    assert(CANbus_Send(dataBuf, true, CARCAN) == SUCCESS);
+
+    // Make sure that only three messages make it through
+    assert(CANbus_Read(&resultBuf, true, CARCAN) == SUCCESS);
+    assert(resultBuf.ID == BPS_TRIP);
+    assert(CANbus_Read(&resultBuf, true, CARCAN) == SUCCESS);
+    assert(resultBuf.ID == STATE_OF_CHARGE);
+    assert(CANbus_Read(&resultBuf, true, CARCAN) == SUCCESS);
+    assert(resultBuf.ID == SUPPLEMENTAL_VOLTAGE);
+    assert(CANbus_Read(&resultBuf, false, CARCAN) == ERROR); // No more messages
+
+    printf("Success!\r\n");
+    for (;;);
 }
 
-
-
-int main(void) {
+int main(void){ //initialize things and spawn task
     OS_ERR err;
     OSInit(&err);
-    if (CANbus_Init(CARCAN) == ERROR) return -1;
-    if (CANbus_Init(MOTORCAN) == ERROR) return -1;
+    if(err != OS_ERR_NONE){
+        printf("OS error code %d\n\r",err);
+    }
+
+    BSP_UART_Init(UART_2);
 
     OSTaskCreate(
-        &Task1TCB,
-        "Task 1",
-        Task1,
-        NULL,
-        4,
-        Task1Stk,
-        64,
-        128,
-        0,
-        0,
-        NULL,
-        OS_OPT_TASK_STK_CLR | OS_OPT_TASK_STK_CHK,
-        &err
+        (OS_TCB*)&Task1_TCB,
+        (CPU_CHAR*)"Task1",
+        (OS_TASK_PTR)Task1,
+        (void*)NULL,
+        (OS_PRIO)4,
+        (CPU_STK*)Task1_Stk,
+        (CPU_STK_SIZE)STACK_SIZE/10,
+        (CPU_STK_SIZE)STACK_SIZE,
+        (OS_MSG_QTY)0,
+        (OS_TICK)NULL,
+        (void*)NULL,
+        (OS_OPT)(OS_OPT_TASK_STK_CLR|OS_OPT_TASK_STK_CHK),
+        (OS_ERR*)&err
     );
 
+    
+    if (err != OS_ERR_NONE) {
+        printf("Task1 error code %d\n\r", err);
+    }
     OSStart(&err);
+    if (err != OS_ERR_NONE) {
+        printf("OS error code %d\n\r", err);
+    }
+    return 0;
 
+}
+
+static void assert(bool cond) {
+    if (!cond) __asm("bkpt");
 }
