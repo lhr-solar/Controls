f0ce32e264ecde2e1fedc806123bd4f3cff324be
Cam0Cow <roie.roiegal@gmail.com>
2023-06-24 21:53:49 -0500

    Fixed test float printing, fixed test stack overflow

--- Tests/Test_MotorControl.c
+++ Tests/Test_MotorControl.c
@@ -3,13 +3,22 @@
 #include "CAN_Queue.h"
 #include "ReadTritium.h"
 #include "Display.h"
+#include "Pedals.h"
+#include "Minions.h"
 #include "UpdateDisplay.h"
 #include "CANConfig.h"
 
 static OS_TCB Task1_TCB;
-#define STACK_SIZE 128
+#define STACK_SIZE 256
 static CPU_STK Task1_Stk[STACK_SIZE];
 
+void p_float(float f) {
+    bool neg = f < 0;
+    if (neg) f = -f;
+    int n = (int) f;
+    f -= n;
+    printf("%s%d.%d\n\r", neg ? "-" : "", n, (int) (f*100));
+}
 
 /*
  * When running this test on the motor testbench, hardcode the SendTritium task
@@ -22,11 +31,15 @@ void Task1(){
 
     CPU_Init();
     OS_CPU_SysTickInit(SystemCoreClock / (CPU_INT32U) OSCfg_TickRate_Hz);
+    BSP_UART_Init(UART_2);
+    Pedals_Init();
+    Minion_Init();
     Display_Init();
     UpdateDisplay_Init();
     CANbus_Init(MOTORCAN, motorCANFilterList, sizeof motorCANFilterList / sizeof(CANId_t));
     for (;;) {
-        printf("Motor velocity is currently %f\r\n", Motor_Velocity_Get());
+        printf("Motor velocity is currently ");
+        p_float(Motor_Velocity_Get());
         OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &err);
         assertOSError(OS_MAIN_LOC, err);
     }
@@ -112,7 +125,7 @@ int main(){
         (OS_TASK_PTR)Task_ReadTritium,
         (void*)NULL,
         (OS_PRIO)TASK_READ_TRITIUM_PRIO,
-        (CPU_STK*)SendTritium_Stk,
+        (CPU_STK*)ReadTritium_Stk,
         (CPU_STK_SIZE)STACK_SIZE/10,
         (CPU_STK_SIZE)STACK_SIZE,
         (OS_MSG_QTY)0,
