5185426b40463dafb0384e46b9b3efdbe84a2531
Madeleine Lee <madeleinercflee@gmail.com>
2023-10-14 20:29:23 +0000

    Changed main loop to check the queue periodically instead of pending on a semaphore. The loop will check two semaphores- one to see if it's time to put an IO State message in the queue (based on if the putIOState timer has expired or not) and one to see if there's a message in the queuethat we should send.

--- Apps/Src/SendCarCAN.c
+++ Apps/Src/SendCarCAN.c
@@ -17,19 +17,19 @@
 #define FIFO_NAME SendCarCAN_Q
 #include "fifo.h"
 
+#define SENDCARCAN_QUEUE_CHECK_MS 50 // How often we should check the CAN queue (in ms delay). Must send faster than queue messages get put in
 static SendCarCAN_Q_t CANFifo;
 
 static OS_SEM CarCAN_Sem4;
 static OS_MUTEX CarCAN_Mtx;
-
-static OS_SEM CarCAN_Sem4;
-static OS_MUTEX CarCAN_Mtx;
+static OS_SEM PutIOTimerAlert_Sem4;
 
 #define IO_STATE_TMR_DLY_MS 250u
 #define IO_STATE_TMR_DLY_TS ((IO_STATE_TMR_DLY_MS * OS_CFG_TMR_TASK_RATE_HZ) / (1000u))
 static OS_TMR IOStateTmr;
 
-static void putIOState(void *p_tmr, void *p_arg);
+static void putIOStateTimerAlert(void *p_tmr, void *p_arg);
+static void putIOState(void);
 
 /**
  * @brief Wrapper to put new message in the CAN queue
@@ -63,6 +63,9 @@ void Task_SendCarCAN(void *p_arg){
     OSSemCreate(&CarCAN_Sem4, "CarCAN_Sem4", 0, &err);
     assertOSError(OS_SEND_CAN_LOC, err);
 
+    OSSemCreate(&PutIOTimerAlert_Sem4, "PutIOTimerAlert_Sem4", 0, &err);
+    assertOSError(OS_SEND_CAN_LOC, err);
+
     SendCarCAN_Q_renew(&CANFifo);
     
     // Set up timer to put car data message every IO_STATE_TMR_DLY_MS ms
@@ -71,7 +74,7 @@ void Task_SendCarCAN(void *p_arg){
                 0,
                 IO_STATE_TMR_DLY_TS,
                 OS_OPT_TMR_PERIODIC,
-                putIOState,
+                putIOStateTimerAlert,
                 NULL,
                 &err
                 );
@@ -84,22 +87,35 @@ void Task_SendCarCAN(void *p_arg){
     memset(&message, 0, sizeof(CANDATA_t));
 
     while (1) {
-        OSSemPend(&CarCAN_Sem4, 0, OS_OPT_PEND_BLOCKING, &ticks, &err);
+        
+        OSSemPend(&PutIOTimerAlert_Sem4, 0, OS_OPT_PEND_NON_BLOCKING, &ticks, &err);
+        if (err != OS_ERR_PEND_WOULD_BLOCK) { // The timer fired; it's time to send an IO state message
+            putIOState();
+        }
+        
+        // Check if there's something to send in the queue (either IOState or Car state from sendTritium)
+        OSSemPend(&CarCAN_Sem4, 0, OS_OPT_PEND_NON_BLOCKING, &ticks, &err);
+        if (err != OS_ERR_PEND_WOULD_BLOCK) { // We have a message in the queue to send
+            assertOSError(OS_SEND_CAN_LOC, err);
+
+            OSMutexPend(&CarCAN_Mtx, 0, OS_OPT_PEND_BLOCKING, &ticks, &err);
+            assertOSError(OS_SEND_CAN_LOC, err);
+        
+            bool res = SendCarCAN_Q_get(&CANFifo, &message);
+            assertOSError(OS_SEND_CAN_LOC, err);
+
+            OSMutexPost(&CarCAN_Mtx, OS_OPT_POST_NONE, &err);
+
+            if(res) CANbus_Send(message, true, CARCAN);
+        }
+            
+        OSTimeDlyHMSM(0, 0, 0, SENDCARCAN_QUEUE_CHECK_MS, OS_OPT_TIME_HMSM_STRICT, &err);
         assertOSError(OS_SEND_CAN_LOC, err);
-
-        OSMutexPend(&CarCAN_Mtx, 0, OS_OPT_PEND_BLOCKING, &ticks, &err);
-        assertOSError(OS_SEND_CAN_LOC, err);
-
-        bool res = SendCarCAN_Q_get(&CANFifo, &message);
-        assertOSError(OS_SEND_CAN_LOC, err);
-
-        OSMutexPost(&CarCAN_Mtx, OS_OPT_POST_NONE, &err);
-
-        if(res) CANbus_Send(message, true, CARCAN);
+        
     }
 }
 
-static void putIOState(void *p_tmr, void *p_arg){
+static void putIOState(void){
     CANDATA_t message;
     memset(&message, 0, sizeof(message));
     message.ID = IO_STATE;
@@ -123,4 +139,11 @@ static void putIOState(void *p_tmr, void *p_arg){
     }
 
     SendCarCAN_Put(message);
+}
+
+// Callback function for the putIOState timer
+static void putIOStateTimerAlert(void *p_tmr, void *p_arg){
+    OS_ERR err;
+    OSSemPost(&PutIOTimerAlert_Sem4, OS_OPT_POST_1, &err);
+    assertOSError(OS_SEND_CAN_LOC, err);
 }
