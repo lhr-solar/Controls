637408328ba01d9ccd815264e79a00707d365e2c
Diya Rajon <diya.rajon@gmail.com>
2023-12-31 02:14:08 +0000

    Contactors mocks

--- Tests/Test_UnitTestContactors.c
+++ Tests/Test_UnitTestContactors.c
@@ -1,7 +1,7 @@
 #include "unity.h"
 #include "fff.h"
-#include "scalar.h"
-
+#include "Contactors.h"
+#include "BSP_GPIO.h"
 
 DEFINE_FFF_GLOBALS;
 
@@ -9,12 +9,17 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-FAKE_VALUE_FUNC2(int, perform_addition, int, int);
-void test_unittest(void){
-    int num1 = 1;
-    int num2 = 2;
-    perform_addition_fake.return_val = 6;
-    TEST_ASSERT_EQUAL(6, perform_scaling(num1, num2));
+FAKE_VOID_FUNC3(BSP_GPIO_Write_Pin, port_t, uint16_t, bool);
+FAKE_VOID_FUNC3(BSP_GPIO_Init, port_t, uint16_t, direction_t);
+//FAKE_VOID_FUNC3(OSMutexCreate, struct os_mutex, CPU_CHAR, OS_ERR);
+FAKE_VALUE_FUNC2(uint8_t,BSP_GPIO_Get_State, port_t, uint16_t );
+//FAKE_VOID_FUNC5(OSMutexPend, OS_MUTEX, OS_TICK, OS_OPT, CPU_TS, OS_ERR);
+//FAKE_VOID_FUNC3(OSMutexPost, OS_MUTEX, OS_OPT, OS_ERR);
+FAKE_VOID_FUNC1(assertOSError, OS_ERR);
+
+
+void test_UnitTestContactors(void){
+    setContactor(ARRAY_PRECHARGE_BYPASS_CONTACTOR, ON);
 }
 
 
