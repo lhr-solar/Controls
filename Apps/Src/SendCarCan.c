de86fcb8d5ad545e72dd229c609ff6fbf2b16d68
Nathaniel Delgado <nathaniel.delgado@utexas.edu>
2024-01-13 16:46:55 -0600

    Removed redundant line in SendCarCan.c

--- Apps/Src/SendCarCan.c
+++ Apps/Src/SendCarCan.c
@@ -2,7 +2,6 @@
  * @copyright Copyright (c) 2018-2023 UT Longhorn Racing Solar
  * @file SendCarCAN.c
  * @brief Function implementations for the SendCarCAN application.
- * @brief Function implementations for the SendCarCAN application.
  *
  * This contains functions relevant to placing CAN messages in a CarCAN queue
  * and periodically sending those messages in the SendCarCAN task. This contains
