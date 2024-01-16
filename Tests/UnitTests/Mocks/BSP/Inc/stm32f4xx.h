72fd00c07306b18a50b5df046823b4c0ff615ff1
Madeleine Lee <madeleinercflee@gmail.com>
2024-01-16 17:19:47 -0600

    Mocked/stubbed BSP stm32f4xx.h and _gpio.h/c files from CMSIS- this should cover all the parts of the os we need, I think?

---
+++ Tests/UnitTests/Mocks/BSP/Inc/stm32f4xx.h
@@ -0,0 +1,32 @@
+
+#ifndef __STM32F4xx_H
+#define __STM32F4xx_H
+
+#include <stdint.h>
+
+
+#ifndef __CONFIG_H
+typedef enum {ERROR = 0, SUCCESS = !ERROR} ErrorStatus;
+#endif
+
+#define     __IO    volatile     
+
+/** 
+  * @brief General Purpose I/O
+  */
+
+typedef struct
+{
+  __IO uint32_t MODER;    /*!< GPIO port mode register,               Address offset: 0x00      */
+  __IO uint32_t OTYPER;   /*!< GPIO port output type register,        Address offset: 0x04      */
+  __IO uint32_t OSPEEDR;  /*!< GPIO port output speed register,       Address offset: 0x08      */
+  __IO uint32_t PUPDR;    /*!< GPIO port pull-up/pull-down register,  Address offset: 0x0C      */
+  __IO uint32_t IDR;      /*!< GPIO port input data register,         Address offset: 0x10      */
+  __IO uint32_t ODR;      /*!< GPIO port output data register,        Address offset: 0x14      */
+  __IO uint16_t BSRRL;    /*!< GPIO port bit set/reset low register,  Address offset: 0x18      */
+  __IO uint16_t BSRRH;    /*!< GPIO port bit set/reset high register, Address offset: 0x1A      */
+  __IO uint32_t LCKR;     /*!< GPIO port configuration lock register, Address offset: 0x1C      */
+  __IO uint32_t AFR[2];   /*!< GPIO alternate function registers,     Address offset: 0x20-0x24 */
+} GPIO_TypeDef;
+
+#endif
---
+++ Tests/UnitTests/Mocks/BSP/Src/stm32f4xx_gpio.c
@@ -0,0 +1,34 @@
+////////Mock
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_gpio.h"
+
+//#include "stm32f4xx_rcc.h"
+
+DEFINE_FAKE_VOID_FUNC(GPIO_DeInit, GPIO_TypeDef*);
+
+DEFINE_FAKE_VOID_FUNC(GPIO_Init, GPIO_TypeDef*, GPIO_InitTypeDef*);
+
+DEFINE_FAKE_VOID_FUNC(GPIO_StructInit, GPIO_InitTypeDef*);
+
+DEFINE_FAKE_VOID_FUNC(GPIO_PinLockConfig, GPIO_TypeDef*, uint16_t);
+
+DEFINE_FAKE_VALUE_FUNC(uint8_t, GPIO_ReadInputDataBit, GPIO_TypeDef*, uint16_t);
+
+DEFINE_FAKE_VALUE_FUNC(uint16_t, GPIO_ReadInputData, GPIO_TypeDef*);
+
+DEFINE_FAKE_VALUE_FUNC(uint8_t, GPIO_ReadOutputDataBit, GPIO_TypeDef*, uint16_t);
+
+DEFINE_FAKE_VALUE_FUNC(uint16_t, GPIO_ReadOutputData, GPIO_TypeDef*);
+
+DEFINE_FAKE_VOID_FUNC(GPIO_SetBits, GPIO_TypeDef*, uint16_t);
+
+DEFINE_FAKE_VOID_FUNC(GPIO_ResetBits, GPIO_TypeDef*, uint16_t);
+
+DEFINE_FAKE_VOID_FUNC(GPIO_WriteBit, GPIO_TypeDef*, uint16_t, BitAction);
+
+DEFINE_FAKE_VOID_FUNC(GPIO_Write, GPIO_TypeDef*, uint16_t);
+
+DEFINE_FAKE_VOID_FUNC(GPIO_ToggleBits, GPIO_TypeDef*, uint16_t);
+
+DEFINE_FAKE_VOID_FUNC(GPIO_PinAFConfig, GPIO_TypeDef*, uint16_t, uint8_t);
