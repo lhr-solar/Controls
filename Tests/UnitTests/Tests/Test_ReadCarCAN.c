47e5b4ac0cb35d7011b4a0c484f8ad1e59734def
Diya Rajon <diya.rajon@gmail.com>
2024-01-23 17:31:37 +0000

    Modified mocks used by RCC. RCC unit test merge in progress.
    Co-authored-by: Madeleine Lee <KnockbackNemo@users.noreply.github.com>

--- Tests/UnitTests/Tests/Test_ReadCarCAN.c
+++ Tests/UnitTests/Tests/Test_ReadCarCAN.c
@@ -13,16 +13,17 @@
 /////////////////////
 // Mock headers
 #include "UpdateDisplay.h"
+#include "CanBus.h"
 
 DEFINE_FFF_GLOBALS;
 
 
 /*** CAN Messages ***/
-static CanData bps_trip_msg = {.id=BPS_TRIP, .idx=0, .data={1}};
+static CanData bps_trip_msg = {.id=kBpsTrip, .idx=0, .data={1}};
 //static CANDATA_t supp_voltage_msg = {.ID=SUPPLEMENTAL_VOLTAGE, .idx=0, .data={100}};
 // static CANDATA_t state_of_charge_msg = {.ID=STATE_OF_CHARGE, .idx=0, .data={0}};
 // static CANDATA_t HV_Array_Msg = {.ID=BPS_CONTACTOR, .idx=0, .data={0b001}};
-static CanData HV_Disable_Msg = {.id=BPS_CONTACTOR, .idx=0, .data={0b000}};
+static CanData HV_Disable_Msg = {.id=kBpsContactor, .idx=0, .data={0b000}};
 // static CANDATA_t HV_MC_Msg = {.ID=BPS_CONTACTOR, .idx=0, .data={0b110}};
 // static CANDATA_t HV_Enabled_Msg = {.ID=BPS_CONTACTOR, .idx=0, .data={0b111}};
 
