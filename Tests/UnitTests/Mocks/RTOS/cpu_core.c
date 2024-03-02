0257ebcec98a150c6a2b4d555c60add1462e0cf8
diyarajon <diya.rajon@gmail.com>
2024-03-02 15:07:28 -0600

    Created cpu core function mocks and deleted defines by using them from original file

--- Tests/UnitTests/Mocks/RTOS/cpu_core.c
+++ Tests/UnitTests/Mocks/RTOS/cpu_core.c
@@ -1,11 +1,8 @@
 /////////////////////////////////////////////
 //////              MOCK               //////
 /////////////////////////////////////////////
-#pragma once
-#include_next "cpu_core.h"
-#include  <cpu.h>
-#include  <cpu_cfg.h>
 
+#include "cpu_core.h"
 #include "fff.h"
 
-DECLARE_FAKE_VOID_FUNC(CPU_Init);
+DEFINE_FAKE_VOID_FUNC(CPU_Init);
