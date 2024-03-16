13f81d1bf5d261d23629e5b4c786759012e24178
MichaelMohn <michael@mohn.com>
2024-03-16 15:57:17 -0600

    Printf and Scanf working

--- Tests/Test_BSP_UART.c
+++ Tests/Test_BSP_UART.c
@@ -10,33 +10,40 @@
 
 #define TX_SIZE 128
 
+/**
+ * Read a line using UART
+ * Print the first 4 chars using UART
+ */
 void TEST_UART_2_Read_Write(){
-    BSP_UART_Init(UART_2);
     const char* testStr = "Enter your favorite number:\n\r";
     BSP_UART_Write(UART_2, (char*) testStr , strlen(testStr));
     
+    //128 is the minimum buffer size for BSP_UART_Read
     char input[128];
     BSP_UART_Read(UART_2, input);
 
-    BSP_UART_Write(UART_2, "\n", 1);
+    //Print the first 4 characters of the input
+    BSP_UART_Write(UART_2, "\n\r", 2);
     BSP_UART_Write(UART_2, input, 4);
 }
 
-int main(void) {
+/**
+ * Read a number using UART through scanf
+ * Print that number using UART printf
+*/
+void Test_UART_2_Printf_Scanf(){
+    int num;
+    printf("Enter a Number and I'll add one to it!\n\r");
+    scanf("%d", &num);
+    printf("\n\r%d", num + 1);
+}
 
-    TEST_UART_2_Read_Write();
-    
+int main(void) {
+    BSP_UART_Init(UART_2);
+
+    //Test_UART_2_Read_Write();
+    //Test_UART_2_Printf_Scanf();
 
     while(1);
-        // char out[2][TX_SIZE];
-        // BSP_UART_Read(UART_2, out[UART_2]);
-        // BSP_UART_Read(UART_3, out[UART_3]);
-        // out[UART_2][strlen(out[UART_2])-1] = 0; // remove the newline, so we can print both in one line for now
-        // out[UART_3][strlen(out[UART_3])-1] = 0;
-        // printf("UART 2: %s\tUART 3: %s\r", out[UART_2], out[UART_3]);
-        // /*
-        //  * If a long message is sent before a short message, the messages will overlap
-        //  * on the display. This is not an issue with UART_2, but just a consequence of
-        //  * how these tests must be structured and outputted.
-        //  */
+
 }
