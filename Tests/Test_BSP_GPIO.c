4a8ebd30f36986b93b8ffcd58bf68453cd74ad61
Akshay G <a.g@utexas.edu>
2024-03-30 12:26:10 -0500

    untested but done?

--- Tests/Test_BSP_GPIO.c
+++ Tests/Test_BSP_GPIO.c
@@ -1,44 +1,65 @@
-#include "common.h"
-#include "config.h"
-#include <bsp.h>
+#include "BSP_GPIO.h"
 
 int main(void) {
+    uint16_t input_port_a[] = {GPIO_Pin_0,
+                               GPIO_Pin_1,
+                               GPIO_Pin_4,
+                               GPIO_Pin_5,
+                               GPIO_Pin_6,
+                               GPIO_Pin_7};
+    
+    // input pin for port B is GPIO_Pin_4
+    // output pin for port B is GPIO_Pin_5
 
-    BSP_GPIO_Init(PORTA, 0xFFFF, 0);
-    BSP_GPIO_Init(PORTB, 0xFFFF, 1);
+    uint8_t data;
+                
+    printf("\n\n-------INIT PINS-------\n\n");
 
-    char port;
-    char rw;
-    uint16_t data = 0;
+    printf("Initializing PORT A input pins (all)\n");
+    BSP_GPIO_Init(PORTA, 0x00F3, INPUT);
+    printf("INIT SUCCESS\n");
 
-    while (1) {
-        /*printf("Choose Port (A, B): ");
-        port = getchar();
-        getchar();
+    printf("Initializing PORT B input pin\n");
+    BSP_GPIO_Init(PORTB, 0x0010, INPUT);
+    printf("INIT SUCCESS\n");
 
-        if (port - 'A' >= NUM_PORTS) {
-            printf("Invalid input\n\r");
-            continue;
-        }*/
-
-        printf("All pins in Port A are only read, all pins in Port B are only write\n");
-        printf("Choose read (from Port A) or write (to Port B)(r/w): ");
-        rw = getchar();
-        getchar();
-
-        if (rw == 'r' || rw == 'R') {
-            port = 'A';
-            data = BSP_GPIO_Read(PORTA);
-            printf("Data from Port A: %d\n\r\n\r", data);
-        } else if (rw == 'w' || rw == 'W') {
-            printf("Enter the data: ");
-            char buffer[10] = {0};
-            fgets(buffer, 10, stdin);
-            data = atoi(buffer);
-            printf("\n\r");
-            BSP_GPIO_Write(PORTB, data);
-        } else {
-            printf("Invalid input\n\r");
-        }
+    printf("Initializing PORT B output pin\n");
+    BSP_GPIO_Init(PORTB, 0x0020, OUTPUT);
+    printf("INIT SUCCESS\n");
+    
+    printf("\n\n-------INPUT PINS-------\n\n");
+    for (int i = 0; i < 6; i++) {
+        printf("Reading port A, pin %X\n", input_port_a[i]);
+        data = BSP_GPIO_Read_Pin(PORTA, input_port_a[i]);
+        printf("Pin: %X, data read: %X\n", input_port_a[i], data);
     }
+
+    printf("\nReading port B, pin %X\n", GPIO_Pin_4);
+    data = BSP_GPIO_Read_Pin(PORTB, GPIO_Pin_4);
+    printf("Pin: %X, data read: %X\n", GPIO_Pin_4, data);
+
+
+    printf("\n\n-------OUTPUT PINS-------\n\n");
+
+    bool write = 0;
+    printf("Writing port B, pin %X\n", GPIO_Pin_5);
+    BSP_GPIO_Write_Pin(PORTB, GPIO_Pin_5, write);
+    printf("Pin: %X, data write: %d\n", GPIO_Pin_5, write);
+
+    printf("Get state port B, pin %X\n", GPIO_Pin_5);
+    data = BSP_GPIO_Get_State(PORTB, GPIO_Pin_5);
+    printf("Pin: %X, data read: %X\n", GPIO_Pin_5, data);
+
+    printf("Type 'n' and hit enter to continue.\n");
+    while (getchar() != 'n') {}
+
+    write = 1;
+    printf("\nWriting port B, pin %X\n", GPIO_Pin_5);
+    BSP_GPIO_Write_Pin(PORTB, GPIO_Pin_5, write);
+    printf("Pin: %X, data write: %d\n", GPIO_Pin_5, write);
+
+    printf("Get state port B, pin %X\n", GPIO_Pin_5);
+    data = BSP_GPIO_Get_State(PORTB, GPIO_Pin_5);
+    printf("Pin: %X, data read: %X\n", GPIO_Pin_5, data);
+    
 }
