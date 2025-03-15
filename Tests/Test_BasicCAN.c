aba39abd8592655ff470ecb49b0b829796d9c218
Lakshay983 <lakshay45gupta@gmail.com>
2025-03-15 11:09:33 -0500

    change to new Controls Leader pins

--- Tests/Test_BasicCAN.c
+++ Tests/Test_BasicCAN.c
@@ -6,9 +6,16 @@
 OS_TCB Task1_TCB;
 static CPU_STK Task1_Stk[DEFAULT_STACK_SIZE];
 
-/*
- * Run this test with CarCAN in loopback mode!
+/**
+    Test the CANbus driver in loopback define either TEST_CARCAN_LOOPBACK or TEST_MOTORCAN_LOOPBACK
+    Not defining either will default to MOTORCAN loopback
  */
+
+ #define TEST_CARCAN_LOOPBACK
+ #ifndef TEST_MOTORCAN_lOOPBACK
+    #define TEST_CARCAN_LOOPBACK
+#endif
+
 
 void Task1(void *p_arg) {
     CPU_Init();
@@ -18,21 +25,34 @@ void Task1(void *p_arg) {
     BSP_GPIO_Init(BPS_FAULT_PORT, BPS_FAULT, OUTPUT, false); 
     BSP_GPIO_Init(MOTOR_CTRL_FAULT_PORT, MOTOR_CTRL_FAULT, OUTPUT, false);
     BSP_GPIO_Init(CONTROLS_FAULT_PORT, CONTROLS_FAULT, OUTPUT, false);
-    
 
-    //CANbus_Init(CARCAN, (CANId_t *) carCANFilterList, NUM_CARCAN_FILTERS);
+
+    #ifdef TEST_CARCAN_LOOPBACK
+    CANbus_Init(CARCAN, (CANId_t *) carCANFilterList, NUM_CARCAN_FILTERS);
+    #elif TEST_MOTORCAN_lOOPBACK
     CANbus_Init(MOTORCAN, (CANId_t *) motorCANFilterList, NUM_MOTORCAN_FILTERS);
-                                
-    CANDATA_t msg, out;         
-    msg.ID = VELOCITY;          
+    #endif
+
+    CANDATA_t msg, out;    
+    CAN_t bus;     
     msg.idx = 0;
     memset(&msg.data, 0xa5, sizeof msg.data);
+
+
+    #ifdef TEST_MOTORCAN_LOOPBACK
+    bus = MOTORCAN;
+    msg.ID = VELOCITY;         
+    #elif defined(TEST_CARCAN_LOOPBACK)
+    bus = CARCAN;
+    msg.ID = BPS_TRIP;
+    #endif
+
     BSP_GPIO_Write_Pin(OS_FAULT_PORT, OS_FAULT, ON);
 
     while (1) {
-        ErrorStatus sendError = CANbus_Send(msg, true, MOTORCAN);
+        ErrorStatus sendError = CANbus_Send(msg, true, bus);
         BSP_GPIO_Write_Pin(CONTROLS_FAULT_PORT, CONTROLS_FAULT, sendError == SUCCESS ? ON : OFF);
-        ErrorStatus readError = CANbus_Read(&out, true, MOTORCAN);
+        ErrorStatus readError = CANbus_Read(&out, true, bus);
         BSP_GPIO_Write_Pin(BPS_FAULT_PORT, BPS_FAULT, readError == SUCCESS ? ON : OFF);
         BSP_GPIO_Write_Pin(MOTOR_CTRL_FAULT_PORT, MOTOR_CTRL_FAULT, ON);
     }
