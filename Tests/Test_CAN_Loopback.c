22fe12d102b6912a4a0b1b3a7a62408be119f5c4
Lakshay983 <lakshay45gupta@gmail.com>
2025-03-29 12:32:51 -0500

    can loopback tet

---
+++ Tests/Test_CAN_Loopback.c
@@ -0,0 +1,45 @@
+#include "Tasks.h"
+#include "CANbus.h"
+#include "daybreak_pins.h"
+#include "StatusLeds.h"
+#include "CANConfig.h"
+
+
+#define TEST_CARCAN_LOOPBACK
+  #ifndef TEST_MOTORCAN_lOOPBACK
+     #define TEST_CARCAN_LOOPBACK
+ #endif
+
+
+int main(){
+    Status_Leds_Init();
+
+    #ifdef TEST_CARCAN_LOOPBACK
+    CANbus_Init(CARCAN, (CANId_t *) carCANFilterList, NUM_CARCAN_FILTERS);
+    #elif TEST_MOTORCAN_lOOPBACK
+    CANbus_Init(MOTORCAN, (CANId_t *) motorCANFilterList, NUM_MOTORCAN_FILTERS);
+    #endif
+
+    Status_Leds_Write(OS_FAULT_LED, true);
+    CANDATA_t msg, out;    
+    CAN_t bus;     
+    msg.idx = 0;
+    memset(&msg.data, 0xa5, sizeof msg.data);
+
+
+    #ifdef TEST_MOTORCAN_LOOPBACK
+    bus = MOTORCAN;
+    msg.ID = VELOCITY;         
+    #elif defined(TEST_CARCAN_LOOPBACK)
+    bus = CARCAN;
+    msg.ID = BPS_TRIP;
+    #endif
+
+    while(1){
+        ErrorStatus sendError = CANbus_Send(msg, true, bus);
+        Status_Leds_Write(CONTROLS_FAULT_LED);
+        ErrorStatus readError = CANbus_Read(&out, true, bus);
+        Status_Leds_Write(BPS_FAULT_LED);
+        Status_Leds_Write(MOTOR_CONTROLLER_FAULT_LED);
+    }
+}
