505232cf215c06a0eeb4180a2d0da3cb1e552bc2
Ravi Shah <ravidhruvishshah@gmail.com>
2025-04-10 16:13:26 -0500

    some helper functions

--- Tests/Test_Lights.c
+++ Tests/Test_Lights.c
@@ -6,24 +6,62 @@
 #include <bsp.h>
 #include "daybreak_pins.h"
 #include "BSP_GPIO.h"
-#define DLY_MS 250u // 250ms delay
+#define BLINK_TO_FADE_OUT 1000u // 1s delay
+#define FADE_OUT_TO_IN 1u       // 1min delay
+#define FADE_IN_TO_BLINK 1u     // 1min delay
 
 // Task_UnveilingLights
 OS_TCB UnveilingLights_TCB;
 static CPU_STK UnveilingLights_Stk[DEFAULT_STACK_SIZE];
 
+// turn all lights on at maximum brightness
+static void On_All(void)
+{
+    BSP_GPIO_Write_Pin(OS_FAULT_PORT, OS_FAULT, true);
+    BSP_GPIO_Write_Pin(TIMER_CLK_PORT, TIMER_CLK, true); // hazard button on dash MUST BE ON, otherwise won't work
+    BSP_GPIO_Write_Pin(BRAKE_LIGHT_PORT, BRAKE_LIGHT, true);
+}
+
+// turn all lights off
+static void Off_All(void)
+{
+    BSP_GPIO_Write_Pin(OS_FAULT_PORT, OS_FAULT, false);
+    BSP_GPIO_Write_Pin(TIMER_CLK_PORT, TIMER_CLK, false); // hazard button on dash MUST BE ON, otherwise won't work
+    BSP_GPIO_Write_Pin(BRAKE_LIGHT_PORT, BRAKE_LIGHT, false);
+}
+
 // function to toggle/fade lights
 static void Blink_All(void)
 {
-    // heartbeat
-    BSP_GPIO_Write_Pin(BPS_FAULT_PORT, BPS_FAULT, !BSP_GPIO_Get_State(BPS_FAULT_PORT, BPS_FAULT));
-
-    // GPIO blink test
-    BSP_GPIO_Toggle_Pin(TIMER_CLK_PORT, TIMER_CLK);
-    BSP_GPIO_Toggle_Pin(BRAKE_LIGHT_PORT, BRAKE_LIGHT);
+    // heartbeat using OS Fault LED on board
     BSP_GPIO_Toggle_Pin(OS_FAULT_PORT, OS_FAULT);
 
+    // GPIO blink turn signals and brake light
+    BSP_GPIO_Toggle_Pin(TIMER_CLK_PORT, TIMER_CLK); // hazard button on dash MUST BE ON, otherwise won't work
+    BSP_GPIO_Toggle_Pin(BRAKE_LIGHT_PORT, BRAKE_LIGHT);
 }
+
+// fades all lights from maximum brightness to off
+// TODO - make the timing configurable?
+static void Fade_All_Out(void)
+{
+    On_All();
+
+    // 90% pwm (high)
+    // all between
+    // 10% pwm (low)
+
+    Off_All();
+}
+
+// fades all lights from off to maximum brightness
+// TODO - make the timing configurable?
+static void Fade_All_Out(void)
+{
+    Off_All();
+}
+
+// TODO: more functions for fade, blink individual light, rear lights, etc
 
 // lights task
 static void Task_UnveilingLights(void *p_arg)
@@ -33,12 +71,16 @@ static void Task_UnveilingLights(void *p_arg)
     while (1)
     {
         Blink_All();
-        OSTimeDlyHMSM(0, 0, 0, DLY_MS, OS_OPT_TIME_HMSM_STRICT, &err);
+        OSTimeDlyHMSM(0, 0, 0, BLINK_TO_FADE_OUT, OS_OPT_TIME_HMSM_STRICT, &err);
+        Fade_All_Out();
+        OSTimeDlyHMSM(0, FADE_OUT_TO_IN, 0, 0, OS_OPT_TIME_HMSM_STRICT, &err);
+        Fade_All_In();
+        OSTimeDlyHMSM(0, FADE_IN_TO_BLINK, 0, 0, OS_OPT_TIME_HMSM_STRICT, &err);
         assertOSError(err);
     }
 }
 
-void Lights_Task_Init()
+static void Lights_Task_Init()
 {
     OS_ERR err;
 
@@ -46,10 +88,9 @@ void Lights_Task_Init()
     OS_CPU_SysTickInit(SystemCoreClock / (CPU_INT32U)OSCfg_TickRate_Hz);
 
     // init GPIO
-    BSP_GPIO_Init(BPS_FAULT_PORT, BPS_FAULT, OUTPUT, false);
+    BSP_GPIO_Init(OS_FAULT_PORT, OS_FAULT, OUTPUT, false);
     BSP_GPIO_Init(TIMER_CLK_PORT, TIMER_CLK, OUTPUT, false);
     BSP_GPIO_Init(BRAKE_LIGHT_PORT, BRAKE_LIGHT, OUTPUT, false);
-    BSP_GPIO_Init(OS_FAULT_PORT, OS_FAULT, OUTPUT, false);
 
     // UnveilingLights
     OSTaskCreate(
