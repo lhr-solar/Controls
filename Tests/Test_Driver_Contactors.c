f5f5b154fbbdbbfbbadf3eb8b05be13c4939f749
Lakshay983 <lakshay45gupta@gmail.com>
2025-04-19 02:08:30 -0500

    make test actually change sense

--- Tests/Test_Driver_Contactors.c
+++ Tests/Test_Driver_Contactors.c
@@ -27,7 +27,7 @@ void Task_Fake_Contactor_Driver(){
     memset(&msg.data, 0x00, sizeof msg.data);
     while(1){
         Status_Leds_Toggle(CRUISE_IND_LED);
-        msg.data[0] = 0x01;
+        msg.data[0] = 0x09;
         msg.data[1] = 0x00;
         // send fake state of Canbus
         CANbus_Send(msg, true, CARCAN);
