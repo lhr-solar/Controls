5b92631efa9990ebed434e029e0fbaff200abc08
OscarPortillo37 <oscarportillo0831@gmail.com>
2025-04-18 20:12:05 -0500

    Cleaned up PWM test

--- Tests/Test_BSP_PWM.c
+++ Tests/Test_BSP_PWM.c
@@ -9,6 +9,9 @@ int main(){
     BSP_PWM_Set_State(INDICATOR_PWM, true);
     BSP_PWM_Set_State(BRAKE_PWM, true);
 
+    volatile int b = 0;
+    for(int i = 0; i < 30000; ++i) {b++;}
+
     printf("Testing off");
     BSP_PWM_Set_State(INDICATOR_PWM, false);
     BSP_PWM_Set_State(BRAKE_PWM, false);
