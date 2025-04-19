73992545734a97ed85ab785897736d22b0a71d2f
OscarPortillo37 <oscarportillo0831@gmail.com>
2025-04-18 20:10:52 -0500

    Cleaned up PWM test

--- Tests/Test_BSP_PWM.c
+++ Tests/Test_BSP_PWM.c
@@ -2,6 +2,14 @@
 
 int main(){
     BSP_PWM_Init(25000, 50);
+    BSP_UART_Init(USB);
+    printf("=========Commencing PWM Test=======");
+
+    printf("Testing 50%% brightness");
     BSP_PWM_Set_State(INDICATOR_PWM, true);
     BSP_PWM_Set_State(BRAKE_PWM, true);
+
+    printf("Testing off");
+    BSP_PWM_Set_State(INDICATOR_PWM, false);
+    BSP_PWM_Set_State(BRAKE_PWM, false);
 }
