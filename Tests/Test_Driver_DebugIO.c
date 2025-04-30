29aaddf0494cae5e8489dd55202f1797d53ed653
Lakshay983 <lakshay45gupta@gmail.com>
2025-04-30 14:47:19 -0500

    add test

---
+++ Tests/Test_Driver_DebugIO.c
@@ -0,0 +1,15 @@
+#include "DebugIO.h"
+
+
+int main(){
+    DebugIO_Init();
+    while(1){
+        for(int i = 0; i < NUM_PINS; i++){
+            DebugIO_Toggle(i);
+            volatile uint32_t waitTime = 0;
+            while(waitTime <= 999999){
+                waitTime++;
+            }
+        }
+    }
+}
