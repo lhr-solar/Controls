8a9854c51d66a4f180f33b821c7ad51e087dab5e
Lakshay983 <lakshay45gupta@gmail.com>
2025-05-04 17:00:59 -0500

    it's over for u akshay

--- Apps/Src/ReadCarCAN.c
+++ Apps/Src/ReadCarCAN.c
@@ -348,7 +348,6 @@ static bool check_MotorControllerContactor(void){
     bool HVContactorState = Contactors_Get(HV_MINUS_CONTACTOR) && Contactors_Get(HV_PLUS_CONTACTOR);
 
     bool motorContactorState = Contactors_Get(MOTOR_CONTROLLER_CONTACTOR);
-    bool motorPrechargeContactorState = Contactors_Get(MOTOR_CONTROLLER_PRECHARGE_BYPASS_CONTACTOR);
     // if the HV contactors are off and the motor contactor is on
     if(!HVContactorState && motorContactorState)
     {
