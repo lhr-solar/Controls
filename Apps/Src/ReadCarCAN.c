fec94a6e517244a55130fe661d9b0681b6eb07c9
Madeleine Lee <madeleinercflee@gmail.com>
2025-05-13 18:58:28 -0500

    Add updateDisplay calls.

--- Apps/Src/ReadCarCAN.c
+++ Apps/Src/ReadCarCAN.c
@@ -248,7 +248,8 @@ void Task_ReadCarCAN(void *p_arg)
             // Note, does not control the Contactors, only stores the recieved state
             Contactors_Set(HV_PLUS_CONTACTOR, (bool)(dataBuf.data[0] & HV_PLUS_CONTACTOR_BIT), true);
             Contactors_Set(HV_MINUS_CONTACTOR, (bool)(dataBuf.data[0] & HV_MINUS_CONTACTOR_BIT), true);
-            Contactors_Set(ARRAY_CONTACTOR,(bool)(dataBuf.data[0] & HV_ARRAY_CONTACTOR_BIT), true);
+            Contactors_Set(ARRAY_CONTACTOR, (bool)(dataBuf.data[0] & HV_ARRAY_CONTACTOR_BIT), true);
+            UpdateDisplay_SetArray(Contactors_Get(ARRAY_CONTACTOR));
             break; // End of BPS Contactor Status Updates
         }
 
@@ -286,11 +287,13 @@ void Task_ReadCarCAN(void *p_arg)
 
             // Update Motor Contactor sense state
             Contactors_Set(MOTOR_CONTROLLER_CONTACTOR, MOTOR_SENSE_ACTUAL_VALUE(dataBuf.data), true);
+            UpdateDisplay_SetMotor(Contactors_Get(MOTOR_CONTROLLER_CONTACTOR));
             // Update Array Precharge sense state
             Contactors_Set(ARRAY_PRECHARGE_BYPASS_CONTACTOR, ARRAY_PRECHARGE_ACTUAL_VALUE(dataBuf.data), true);
-
+            UpdateDisplay_SetArrayPrecharge(Contactors_Get(ARRAY_PRECHARGE_BYPASS_CONTACTOR));
             // Update Motor Precharge sense state
             Contactors_Set(MOTOR_CONTROLLER_PRECHARGE_BYPASS_CONTACTOR, MOTOR_PRECHARGE_ACTUAL_VALUE(dataBuf.data), true);
+            UpdateDisplay_SetMotorPrecharge(Contactors_Get(MOTOR_CONTROLLER_PRECHARGE_BYPASS_CONTACTOR));
 
             Status_Leds_Write(MOTOR_PRECHARGE_CONTACTOR_LED, Contactors_Get(MOTOR_CONTROLLER_PRECHARGE_BYPASS_CONTACTOR));
             Status_Leds_Write(ARRAY_PRECHARGE_CONTACTOR_LED, Contactors_Get(ARRAY_PRECHARGE_BYPASS_CONTACTOR));
